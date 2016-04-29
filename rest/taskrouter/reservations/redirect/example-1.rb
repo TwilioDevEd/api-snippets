d9355ea90330f124d90076e5d7ac5ca44a9eb07b
Ronualdo Maciel <ronualdoxm@gmail.com>
2016-04-29 18:29:35 -0300

    Includes reservations redirect snippets

---
+++ rest/taskrouter/reservations/redirect/example-1.rb
@@ -0,0 +1,19 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = '{{ account_sid }}'
+auth_token = '{{ auth_token }}'
+workspace_sid = '{{ workspace_sid }}'
+task_sid = '{{ task_sid }}'
+
+client = Twilio::REST::TaskRouterClient.new account_sid, auth_token, workspace_sid
+
+reservation = client.workspace.tasks.get(task_sid).reservations.get(reservation_sid)
+reservation.update(Instruction: 'Redirect',
+                   RedirectCallSid: 'CA123456789',
+                   RedirectUrl: 'http://example.com/assignment_redirect')
+
+puts reservation.reservation_status
+puts reservation.worker_name
