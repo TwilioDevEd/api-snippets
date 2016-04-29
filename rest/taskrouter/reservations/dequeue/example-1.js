f6a05ee7ed09113ae4b1714b9573cd39d04f1e80
Ronualdo Maciel <ronualdoxm@gmail.com>
2016-04-29 18:05:39 -0300

    Includes taskrouter dequeue snippets

---
+++ rest/taskrouter/reservations/dequeue/example-1.js
@@ -0,0 +1,20 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These vars are your accountSid and authToken from twilio.com/user/account
+var twilio = require('twilio');
+
+var accountSid = "{{ account_sid }}";
+var authToken = "{{ auth_token }}";
+var workspaceSid = "{{ workspace_sid }}";
+var taskSid = "{{ task_sid }}";
+var reservationSid = "{{ reservation_sid }}";
+
+var client = new twilio.TaskRouterClient(accountSid, authToken, workspaceSid);
+
+// dequeue a reservation
+client.workspace.tasks(taskSid).reservations(reservationSid).update({
+    instruction: 'dequeue',
+    dequeueFrom: '+18001231234'
+}, function(err, reservation) {
+    console.log(reservation.reservation_status);
+    console.log(reservation.worker_name);
+});
