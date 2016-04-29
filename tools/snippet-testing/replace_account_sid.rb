9a9c3a69a168dc442d1845b98a3f41f685c7582f
Mario Celi <mceli@stackbuilders.com>
2016-04-29 15:54:33 -0500

    Replace account sid with ACXXXXXXXX format

--- tools/snippet-testing/replace_account_sid.rb
+++ tools/snippet-testing/replace_account_sid.rb
@@ -1,8 +1,12 @@
 def replace_account_sid(file_path)
   file = File.open(File.expand_path(file_path), 'r')
   original = file.read
-  replaced = original.gsub(/AC\w{32}/, '{{ account_sid }}')
+  replaced = original.gsub('{{ account_sid }}', 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
   replaced = replaced.gsub('{AuthToken}', '{{ auth_token }}')
+
+  # This was the original replace
+  # replaced = original.gsub(/AC\w{32}/, '{{ account_sid }}')
+  # replaced = replaced.gsub('{AuthToken}', '{{ auth_token }}')
 
   unless original == replaced
     puts replaced
@@ -13,5 +17,6 @@ def replace_account_sid(file_path)
 end
 
 Dir.glob("**/*") do |file|
+  next if file.include?('snippet-testing')
   replace_account_sid(file) unless File.directory?(file)
 end
