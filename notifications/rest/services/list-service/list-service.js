05d5cfe740b6af659bc5ac649738a04a925557ab
Jeffrey Linwood <jlinwood@gmail.com>
2016-05-09 23:32:36 -0500

    Add Node.js API snippets for notify

---
+++ notifications/rest/services/list-service/list-service.js
@@ -0,0 +1,12 @@
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var authToken = '{{ auth_token }}';
+var Twilio = require('twilio');
+
+var client = new Twilio(accountSid, authToken);
+
+client.notifications.v1.services.list()
+.then(function(response) {
+    console.log(response);
+}).catch(function(error) {
+    console.log(error);
+});
---
+++ notifications/rest/services/update-service/update-service.js
@@ -0,0 +1,13 @@
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var authToken = '{{ auth_token }}';
+var Twilio = require('twilio');
+
+var client = new Twilio(accountSid, authToken);
+
+client.notifications.v1.services("ISxxx").update({
+    friendlyName: "NewFlowHipSlackChatDock"
+}).then(function(response) {
+    console.log(response);
+}).catch(function(error) {
+    console.log(error);
+});
