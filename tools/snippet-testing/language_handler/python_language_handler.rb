76338e2ff282821dc8fc8dbc9195a4cf8172a083
Mario Celi <mcelicalderon@gmail.com>
2016-05-19 17:18:33 -0500

    Skip ssl verification in python, missing the route

--- tools/snippet-testing/language_handler/python_language_handler.rb
+++ tools/snippet-testing/language_handler/python_language_handler.rb
@@ -4,6 +4,19 @@ module LanguageHandler
   class PythonLanguageHandler < BaseLanguageHandler
     private
 
+    def text_with_replacements(file_content)
+      text_with_custom_header(file_content)
+    end
+
+    def text_with_custom_header(file_content)
+      file_content.prepend(
+        "import twilio.rest.resources.base\n"\
+        "import sys\n"\
+        "twilio.rest.resources.base.get_cert_file = lambda:  None\n"\
+        "sys.modules['twilio.rest.base.resources'] = twilio.rest.resources.base"\
+      )
+    end
+
     def lang_cname
       'py'
     end
