c8e897910142d06846e341d991556e3747d7b017
Kevin Whinnery <kevin.whinnery@gmail.com>
2016-05-21 16:12:25 -0500

    add download comment, change URL for curl

--- notifications/rest/notifications/send-notification/send-notification.py
+++ notifications/rest/notifications/send-notification/send-notification.py
@@ -1,4 +1,6 @@
-# Download the Python helper library from twilio.com/docs/python/install
+# NOTE: This example uses the next generation Twilio helper library - for more
+# information on how to download and install this version, visit
+# https://www.twilio.com/docs/libraries/python
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
--- notifications/rest/notifications/send-notification/send-notification.rb
+++ notifications/rest/notifications/send-notification/send-notification.rb
@@ -1,4 +1,6 @@
-# Get twilio-ruby from twilio.com/docs/ruby/install
+# NOTE: This example uses the next generation Twilio helper library - for more
+# information on how to download and install this version, visit
+# https://www.twilio.com/docs/libraries/ruby
 require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/user/account
