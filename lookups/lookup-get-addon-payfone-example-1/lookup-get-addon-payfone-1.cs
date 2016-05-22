cd3a1cfa1221da1a118eb4b9490c9f2cf010464d
Devin Rader <devin@twilio.com>
2016-05-22 14:58:34 -0400

    Add Snake Case Contract resolver

--- lookups/lookup-get-addon-payfone-example-1/lookup-get-addon-payfone-1.cs
+++ lookups/lookup-get-addon-payfone-example-1/lookup-get-addon-payfone-1.cs
@@ -1,6 +1,8 @@
 using Newtonsoft.Json;
 using RestSharp;
 using RestSharp.Authenticators;
+using Newtonsoft.Json.Serialization;
+
 
 // Download the twilio-csharp library from twilio.com/docs/csharp/install
 using System;
@@ -23,5 +25,51 @@ class Example
 
     Console.WriteLine(result.add_ons.results.payfone_tcpa_compliance.result.Description);
     Console.WriteLine(result.add_ons.results.payfone_tcpa_compliance.result.Response.NumberMatch);
+  }
+
+  public class SnakeCaseContractResolver : DefaultContractResolver
+  {
+    protected override string ResolvePropertyName(string propertyName)
+    {
+      return GetSnakeCase(propertyName);
+    }
+
+    private string GetSnakeCase(string input)
+    {
+      if (string.IsNullOrEmpty(input))
+        return input;
+
+      var buffer = "";
+
+      for (var i = 0; i < input.Length; i++)
+      {
+        var isLast = (i == input.Length - 1);
+        var isSecondFromLast = (i == input.Length - 2);
+
+        var curr = input[i];
+        var next = !isLast ? input[i + 1] : '\0';
+        var afterNext = !isSecondFromLast && !isLast ? input[i + 2] : '\0';
+
+        buffer += char.ToLower(curr);
+
+        if (!char.IsDigit(curr) && char.IsUpper(next))
+        {
+          if (char.IsUpper(curr))
+          {
+            if (!isLast && !isSecondFromLast && !char.IsUpper(afterNext))
+              buffer += "_";
+          }
+          else
+            buffer += "_";
+        }
+
+        if (!char.IsDigit(curr) && char.IsDigit(next))
+          buffer += "_";
+        if (char.IsDigit(curr) && !char.IsDigit(next) && !isLast)
+          buffer += "_";
+      }
+
+      return buffer;
+    }
   }
 }
