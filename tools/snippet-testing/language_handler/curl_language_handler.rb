c8f8e6556fb16b51cebc37aae87caca2a5bb1cba
Mario Celi <mceli@stackbuilders.com>
2016-06-10 17:26:42 -0500

    Enable curl snippets output verification

--- tools/snippet-testing/language_handler/curl_language_handler.rb
+++ tools/snippet-testing/language_handler/curl_language_handler.rb
@@ -1,3 +1,5 @@
+require 'open3'
+require 'json'
 require_relative 'base_language_handler'
 
 module LanguageHandler
@@ -10,6 +12,21 @@ module LanguageHandler
 
     def lang_cname
       'curl'
+    end
+
+    def execute_with_suppressed_output(command)
+      stdout, _, status = Open3.capture3(command)
+      exit_code = status.exitstatus
+      if exit_code == 0
+        begin
+          JSON.parse(stdout)
+        rescue JSON::ParserError
+          exit_code = 1
+        end
+      end
+      success = exit_code == 0
+      puts success ? "success [#{lang_cname}]".green : "failure [#{lang_cname}]".red
+      success
     end
 
     def text_with_specific_replacements(file_content)
