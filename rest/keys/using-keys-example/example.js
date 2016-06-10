5b5f66e1cd41e93263f6735a2b1d91660a26719c
Kevin Whinnery <kevin.whinnery@gmail.com>
2016-06-10 16:59:07 -0500

    WIP snippets

---
+++ rest/keys/using-keys-example/example.js
@@ -0,0 +1,20 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// Instantiate a client using your API key and secret - but then, you'll need to
+// manually specify your main account (or subaccount) SID when making a request
+var accountSid = '{{ account_sid }}';
+var apiKey = '{{ api_key }}';
+var apiSecret = '{{ api_secret }}';
+var client = require('twilio')(apiKey, apiSecret);
+
+// Create a reference to your Twilio account
+var account = client.accounts(accountSid);
+
+// Create a message for your account (send an outbound SMS)
+account.messages.create({
+  to: '+16518675309', // the destination phone number
+  from: '+15017250604', // a Twilio number in your account
+  body: 'Never gonna give you up.' // body of a text message
+}, (err, messageData) => {
+  // print SID of the message you just sent
+  console.log(messageData.sid);
+});
