16e54568e80a180f21b59193b1fcce83b2956be6
Mario Celi <mceli@stackbuilders.com>
2016-06-14 17:08:57 -0500

    Fix snippets until rest/messages

--- rest/messages/send-message/example-1.cs
+++ rest/messages/send-message/example-1.cs
@@ -1,21 +1,20 @@
-// Download the twilio-csharp library from twilio.com/docs/csharp/install 
-using System; 
-using Twilio; 
-class Example 
-{ 
- static void Main(string[] args) 
- { 
-    // Find your Account Sid and Auth Token at twilio.com/user/account 
-    string AccountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; 
-    string AuthToken = "[AuthToken]"; 
-    var twilio = new TwilioRestClient(AccountSid, AuthToken); 
- 
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+class Example
+{
+ static void Main(string[] args)
+ {
+    // Find your Account Sid and Auth Token at twilio.com/user/account
+    string AccountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    string AuthToken = "[AuthToken]";
+    var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
     var message = twilio.SendMessage(
-        "+14158141829", "+16518675309", 
-        "Hey Jenny! Good luck on the bar exam!", 
-        new string[] { "http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg" } , 
-        null
-    ); 
-    Console.WriteLine(message.Sid); 
- } 
+        "+14158141829", "+16518675309",
+        "Hey Jenny! Good luck on the bar exam!",
+        new string[] { "http://farm2.static.flickr.com/1075/1404618563_3ed9a44a3a.jpg" }
+    );
+    Console.WriteLine(message.Sid);
+ }
 }
--- rest/messages/send-messages-copilot/send-messages-copilot.java
+++ rest/messages/send-messages-copilot/send-messages-copilot.java
@@ -1,27 +1,31 @@
-// You may want to be more specific in your imports 
-import java.util.*; 
-import com.twilio.sdk.*; 
-import com.twilio.sdk.resource.factory.*; 
-import com.twilio.sdk.resource.instance.*; 
-import com.twilio.sdk.resource.list.*; 
- 
-public class TwilioTest { 
-  // Find your Account Sid and Token at twilio.com/user/account 
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; 
-  public static final String AUTH_TOKEN = "[AuthToken]"; 
+// You may want to be more specific in your imports
+import java.util.*;
+import com.twilio.sdk.*;
+import com.twilio.sdk.resource.factory.*;
+import com.twilio.sdk.resource.instance.*;
+import com.twilio.sdk.resource.list.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
-  public static void main(String[]args) throws TwilioRestException { 
-    TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN); 
+public class TwilioTest {
+  // Find your Account Sid and Token at twilio.com/user/account
+  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  public static final String AUTH_TOKEN = "[AuthToken]";
 
-    // Build the parameters 
-    List<NameValuePair> params = new ArrayList<NameValuePair>(); 
-    params.add(new BasicNameValuePair("To", "+16518675309")); 
-    params.add(new BasicNameValuePair("MessagingServiceSid", "MG9752274e9e519418a7406176694466fa")); 
-    params.add(new BasicNameValuePair("Body", 
+  public static void main(String[]args) throws TwilioRestException {
+    TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+    // Build the parameters
+    List<NameValuePair> params = new ArrayList<NameValuePair>();
+    params.add(new BasicNameValuePair("To", "+16518675309"));
+    params.add(new BasicNameValuePair("MessagingServiceSid", "MG9752274e9e519418a7406176694466fa"));
+    params.add(new BasicNameValuePair("Body",
       "Phantom Menace was clearly the best of the prequel trilogy."));
 
-    MessageFactory messageFactory = client.getAccount().getMessageFactory(); 
-    Message message = messageFactory.create(params); 
-    System.out.println(message.getSid()); 
-  } 
+    MessageFactory messageFactory = client.getAccount().getMessageFactory();
+    Message message = messageFactory.create(params);
+    System.out.println(message.getSid());
+  }
 }
