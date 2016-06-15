7ef58e7f1ee9726e23abe4adff0d024a5e52a2b8
Mario Celi <mceli@stackbuilders.com>
2016-06-15 09:14:32 -0500

    Fix rest/sip-in snippets

--- rest/sip-in/create-domain/create-domain.cs
+++ rest/sip-in/create-domain/create-domain.cs
@@ -1,22 +1,23 @@
 // Download the twilio-csharp library from twilio.com/docs/csharp/install
 using System;
 using Twilio;
-class Example 
+
+class Example
 {
-  static void Main(string[] args) 
+  static void Main(string[] args)
   {
     // Find your Account Sid and Auth Token at twilio.com/user/account
     string AccountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
     string AuthToken = "your_auth_token";
     var twilio = new TwilioRestClient(AccountSid, AuthToken);
 
-    var options = new SipDomainOptions();
+    var options = new DomainOptions();
     options.FriendlyName = "My Domain";
     options.VoiceUrl = "https://demo.twilio.com/welcome";
     options.AuthType = "IP_ACL";
     options.DomainName = "marlo.sip.twilio.com";
-    var domain = twilio.CreateSipDomain(options);
-    
+    var domain = twilio.CreateDomain(options);
+
     Console.WriteLine(domain.Sid);
   }
 }
--- rest/sip-in/get-domain-instance/get-domain-instance.cs
+++ rest/sip-in/get-domain-instance/get-domain-instance.cs
@@ -1,17 +1,17 @@
 // Download the twilio-csharp library from twilio.com/docs/csharp/install
 using System;
 using Twilio;
-class Example 
+class Example
 {
-  static void Main(string[] args) 
+  static void Main(string[] args)
   {
     // Find your Account Sid and Auth Token at twilio.com/user/account
     string AccountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
     string AuthToken = "your_auth_token";
     var twilio = new TwilioRestClient(AccountSid, AuthToken);
 
-    var domain = twilio.GetSipDomain("SD32a3c49700934481addd5ce1659f04d2");
-    
+    var domain = twilio.GetDomain("SD32a3c49700934481addd5ce1659f04d2");
+
     Console.WriteLine(domain.VoiceMethod);
   }
 }
