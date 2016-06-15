649811e813f8ffd5d3b4c5f44aa1ecee7cd3d9e3
Mario Celi <mceli@stackbuilders.com>
2016-06-15 15:59:32 -0500

    Fix more taskrouter snippets

--- rest/taskrouter/activities/list/post/example-1/example-1.php
+++ rest/taskrouter/activities/list/post/example-1/example-1.php
@@ -7,7 +7,7 @@ $accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 $authToken = "your_auth_token";
 $workspaceSid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-$client = new TaskRouter_Services_Twilio($accountSid, $authToken, null);
+$client = new TaskRouter_Services_Twilio($accountSid, $authToken, $workspaceSid);
 
 $activity = $client->workspace->activities->create("NewAvailableActivity", true);
 echo $activity->friendly_name;
