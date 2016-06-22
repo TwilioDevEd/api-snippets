e80845257d9c71e9c5d1fa1d9adafa48b128ab7e
Mario Celi <mceli@stackbuilders.com>
2016-06-22 16:37:48 -0500

    Delete broken snippets in rest and pricing directories

--- rest/call-feedback/summary-instance-delete-example-1/summary-instance-delete-example-1.py
+++
@@ -1,9 +0,0 @@
-# Download the Python helper library from twilio.com/docs/python/install
-from twilio.rest import TwilioRestClient
-
-# Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-auth_token  = "your_auth_token"
-client = TwilioRestClient(account_sid, auth_token)
-
-client.feedback_summary.delete("FSa346467ca321c71dbd5e12f627deb854")
