f6074b4424e062b7464304510d702e21b3df43c3
Mario Celi <mceli@stackbuilders.com>
2016-07-08 14:06:15 -0500

    Make test harness test 4.x.php and 5.x.php files

--- tools/snippet-testing/language_handler/php_4_language_handler.rb
+++ tools/snippet-testing/language_handler/php_4_language_handler.rb
@@ -2,6 +2,6 @@ require_relative 'php_language_handler'
 
 module LanguageHandler
   class Php4LanguageHandler < PhpLanguageHandler
-    LANG_CNAME = '4.php'.freeze
+    LANG_CNAME = '4.x.php'.freeze
   end
 end
