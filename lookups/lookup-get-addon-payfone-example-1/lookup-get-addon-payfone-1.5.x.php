7d34cdeb92632079e55ca3d49d870c5f743732fa
Eliecer Hernandez <ehernandez@stackbuilders.com>
2016-07-13 16:27:43 -0500

    Add lookups twilio-php v5 snippets

---
+++ lookups/lookup-get-addon-payfone-example-1/lookup-get-addon-payfone-1.5.x.php
@@ -0,0 +1,15 @@
+<?php
+
+$url = 'https://lookups.twilio.com/v1/PhoneNumbers/+16502530000/?AddOns=payfone_tcpa_compliance&AddOns.payfone_tcpa_compliance.RightPartyContactedDate=20160101';
+
+$process = curl_init($url);
+curl_setopt($process, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($process, CURLOPT_USERPWD, 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX:your_auth_token');
+curl_setopt($process, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+$response = curl_exec($process);
+curl_close($process);
+
+echo $response;
+
+?>
