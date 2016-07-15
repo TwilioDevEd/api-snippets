35d018d5e66572e2f71ef2ed49033b1934dd23dd
Eliecer Hernandez <ehernandez@stackbuilders.com>
2016-07-15 10:55:44 -0500

    Add php v5 for notifications snippets

---
+++ notifications/rest/bindings/retrieve-binding/retrieve-binding.5.x.php
@@ -0,0 +1,22 @@
+<?php
+// NOTE: This example uses the next generation Twilio helper library - for more
+// information on how to download and install this version, visit
+// https://www.twilio.com/docs/libraries/php
+require_once '/path/to/vendor/autoload.php';
+
+use Twilio\Rest\Client;
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token = "your_auth_token";
+
+// Initialize the client
+$client = new Client($sid, $token);
+
+// Retrieve a binding
+$binding = $client->notifications->services()
+    ->getContext("ISxxx")
+    ->bindings("BSxxx")
+    ->fetch();
+
+echo $binding->identity;
