d2221a17052dc53aec8c073159d57efe4f0f369a
Eliecer Hernandez <ehernandez@stackbuilders.com>
2016-07-15 12:31:08 -0500

    Add next-gen php snippets for stun-turn

---
+++ stun-turn/list-post-example/list-post-example.5.x.php
@@ -0,0 +1,11 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$authToken = "your_auth_token";
+$client = new Services_Twilio($sid, $authToken);
+
+$token = $client->account->tokens->create();
+echo $token->username;
