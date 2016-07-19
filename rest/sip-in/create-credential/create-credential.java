d56cfdafac3cd269ccb75424479494ace85d6a7f
Mario Celi <mceli@stackbuilders.com>
2016-07-19 13:59:22 -0500

    Remove first duplicate snippets sip-in dir

--- rest/sip-in/create-credential/create-credential.java
+++ rest/sip-in/create-credential/create-credential.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-public class Example { 
+public class Example {
 
   // Find your Account Sid and Token at twilio.com/user/account
   public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
@@ -22,8 +22,7 @@ public class Example {
     List<NameValuePair> params = new ArrayList<NameValuePair>();
     params.add(new BasicNameValuePair("Username", "WeeBey"));
     params.add(new BasicNameValuePair("Password", "05"));
-    
-    
+
     CredentialFactory credentialFactory = client.getAccount().getCredentialList("CL32a3c49700934481addd5ce1659f04d2").getCredentialFactory();
     Credential credential = credentialFactory.create(params);
     System.out.println(credential.getSid());
