06ee11d58ba39689b12f73450f43a876eba3eaf6
Mario Celi <mceli@stackbuilders.com>
2016-07-28 15:11:06 -0500

    Remove duplicate snippets in rest/domain/

--- rest/sip-in/create-domain/create-domain.4.x.php
+++ rest/sip-in/create-domain/create-domain.4.x.php
@@ -3,12 +3,16 @@
 require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; 
-$token = "your_auth_token"; 
+$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token = "your_auth_token";
 $client = new Services_Twilio($sid, $token);
 
-$domain = $client->account->sip->domains->create("My Domain", "marlo.sip.twilio.com", array(
+$domain = $client->account->sip->domains->create(
+    "My Domain",
+    "marlo.sip.twilio.com",
+    array(
         "VoiceUrl" => "https://demo.twilio.com/welcome",
-    "AuthType" => "IP_ACL"
-    ));
+        "AuthType" => "IP_ACL"
+    )
+);
 echo $domain->sid;
