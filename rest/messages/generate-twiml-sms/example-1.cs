77b04e7bee4dd6d5ad550c741d9f7453fac0bb41
David Prothero <david@prothero.com>
2016-08-11 09:43:29 -0700

    Improved C# example

--- rest/messages/generate-twiml-sms/example-1.cs
+++ rest/messages/generate-twiml-sms/example-1.cs
@@ -1,4 +1,17 @@
-Response.ContentType = "text/xml";
-var twiml = new Twilio.TwiML.TwilioResponse();
+// In Package Manager, run:
+// Install-Package Twilio.Mvc -DependencyVersion HighestMinor
 
-twiml.Message("The Robots are coming! Head for the hills!")
+using Twilio.Mvc;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+
+public class HelloWorldController : TwilioController
+{
+  [HttpPost]
+  public ActionResult Index(SmsRequest request)
+  {
+      var response = new TwilioResponse();
+      response.Message("The Robots are coming! Head for the hills!");
+      return TwiML(response);
+  }
+}
