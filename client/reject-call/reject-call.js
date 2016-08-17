282b98a599ba73e80ed84bc5715fece523f6fde0
David Prothero <david@prothero.com>
2016-08-17 14:47:34 -0700

    Twilio Client - reject incoming call

---
+++ client/reject-call/reject-call.js
@@ -0,0 +1,11 @@
+Twilio.Device.incoming(function (conn) {
+  console.log('Incoming connection from ' + conn.parameters.From);
+  var archEnemyPhoneNumber = '+15417280966';
+  
+  if (conn.parameters.From === archEnemyPhoneNumber) {
+    conn.reject();
+  } else {
+    // accept the incoming connection and start two-way audio
+    conn.accept();
+  }
+});
