386b0a1de476f6dbd19d176d8d4280ef0930931f
David Prothero <david@prothero.com>
2016-08-17 07:14:08 -0700

    Updated Twilio Client outgoing capability snippets

--- client/capability-token-outgoing/capability-token.5.x.php
+++ client/capability-token-outgoing/capability-token.5.x.php
@@ -1,1 +1,15 @@
-$capability->allowClientOutgoing('APXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+<?php
+include "vendor/autoload.php";
+
+use Twilio\Jwt\ClientToken;
+
+// put your Twilio API credentials here
+$accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$authToken  = 'your_auth_token';
+$appSid = 'APXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+$capability = new ClientToken($accountSid, $authToken);
+$capability->allowClientOutgoing($appSid);
+$token = $capability->generateToken();
+
+echo $token
--- client/capability-token-outgoing/capability-token.js
+++ client/capability-token-outgoing/capability-token.js
@@ -1,1 +1,28 @@
-  capability.allowClientOutgoing("APXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+var http = require('http'),
+    express = require('express'),
+    twilio = require('twilio');
+
+var app = express();
+
+app.get('/token', function(req, res) {
+  // put your Twilio API credentials here
+  var accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  var authToken = "your_auth_token";
+
+  // put your Twilio Application Sid here
+  var appSid = "APXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+  var capability = new twilio.Capability(accountSid, authToken);
+  capability.allowClientOutgoing(appSid);
+  var token = capability.generate();
+
+  res.set('Content-Type', 'application/jwt')
+  res.send(token);
+});
+
+app.post('/voice', function (req, res) {
+    // TODO: Create TwiML response
+});
+
+http.createServer(app).listen(1337, '127.0.0.1');
+console.log('Twilio Client app server running at http://127.0.0.1:1337/');
