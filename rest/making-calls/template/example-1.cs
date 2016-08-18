8f036f0626021a5b0fd98c04c47445e0475f5452
Mario Celi <mceli@stackbuilders.com>
2016-08-18 17:27:21 -0500

    Add C# template

---
+++ rest/making-calls/template/example-1.cs
@@ -0,0 +1,28 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/user/account
+    {#accountSid}}string AccountSid = "{{accountSid}}";{{/accountSid}}
+    {{#authToken}}string AuthToken = "{{authToken}}";{{/authToken}}
+    var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+    var options = new CallOptions();
+    {{#fromPhoneNumber}}options.From = "{{fromPhoneNumber}}";{{/fromPhoneNumber}}
+    {{#toPhoneNumber}}options.To = "{{toPhoneNumber}}";{{/toPhoneNumber}}
+    {{#toVoiceClient}}options.To = "{{toVoiceClient}}";{{/toVoiceClient}}
+    {{#callUrl}}options.Url = "{{callUrl}}";{{/callUrl}}
+    {{#sendDigitsVoice}}options.SendDigits = "{{#sendDigitsVoice}}";{{/sendDigitsVoice}}
+    {{#callMethod}}options.Method = "{{#callMethod}}";{{/callMethod}}
+    {{#callStatusCallback}}options.StatusCallback = "{{callStatusCallback}}";{{/callStatusCallback}}
+    {{#callStatusCallbackMethod}}options.StatusCallbackMethod = "{{callStatusCallbackMethod}}";{{/callStatusCallbackMethod}}
+    {{#callStatusCallbackEvent}}options.StatusCallbackEvents = new string[] { {{#callStatusCallbackEvents}}{{.}}{{/callStatusCallbackEvents}} };{{/callStatusCallbackEvent}}
+
+    var call = twilio.InitiateOutboundCall(options);
+
+    Console.WriteLine(call.Sid);
+  }
+}
