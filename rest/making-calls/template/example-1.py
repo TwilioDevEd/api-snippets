5044cf97cb1c5126f5d8cf1458cd4e86ac0ca565
Mario Celi <mceli@stackbuilders.com>
2016-08-19 09:57:12 -0500

    Add ruby template, fix python template

--- rest/making-calls/template/example-1.py
+++ rest/making-calls/template/example-1.py
@@ -2,8 +2,8 @@
 from twilio.rest import TwilioRestClient
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-auth_token  = "your_auth_token"
+{{#accountSid}}account_sid = "{{accountSid}}"{{/accountSid}}
+{{#authToken}}auth_token = "{{authToken}}"{{/authToken}}
 client = TwilioRestClient(account_sid, auth_token)
 
 call = client.calls.create(
