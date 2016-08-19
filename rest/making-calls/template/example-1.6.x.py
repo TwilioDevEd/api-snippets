193e8a9c0ffede52d4243fcb78d2c6dfa9573fb3
Mario Celi <mceli@stackbuilders.com>
2016-08-19 17:15:28 -0500

    Add python next-gen templates

---
+++ rest/making-calls/template/example-1.6.x.py
@@ -0,0 +1,26 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "{{accountSid}}"
+auth_token = "{{authToken}}"
+client = Client(account_sid, auth_token)
+
+call = client.account.calls.create(
+    {{#fromPhoneNumber}}from_="{{fromPhoneNumber}}",{{/fromPhoneNumber}}
+    {{#toPhoneNumber}}to="{{toPhoneNumber}}",{{/toPhoneNumber}}
+    {{#toVoiceClient}}to="{{toVoiceClient}}",{{/toVoiceClient}}
+    {{#callUrl}}url="{{callUrl}}",{{/callUrl}}
+    {{#sendDigitsVoice}}send_digits="{{sendDigitsVoice}}",{{/sendDigitsVoice}}
+    {{#callMethod}}method: "{{callMethod}}",{{/callMethod}}
+    {{#callStatusCallback}}status_callback="{{callStatusCallback}}",{{/callStatusCallback}}
+    {{#callStatusCallbackMethod}}status_callback_method="{{callStatusCallbackMethod}}",{{/callStatusCallbackMethod}}
+    {{#callStatusCallbackEvent}}
+    status_callback_event=[
+    {{#callStatusCallbackEvents}}
+        "{{.}}",
+    {{/callStatusCallbackEvents}}
+    ],
+    {{/callStatusCallbackEvent}}
+)
+print(call.sid)
