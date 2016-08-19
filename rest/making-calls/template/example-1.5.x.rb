203338329e576fcac9f2f9626c0a905836f08978
Mario Celi <mceli@stackbuilders.com>
2016-08-19 16:47:13 -0500

    Add ruby next-gen template

---
+++ rest/making-calls/template/example-1.5.x.rb
@@ -0,0 +1,27 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "{{accountSid}}"
+auth_token = "{{authToken}}"
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+call = @client.account.calls.create(
+    {{#fromPhoneNumber}}from: "{{fromPhoneNumber}}",{{/fromPhoneNumber}}
+    {{#toPhoneNumber}}to: "{{toPhoneNumber}}",{{/toPhoneNumber}}
+    {{#toVoiceClient}}to: "{{toVoiceClient}}",{{/toVoiceClient}}
+    {{#callUrl}}url: "{{callUrl}}",{{/callUrl}}
+    {{#sendDigitsVoice}}send_digits: "{{sendDigitsVoice}}",{{/sendDigitsVoice}}
+    {{#callMethod}}method: "{{callMethod}}",{{/callMethod}}
+    {{#callStatusCallback}}status_callback: "{{callStatusCallback}}",{{/callStatusCallback}}
+    {{#callStatusCallbackMethod}}status_callback_method: "{{callStatusCallbackMethod}}",{{/callStatusCallbackMethod}}
+    {{#callStatusCallbackEvent}}
+    status_callback_event: [
+    {{#callStatusCallbackEvents}}
+        "{{.}}",
+    {{/callStatusCallbackEvents}}
+    ],
+    {{/callStatusCallbackEvent}}
+)
+puts call.to
