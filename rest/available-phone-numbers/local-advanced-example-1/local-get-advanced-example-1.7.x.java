f259df10bb69f8ccf7cb18ecd3e8336ee430e7c6
Mario Celi <mceli@stackbuilders.com>
2016-08-22 17:31:21 -0500

    Fix java next-gen local get advanced example

--- rest/available-phone-numbers/local-advanced-example-1/local-get-advanced-example-1.7.x.java
+++ rest/available-phone-numbers/local-advanced-example-1/local-get-advanced-example-1.7.x.java
@@ -1,8 +1,8 @@
 // Install the Java helper library from twilio.com/docs/java/install
 import com.twilio.Twilio;
-import com.twilio.rest.api.v2010.account.incomingphonenumber.LocalCreator;
 import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.availablephonenumbercountry.Local;
+import com.twilio.rest.api.v2010.account.incomingphonenumber.LocalCreator;
 import com.twilio.type.PhoneNumber;
 
 public class Example {
@@ -13,7 +13,8 @@ public class Example {
   public static void main(String[] args) {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    ResourceSet<Local> numbers = Local.read("US").byAreaCode(619).byContains("555").execute();
+    ResourceSet<Local> numbers = Local.read("US").byContains("555")
+        .byNearLatLong("37.840699,-122.461853").byDistance(50).byInRegion("CA").execute();
     PhoneNumber availableNumber = numbers.iterator().next().getPhoneNumber();
 
     new LocalCreator(availableNumber).execute();
