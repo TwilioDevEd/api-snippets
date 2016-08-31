c77c84b58a07a4f254a520c6c99dea97abb0b83c
David Prothero <david@prothero.com>
2016-08-31 15:07:30 -0700

    Fixed sms w/ callback Java 7.x sample

--- rest/messages/send-sms-callback/send-sms-callback.7.x.java
+++ rest/messages/send-sms-callback/send-sms-callback.7.x.java
@@ -1,23 +1,22 @@
 // Install the Java helper library from twilio.com/docs/java/install
-import com.twilio.sdk.Twilio;
-import com.twilio.sdk.resource.api.v2010.account.Message;
-import com.twilio.sdk.type.PhoneNumber;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
 public class Example {
   // Find your Account Sid and Token at twilio.com/user/account
   public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String API_KEY = "SKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String API_SECRET = "your_api_secret";
+  public static final String AUTH_TOKEN = "your_auth_token";
 
   public static void main(String[] args) {
-    Twilio.init(API_KEY, API_SECRET, ACCOUNT_SID);
+    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    Message message = Message.create(ACCOUNT_SID,
-        new PhoneNumber("+15558675309"),
-        new PhoneNumber("+15017250604"),
-        "McAvoy or Stewart? These timelines can get so confusing.")
-            .setStatusCallback("http://requestb.in/1234abcd")
-            .execute();
+    Message message = Message.create(
+      new PhoneNumber("+15558675309"),
+      new PhoneNumber("+15017250604"),
+      "McAvoy or Stewart? These timelines can get so confusing.")
+        .setStatusCallback("http://requestb.in/1234abcd")
+        .execute();
 
     System.out.println(message.getSid());
   }
