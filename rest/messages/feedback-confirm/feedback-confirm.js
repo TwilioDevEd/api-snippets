98f7f10f49d28e417411ca786de6ccda88d17b9a
David Prothero <david@prothero.com>
2016-09-02 16:28:23 -0700

    Feedback API Node.js samples completed

--- rest/messages/feedback-confirm/feedback-confirm.js
+++ rest/messages/feedback-confirm/feedback-confirm.js
@@ -1,18 +1,36 @@
+// Twilio Credentials 
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; 
+var authToken = 'your_auth_token'; 
+
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+
 var http = require('http'),
     express = require('express');
 
 var app = express();
 
 app.get('/confirm', function(req, res) {
-    var uniqueId = req.query.id;
-    // Lookup variable `uniqueId` in a database to find messageSid
-    var messageSid = 'SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+  var uniqueId = req.query.id;
+  // Lookup variable `uniqueId` in a database to find messageSid
+  var messageSid = 'SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-    // TODO: Implement
+  // Send Feedback to Twilio
+  client.request({
+    url: '/Accounts/' + accountSid + '/Messages/' + messageSid + '/Feedback',
+    method: 'POST',
+    form: {
+      Outcome: "confirmed"
+    }
+  }, function (err, data) {
 
+    // Handle remaining request normally
+    res.send('Thank you!');
     res.end();
+
+  });
 });
 
 http.createServer(app).listen(1337, function () {
-    console.log("Express server listening on port 1337");
+    console.log('Express server listening on port 1337');
 });
