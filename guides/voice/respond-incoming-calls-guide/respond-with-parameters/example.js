08351cb0cde8b7fd1dfc15b017aeabf4ed2bd40a
Kevin Whinnery <kevin.whinnery@gmail.com>
2016-09-01 23:21:16 -0500

    add voice guide snippets for incoming

---
+++ guides/voice/respond-incoming-calls-guide/respond-with-parameters/example.js
@@ -0,0 +1,33 @@
+// This example uses JavaScript language features present in Node.js 6+
+'use strict';
+
+const express = require('express');
+const twilio = require('twilio');
+const urlencoded = require('body-parser').urlencoded;
+
+let app = express();
+
+// Parse incoming POST params with Express middleware
+app.use(urlencoded({ extended: false }));
+
+// Create a route that will handle Twilio webhook requests, sent as an 
+// HTTP POST to /voice in our application
+app.post('/voice', (request, response) => {
+  // Get information about the incoming call, like the city associated
+  // with the phone number (if Twilio can discover it)
+  let city = request.body.FromCity;
+
+  // Use the Twilio Node.js SDK to build an XML response
+  let twiml = new twilio.TwimlResponse();
+  twiml.say(`Never gonna give you up ${city}.`, { 
+    voice: 'alice' 
+  });
+  twiml.play('https://demo.twilio.com/docs/classic.mp3');
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
+});
+
+// Create an HTTP server and listen for requests on port 3000
+app.listen(3000);
