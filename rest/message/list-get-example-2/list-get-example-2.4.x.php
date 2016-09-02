6afbfaf0bcf3b1b463639dbecf5747db55f9181f
David Prothero <david@prothero.com>
2016-09-02 09:48:04 -0700

    SMS Message List with filter sample

---
+++ rest/message/list-get-example-2/list-get-example-2.4.x.php
@@ -0,0 +1,14 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; 
+$token = "your_auth_token"; 
+$client = new Services_Twilio($sid, $token);
+
+// Loop over the list of messages and echo a property for each one
+// TODO: Add To, From, DateSent> parameters
+foreach ($client->account->messages as $message) {
+    echo $message->body;
+}
---
+++ rest/message/list-get-example-2/list-get-example-2.6.x.java
@@ -0,0 +1,25 @@
+// Install the Java helper library from twilio.com/docs/java/install
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.instance.Message;
+import com.twilio.sdk.resource.list.MessageList;
+
+public class Example { 
+
+  // Find your Account Sid and Token at twilio.com/user/account
+  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  public static final String AUTH_TOKEN = "your_auth_token";
+
+  public static void main(String[] args) throws TwilioRestException {
+    TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+    // TODO: Add To, From, DateSent> parameters
+    MessageList messages = client.getAccount().getMessages();
+    
+    // Loop over messages and print out a property for each one.
+    for (Message message : messages) {
+      System.out.println(message.getBody());
+    }
+    
+  }
+}
