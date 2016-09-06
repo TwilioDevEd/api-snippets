0cc69fc21f22791bc6a7064a07fefe54a1dd7f9a
Andrew T. Baker <abaker@twilio.com>
2016-09-05 22:16:01 -0700

    Add samples for Record Calls voice guide

---
+++ guides/voice/record-calls-guide/record-twiml-transcribe/example.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+from twilio import twiml
+
+@app.route("/record", methods=['GET', 'POST'])
+def record():
+    """Returns TwiML which prompts the caller to record a message"""
+    # Start our TwiML response
+    response = twiml.Response()
+
+    # Use <Say> to give the caller some instructions
+    response.say('Hello. Please leave a message and I will transcribe it.')
+    response.record(transcribe=True, maxLength=30)
+    response.hangup()
+
+    return str(response)
+
+if __name__ == "__main__":
+    app.run()
