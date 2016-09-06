e551d8886f122685c6c6f517a25c821724208cb0
Andrew T. Baker <abaker@twilio.com>
2016-09-05 22:29:06 -0700

    Update comment in record transcribe example

--- guides/voice/record-calls-guide/record-twiml-transcribe/example.py
+++ guides/voice/record-calls-guide/record-twiml-transcribe/example.py
@@ -12,7 +12,7 @@ def record():
     # Use <Say> to give the caller some instructions
     response.say('Hello. Please leave a message and I will transcribe it.')
 
-    # Use <Record> to record the caller's message
+    # Use <Record> to record and transcribe the caller's message
     response.record(transcribe=True, maxLength=30)
 
     # End the call with <Hangup>
