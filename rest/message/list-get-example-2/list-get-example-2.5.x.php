ca80d7fd89cea4fe7726a12409519b487c6b1fb0
mosampaio <mosampaio@gmail.com>
2016-09-07 12:46:55 -0300

    Undo vendor path modification

--- rest/message/list-get-example-2/list-get-example-2.5.x.php
+++ rest/message/list-get-example-2/list-get-example-2.5.x.php
@@ -1,6 +1,6 @@
 <?php
 // Get the PHP helper library from twilio.com/docs/php/install
-require_once 'vendor/autoload.php'; // Loads the library
+require_once '/path/to/vendor/autoload.php'; // Loads the library
 use Twilio\Rest\Client;
 
 // Your Account Sid and Auth Token from twilio.com/user/account
