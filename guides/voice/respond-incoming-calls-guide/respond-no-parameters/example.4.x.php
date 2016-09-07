b7a409eb32d9653e2475dc198c7dbce0786818d8
mosampaio <mosampaio@gmail.com>
2016-09-07 16:34:47 -0300

    Add PHP snippets to respond-incoming-phone-call

---
+++ guides/voice/respond-incoming-calls-guide/respond-no-parameters/example.4.x.php
@@ -0,0 +1,9 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+
+// this line loads the library 
+require_once '/path/to/vendor/autoload.php';
+
+$response = new Services_Twilio_Twiml;
+$response->say("hello world!", array('voice' => 'alice'));
+print $response;
