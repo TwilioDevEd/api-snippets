300af95d0593ced90907427b91d1e1032f2aab4d
Andrew T. Baker <abaker@twilio.com>
2016-09-08 13:05:12 -0700

    Add filters to Python list-get-example-2

--- rest/message/list-get-example-2/list-get-example-2.py
+++ rest/message/list-get-example-2/list-get-example-2.py
@@ -1,11 +1,15 @@
-# Download the Python helper library from twilio.com/docs/python/install
+# Download the Python helper library from twilio.com/docs/libraries/python
+from datetime import date
 from twilio.rest import TwilioRestClient
 
-# Your Account Sid and Auth Token from twilio.com/user/account
+# Your Account Sid and Auth Token from twilio.com/console
 account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 auth_token  = "your_auth_token"
 client = TwilioRestClient(account_sid, auth_token)
 
-# A list of message objects with the properties described above
-# TODO: Add To, From, DateSent> parameters
-messages = client.messages.list()
+# A list of message objects with filtering
+messages = client.messages.list(
+    to='+15558675309',
+    from_='+15017250604',
+    date_sent=date(2016, 8, 31)
+)
