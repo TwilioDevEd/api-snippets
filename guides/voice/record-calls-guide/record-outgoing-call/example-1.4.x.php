5cbd655c938d7dbadb6d4f17a12eea96bbf38202
Agustin Camino <agustin.camino@gmail.com>
2016-09-08 08:43:36 -0500

    Add record outgoing call for PHP

--- guides/voice/record-calls-guide/record-outgoing-call/example-1.4.x.php
+++ guides/voice/record-calls-guide/record-outgoing-call/example-1.4.x.php
@@ -3,9 +3,13 @@
 require_once('/path/to/twilio-php/Services/Twilio.php'); // Loads the library
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; 
-$token = "your_auth_token"; 
-$client = new Services_Twilio($sid, $token);
+$accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$authToken = "your_auth_token";
+$client = new Services_Twilio($accountSid, $authToken);
 
-$call = $client->account->calls->create("+15017250604", "+14155551212", "http://demo.twilio.com/docs/voice.xml", array());
+$call = $client->account->calls->create(
+    "+15017250604", "+14155551212", "http://demo.twilio.com/docs/voice.xml",
+    array("Record" => true)
+);
+
 echo $call->sid;
--- guides/voice/record-calls-guide/record-outgoing-call/example-1.5.x.php
+++ guides/voice/record-calls-guide/record-outgoing-call/example-1.5.x.php
@@ -4,13 +4,13 @@ require_once '/path/to/vendor/autoload.php'; // Loads the library
 use Twilio\Rest\Client;
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-$token = "your_auth_token";
-$client = new Client($sid, $token);
+$accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$authToken = "your_auth_authToken";
+$client = new Client($accountSid, $authToken);
 
 $call = $client->calls->create(
     "+14155551212", "+14158675309",
-    array("url" => "http://demo.twilio.com/docs/voice.xml")
+    array("url" => "http://demo.twilio.com/docs/voice.xml", "record" => true)
 );
 
 echo $call->sid;
