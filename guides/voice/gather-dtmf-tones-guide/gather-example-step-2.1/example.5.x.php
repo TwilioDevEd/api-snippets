42e76dd75161a3ded471a74763d89108134d3ac0
Mario Celi <mceli@stackbuilders.com>
2016-09-08 15:22:10 -0500

    Add snippets for Gather User Input via Keypad (DTMF Tones) in PHP

---
+++ guides/voice/gather-dtmf-tones-guide/gather-example-step-2.1/example.5.x.php
@@ -0,0 +1,31 @@
+<?php
+// Create a route that will handle Twilio Gather verb action requests,
+// sent as an HTTP POST to /gather in our application
+require_once '/path/to/vendor/autoload.php';
+
+use Twilio\Twiml;
+
+// Use the Twilio PHP SDK to build an XML response
+$response = new Twiml();
+
+// If the user entered digits, process their request
+if (array_key_exists('Digits', $_POST)) {
+    switch ($_POST['Digits']) {
+    case 1:
+        $response->say('You selected sales. Good for you!');
+        break;
+    case 2:
+        $response->say('You need support. We will help!');
+        break;
+    default:
+        $response->say('Sorry, I don\'t understand that choice.');
+        $response->redirect('/voice');
+    }
+} else {
+    // If no input was sent, redirect to the /voice route
+    $response->redirect('/voice');
+}
+
+// Render the response as XML in reply to the webhook request
+header('Content-Type: text/xml');
+echo $response;
