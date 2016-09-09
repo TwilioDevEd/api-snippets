acb38cb5b0483266bee67f20b956abb7d10adfc8
mosampaio <mosampaio@gmail.com>
2016-09-09 10:19:18 -0300

    Record phone call in Java

---
+++ guides/voice/record-calls-guide/record-twiml/example.java
@@ -0,0 +1,29 @@
+@SuppressWarnings("serial")
+@WebServlet("/voice")
+public class RecordServlet extends HttpServlet {
+
+  // Handle HTTP POST to /record
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    // Create a TwiML builder object
+    TwiMLResponse twiml = new TwiMLResponse();
+    
+    TwiMLResponse twiml = new TwiMLResponse();
+    try {
+      // Use <Say> to give the caller some instructions
+      twiml.append(new Say("Hello. Please leave a message after the beep."));
+
+      // Use <Record> to record the caller's message
+      twiml.append(new Record());
+
+      // End the call with <Hangup>
+      twiml.append(new Hangup());
+    } catch (TwiMLException e) {
+      e.printStackTrace();
+    }
+    
+    // Render TwiML as XML
+    response.setContentType("text/xml");
+    response.getWriter().print(twiml.toXML());
+  }
+}
