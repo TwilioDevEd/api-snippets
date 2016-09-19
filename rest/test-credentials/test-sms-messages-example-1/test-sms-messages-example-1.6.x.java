53e3d9fc6a7e30ab2c375e867b87db8d1d70af20
Agustin Camino <agustin.camino@gmail.com>
2016-09-19 14:20:28 -0500

    Update examples to use the Messages resource

--- rest/test-credentials/test-sms-messages-example-1/test-sms-messages-example-1.6.x.java
+++ rest/test-credentials/test-sms-messages-example-1/test-sms-messages-example-1.6.x.java
@@ -1,32 +1,31 @@
 // Install the Java helper library from twilio.com/docs/java/install
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
-import com.twilio.sdk.resource.factory.SmsFactory;
-import com.twilio.sdk.resource.instance.Sms;
-import com.twilio.sdk.resource.list.SmsList;
-import java.util.ArrayList;
-import java.util.List;
+import com.twilio.sdk.resource.factory.MessageFactory;
+import com.twilio.sdk.resource.instance.Message;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-public class Example { 
+import java.util.ArrayList;
+import java.util.List;
 
-  // Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String AUTH_TOKEN = "your_auth_token";
+public class Example {
 
-  public static void main(String[] args) throws TwilioRestException {
-    TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
 
-    // Build a filter for the SmsList
-    List<NameValuePair> params = new ArrayList<NameValuePair>();
-    params.add(new BasicNameValuePair("Body", "All in the game, yo"));
-    params.add(new BasicNameValuePair("To", "+14108675309"));
-    params.add(new BasicNameValuePair("From", "+15005550006"));
-    
-    
-    SmsFactory smsFactory = client.getAccount().getSmsFactory();
-    Sms sms = smsFactory.create(params);
-    System.out.println(sms.getSid());
-  }
+    public static void main(String[] args) throws TwilioRestException {
+        TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+        // Build a filter for search
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("Body", "All in the game, yo"));
+        params.add(new BasicNameValuePair("To", "+14108675309"));
+        params.add(new BasicNameValuePair("From", "+15005550006"));
+
+        MessageFactory messageFactory = client.getAccount().getMessageFactory();
+        Message message = messageFactory.create(params);
+        System.out.println(message.getSid());
+    }
 }
--- rest/test-credentials/test-sms-messages-example-2/test-sms-messages-example-2.6.x.java
+++ rest/test-credentials/test-sms-messages-example-2/test-sms-messages-example-2.6.x.java
@@ -1,32 +1,34 @@
 // Install the Java helper library from twilio.com/docs/java/install
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
-import com.twilio.sdk.resource.factory.SmsFactory;
-import com.twilio.sdk.resource.instance.Sms;
-import com.twilio.sdk.resource.list.SmsList;
-import java.util.ArrayList;
-import java.util.List;
+import com.twilio.sdk.resource.factory.MessageFactory;
+import com.twilio.sdk.resource.instance.Message;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-public class Example { 
+import java.util.ArrayList;
+import java.util.List;
 
-  // Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String AUTH_TOKEN = "your_auth_token";
 
-  public static void main(String[] args) throws TwilioRestException {
-    TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+public class Example {
 
-    // Build a filter for the SmsList
-    List<NameValuePair> params = new ArrayList<NameValuePair>();
-    params.add(new BasicNameValuePair("Body", "Hey Mr Nugget, you the bomb!"));
-    params.add(new BasicNameValuePair("To", "+15005550009"));
-    params.add(new BasicNameValuePair("From", "+15005550006"));
-    
-    
-    SmsFactory smsFactory = client.getAccount().getSmsFactory();
-    Sms sms = smsFactory.create(params);
-    System.out.println(sms.getSid());
-  }
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) throws TwilioRestException {
+        TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+
+        // Build a filter for the SmsList
+        List<NameValuePair> params = new ArrayList<>();
+        params.add(new BasicNameValuePair("Body", "Hey Mr Nugget, you the bomb!"));
+        params.add(new BasicNameValuePair("To", "+15005550009"));
+        params.add(new BasicNameValuePair("From", "+15005550006"));
+
+        MessageFactory messageFactory = client.getAccount().getMessageFactory();
+        Message message = messageFactory.create(params);
+
+        System.out.println(message.getSid());
+    }
 }
+
