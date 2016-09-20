beaae0b7ccd098811f0233f32892e8f3aa6f26d2
Mario Celi <mceli@stackbuilders.com>
2016-09-20 08:40:20 -0500

    Fix failing PHP snippet

--- rest/test-credentials/test-sms-messages-example-3/test-sms-messages-example-3.4.x.php
+++ rest/test-credentials/test-sms-messages-example-3/test-sms-messages-example-3.4.x.php
@@ -3,13 +3,15 @@
 require_once '/path/to/twilio-php/Services/Twilio.php'; // Loads the library
 
 // Your Account Sid and Auth Token from twilio.com/user/account
-$accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; 
-$authToken = "your_auth_token"; 
+$accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$authToken = "your_auth_token";
 $client = new Services_Twilio($accountSid, $authToken);
 
 $message = $client->account->messages->create(
-    'To'   => '+15005550006',
-    'From' => '+14108675309'
+    array(
+        'To'   => '+15005550006',
+        'From' => '+14108675309'
+    )
 );
 
 echo $message->sid;
