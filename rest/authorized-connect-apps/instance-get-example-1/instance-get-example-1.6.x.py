f89d3572203bd7eb42c68a795006f43ff1aa0a61
Agustin Camino <agustin.camino@gmail.com>
2016-09-21 11:03:20 -0500

    Add 'authorized connect apps' examples

---
+++ rest/authorized-connect-apps/instance-get-example-1/instance-get-example-1.6.x.py
@@ -0,0 +1,13 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+authorized_app = client \
+        .authorized_connect_apps("CN47260e643654388faabe8aaa18ea6756") \
+        .fetch()
+
+print(authorized_app.connect_app_homepage_url)
