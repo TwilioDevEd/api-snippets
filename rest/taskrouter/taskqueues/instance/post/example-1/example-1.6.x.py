bcf9c9e53cf156805f5ec26d189a1af300f5b993
mosampaio <mosampaio@gmail.com>
2016-09-21 14:20:34 -0300

    Add next-gen python snippet for rest.taskrouter - taskqueues

---
+++ rest/taskrouter/taskqueues/instance/post/example-1/example-1.6.x.py
@@ -0,0 +1,24 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+workspace_sid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+taskqueue_sid = "WQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+client = Client(account_sid, auth_token)
+
+taskqueue = client.taskrouter.v1.workspaces(sid=workspace_sid) \
+        .task_queues(sid=taskqueue_sid) \
+        .update(target_workers='languages HAS "english"')
+
+print(taskqueue.target_workers)
+
+# alternatively
+taskqueue = client.taskrouter.v1.workspaces(sid=workspace_sid) \
+        .task_queues(sid=taskqueue_sid).fetch()
+
+taskqueue = taskqueue.update(target_workers='languages HAS "english"')
+
+print(taskqueue.target_workers)
