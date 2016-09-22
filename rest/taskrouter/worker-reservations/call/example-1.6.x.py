6bb61b7a069d5021dfe4e54b0155745141e0b3d4
mosampaio <mosampaio@gmail.com>
2016-09-22 13:55:16 -0300

    Add next-gen python snippet for rest.taskrouter - worker-reservations

---
+++ rest/taskrouter/worker-reservations/call/example-1.6.x.py
@@ -0,0 +1,25 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+workspace_sid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+worker_sid = "WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+reservation_sid = "WRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+client = Client(username=account_sid, password=auth_token)
+
+# call using a reservation
+reservation = client.taskrouter.v1.workspaces(sid=workspace_sid) \
+        .workers(sid=worker_sid).reservations(sid=reservation_sid) \
+        .update(
+          instruction='call',
+          call_from='+15558675309',
+          call_url='http://example.com/agent_answer',
+          call_status_callback_url='http://example.com/agent_answer_status_cb',
+          call_accept='true'
+)
+
+print(reservation.reservation_status)
+print(reservation.worker_name)
---
+++ rest/taskrouter/worker-reservations/list-get-example1/example-1.6.x.py
@@ -0,0 +1,17 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+workspace_sid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+worker_sid = "WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+client = Client(username=account_sid, password=auth_token)
+
+reservations = client.taskrouter.v1.workspaces(sid=workspace_sid) \
+        .workers(sid=worker_sid).reservations.list()
+
+for reservation in reservations:
+    print(reservation.reservation_status)
+    print(reservation.worker_name)
