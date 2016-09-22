389be1e34de53ec6afaa525e6deec23a59d43774
Agustin Camino <agustin.camino@gmail.com>
2016-09-22 11:43:10 -0500

    Add member examples

---
+++ rest/member/instance-post-example-1/instance-post-example-1.6.x.py
@@ -0,0 +1,14 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+member = client.queues("QU5ef8732a3c49700934481addd5ce1659") \
+               .members("Front") \
+               .update(url="http://demo.twilio.com/docs/voice.xml",
+                       method="POST")
+
+print(member.wait_time)
