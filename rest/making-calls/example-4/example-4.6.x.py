6fc04ccd335cfc20c26e244bb0fec56f30828882
Agustin Camino <agustin.camino@gmail.com>
2016-09-22 09:53:52 -0500

    Add 'making calls' examples

---
+++ rest/making-calls/example-4/example-4.6.x.py
@@ -0,0 +1,18 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+        to="+14155551212",
+        from_="+18668675309",
+        url="http://demo.twilio.com/docs/voice.xml",
+        method="GET",
+        status_callback="https://www.myapp.com/events",
+        status_callback_method="POST",
+        status_callback_event=["initiated", "ringing", "answered", "completed"])
+
+print(call.sid)
