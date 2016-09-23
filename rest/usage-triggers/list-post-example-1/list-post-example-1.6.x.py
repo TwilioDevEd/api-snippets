5d46b66ee204a57e357981a11252b0ef7b25481f
mosampaio <mosampaio@gmail.com>
2016-09-23 11:30:29 -0300

    Add usage-triggers examples

---
+++ rest/usage-triggers/list-post-example-1/list-post-example-1.6.x.py
@@ -0,0 +1,15 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+trigger = client.usage.triggers.create(
+    trigger_value="1000",
+    usage_category="sms",
+    callback_url="http://www.example.com/")
+
+print(trigger.sid)
