2d89a3f6b7db095fc218da8a96f864566ff5664b
Agustin Camino <agustin.camino@gmail.com>
2016-09-23 12:13:51 -0500

    Add sms examples

---
+++ rest/sms/instance-get-example-1/instance-get-example-1.6.x.py
@@ -0,0 +1,14 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+# Get an object from its sid. If you do not have a sid,
+# check out the list resource examples on this page
+sms = client.messages("SM800f449d0399ed014aae2bcc0cc2f2ec") \
+            .fetch()
+
+print(sms.body)
