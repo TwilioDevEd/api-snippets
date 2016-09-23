ebad929bdfe3d2b0ce8073f4daf43296aee2937d
Mario Celi <mcelicalderon@gmail.com>
2016-09-23 16:22:47 -0500

    Add lookups/ python next-gen snippets

--- lookups/lookup-get-addon-payfone-example-1/lookup-get-addon-payfone-1.5.x.py
+++ lookups/lookup-get-addon-payfone-example-1/lookup-get-addon-payfone-1.5.x.py
@@ -5,7 +5,7 @@ url = "https://lookups.twilio.com/v1/PhoneNumbers/+16502530000/?AddOns=payfone_t
 headers = {
     'accept': "application/json",
     'content-type': "application/json"
-    }
+}
 
 response = requests.get(url, headers=headers, auth=('ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'your_auth_token'))
 
