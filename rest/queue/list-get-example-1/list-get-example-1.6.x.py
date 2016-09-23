6fe27fd8d59b4daa415927f073d97ec71b136731
Agustin Camino <agustin.camino@gmail.com>
2016-09-23 08:44:50 -0500

    Add queue examples

---
+++ rest/queue/list-get-example-1/list-get-example-1.6.x.py
@@ -0,0 +1,11 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+# A list of queue objects with the properties described above
+for queue in client.queues.list():
+    print(queue.average_wait_time)
