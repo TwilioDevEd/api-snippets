1883b83b9b955cad7f29655533c62f3e0271e4c5
Jeffrey Linwood <jlinwood@gmail.com>
2016-09-29 14:15:05 -0500

    Handle participant connect and disconnect from rooms

--- video/rooms/handle-participant-events/handle-participant-events.m
+++ video/rooms/handle-participant-events/handle-participant-events.m
@@ -1,17 +1,8 @@
-#pragma mark - TWCConversationDelegate
-
-- (void)conversation:(TWCConversation *)conversation 
-didConnectParticipant:(TWCParticipant *)participant {
-	NSLog(@"Participant connected: %@", [participant identity]);
+#pragma mark - TVIParticipantDelegate methods
+- (void)participant:(TVIParticipant *)participant addedVideoTrack:(TVIVideoTrack *)videoTrack {
+    NSLog(@"Participant %@ added a video track",participant.identity);
 }
 
-- (void)conversation:(TWCConversation *)conversation 
-didFailToConnectParticipant:(TWCParticipant *)participant error:(NSError *)error {
-	NSLog(@"Participant failed to connect: %@ with error: %@", 
-		[participant identity], error.localizedDescription);
-}
-
-- (void)conversation:(TWCConversation *)conversation 
-didDisconnectParticipant:(TWCParticipant*)participant {
-	NSLog(@"Participant disconnected: %@", [participant identity]);
+- (void)participant:(TVIParticipant *)participant removedVideoTrack:(TVIVideoTrack *)videoTrack {
+    NSLog(@"Participant %@ removed a video track",participant.identity);
 }
