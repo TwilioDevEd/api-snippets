3c953914a3d1c46f52b23f2368aec193de40e4d9
Jeffrey Linwood <jlinwood@gmail.com>
2016-09-29 10:21:46 -0500

    Add Create a Room API Snippets for Video Rooms API

--- video/rooms/create-room/create-room.swift
+++ video/rooms/create-room/create-room.swift
@@ -1,21 +1,11 @@
-@IBAction func invite(sender: AnyObject) {
-    /* See the "Specify Local Media Constraints when Creating a Conversation"
-    guide for instructions on constructing LocalMedia */
-    outgoingInvite = self.client?.inviteToConversation("alice", localMedia:localMedia!,
-        { conversation, error in
-            if error == nil {
-                conversation!.delegate = self
-                print("Successfully connected to Conversation: " + conversation!.sid!)
-            } else {
-                print("Unable to connect to Conversation: " + error!.localizedDescription)
-            }
+@IBAction func createARoom(sender: AnyObject) {
+    let connectOptions = TVIConnectOptions.init(block: { (builder) in
+        builder.name = "my-room"
     })
+    room = videoClient.connectWithOptions(connectOptions, delegate: self)
 }
 
-// MARK: TWCConversationDelegate
-extension ViewController: TWCConversationDelegate {
-  func conversation(conversation: TWCConversation,
-      didConnectParticipant participant: TWCParticipant) {
-  	  print("A remote Participant connected: " + participant.sid!)
-  }
+// MARK: TVIRoomDelegate
+func didConnectToRoom(room: TVIRoom) {
+    print("Did connect to room")
 }
