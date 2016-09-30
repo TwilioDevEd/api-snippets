08449e4b58983dec24b87e6101a55492bec65e07
Jeffrey Linwood <jlinwood@gmail.com>
2016-09-30 14:00:51 -0500

    Update disconnect from room snippets

--- video/rooms/disconnect-from-room/disconnect-from-room.js
+++ video/rooms/disconnect-from-room/disconnect-from-room.js
@@ -1,7 +1,7 @@
 room.on('disconnected', function(room) {
   /* Release the local camera and microphone if we're no longer using this 
   LocalMedia instance in another Room */
-  room.localMedia.stop();
+  room.localParticipant.media.detach();
 });
 
 // To disconnect from a Room
