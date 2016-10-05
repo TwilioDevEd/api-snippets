b28ad8e95bc6acb76a36d09eb6b4a56aeb35c5ed
Jeffrey Linwood <jlinwood@gmail.com>
2016-10-05 10:12:56 -0500

    Update by adding Android snippets for Video Rooms

--- video/rooms/display-participants-video/display-participants-video.java
+++ video/rooms/display-participants-video/display-participants-video.java
@@ -1,51 +1,21 @@
-// First, we set a Participant Listener when a Participant first connects:
-private Conversation.Listener conversationListener() {
-  return new Conversation.Listener() {
+// First, we set a Media Listener when a Participant first connects:
+private Room.Listener roomListener() {
+  return new Room.Listener() {
     @Override
-    public void onParticipantConnected(Conversation conversation,
-                                       Participant participant) {
-      participant.setParticipantListener(participantListener());
+    public void onParticipantConnected(Room room, Participant participant) {
+      participant.getMedia().setListener(mediaListener());
     }
-
-    ...
-
+  };
 }
 
-/* In the Participant Listener, we can respond when the Participant adds a Video
+/* In the Media Listener, we can respond when the Participant adds a Video
 Track by rendering it on screen: */
-
-private Participant.Listener participantListener() {
-  return new Participant.Listener() {
-    @Override
-    public void onVideoTrackAdded(Conversation conversation,
-                                  Participant participant,
-                                  VideoTrack videoTrack) {
-      Log.i(TAG, "The remote Participant added a Video Track");
-
-      /* VideoViewRenderer is a renderer provided by Twilio that can
-      render a Video Track onto a ViewGroup */
-      participantVideoRenderer = new VideoViewRenderer(ConversationActivity.this,
-                                                       remoteParticipantViewGroup);
-      participantVideoRenderer.setObserver(new VideoRenderer.Observer() {
-
-        /* The Renderer Observer allows us to subscribe to important
-        events on the Video Track */
-        @Override
-        public void onFirstFrame() {
-          Log.i(TAG, "The first frame of video content arrived.");
-        }
-
-        @Override
-        public void onFrameDimensionsChanged(int width, int height) {
-          Log.i(TAG, "The dimensions of the video track changed to: " +
-                      width + "x" + height);
-        }
-
-      });
-      videoTrack.addRenderer(participantVideoRenderer);
-    }
-
-    ...
-
+private Media.Listener mediaListener() {
+  return new Media.Listener() {
+      @Override
+      public void onVideoTrackAdded(Media media, VideoTrack videoTrack) {
+        primaryVideoView.setMirror(false);
+        videoTrack.addRenderer(primaryVideoView);
+      }
   };
 }
--- video/rooms/specify-constraints/specify-constraints.java
+++ video/rooms/specify-constraints/specify-constraints.java
@@ -1,32 +1,4 @@
-// See the "Create a Conversation" guide for for details of the ConversationCallback
-...
-  outgoingInvite = conversationsClient
-    .sendConversationInvite(participants,
-                            setupLocalMedia(),
-                            new ConversationCallback() {
-  ...
-
-  };
-
-
-private CameraCapturer cameraCapturer;
-
-// Create LocalMedia with a camera track and no microphone track
-private LocalMedia setupLocalMedia() {
-  LocalMedia localMedia = new LocalMedia(localMediaListener());
-  LocalVideoTrack localVideoTrack = new LocalVideoTrack(cameraCapturer);
-  localMedia.addLocalVideoTrack(localVideoTrack);
-  return localMedia;
-}
-
-// LocalMedia listener
-private LocalMedia.Listener localMediaListener(){
-  return new LocalMedia.Listener() {
-    @Override
-    public void onLocalVideoTrackAdded(Conversation conversation,
-                                       LocalVideoTrack localVideoTrack) {
-      // Attach Camera track to renderer...
-    }
-    ...
-  };
+if (localMedia.getVideoTracks().size() == 0) {
+  CameraCapturer cameraCapturer = new CameraCapturer(this, CameraSource.FRONT_CAMERA, null);
+  LocalVideoTrack localVideoTrack = localMedia.addVideoTrack(true, cameraCapturer);
 }
