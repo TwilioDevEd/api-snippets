b86b234d5aafd2e3503a72929a20ad2cebf574b6
Kevin Whinnery <kevin.whinnery@gmail.com>
2016-10-11 13:51:49 -0500

    Update example-1.js

--- rest/messages/generate-twiml-sms/example-1.js
+++ rest/messages/generate-twiml-sms/example-1.js
@@ -1,17 +1,17 @@
-var http = require('http'),
-    express = require('express'),
-    twilio = require('twilio');
+var http = require('http');
+var express = require('express');
+var twilio = require('twilio');
 
 var app = express();
 
 app.post('/sms', function(req, res) {
-    var twilio = require('twilio');
-    var twiml = new twilio.TwimlResponse();
-    twiml.message('The Robots are coming! Head for the hills!');
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
+  var twilio = require('twilio');
+  var twiml = new twilio.TwimlResponse();
+  twiml.message('The Robots are coming! Head for the hills!');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
 });
 
 http.createServer(app).listen(1337, function () {
-    console.log("Express server listening on port 1337");
+  console.log("Express server listening on port 1337");
 });
