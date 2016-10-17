5786f1c4c89b8b9293482e33a71c9b9eaa3e2fd1
Mario Celi <mcelicalderon@gmail.com>
2016-10-17 16:38:13 -0500

    Add first rest/ ruby next-gen snippets

---
+++ rest/addresses/instance-get-example-1/instance-get-example-1.5.x.rb
@@ -0,0 +1,12 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Get an object from its sid. If you do not have a sid,
+# check out the list resource examples on this page
+@address = @client.account.addresses('AP2a0747eba6abf96b7e3c3ff0b4530f6e').fetch
+puts @address.customer_name
