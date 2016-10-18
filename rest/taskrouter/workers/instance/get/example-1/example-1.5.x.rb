6430c2e1cd067e24cc377fbb1b02a15d9642c4d6
mosampaio <mosampaio@gmail.com>
2016-10-18 13:23:39 -0300

    Add workers examples

---
+++ rest/taskrouter/workers/instance/get/example-1/example-1.5.x.rb
@@ -0,0 +1,14 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+workspace_sid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+worker_sid = 'WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+client = Twilio::REST::Client.new account_sid, auth_token
+
+worker = client.taskrouter.v1.workspaces(workspace_sid).workers(worker_sid).fetch
+puts worker.friendly_name
---
+++ rest/taskrouter/workers/list/get/example-2/example-2.5.x.rb
@@ -0,0 +1,24 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+workspace_sid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+client = Twilio::REST::Client.new account_sid, auth_token
+
+workspace = client.taskrouter.v1.workspaces(workspace_sid)
+
+workspace.workers.list(available: "1").each do |worker|
+  puts worker.friendly_name
+end
+
+workspace.workers.list(task_queue_sid: "WQf855e98ad280d0a0a325628e24ca9627", available: "1").each do |worker|
+  puts worker.friendly_name
+end
+
+workspace.workers.list(target_workers_expression: "type == 'leads'").each do |worker|
+  puts worker.friendly_name
+end
