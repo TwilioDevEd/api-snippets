a5b449d5c7654f05561b38ec24e57f542d6f0912
Twilio CMS User <kwhinnery@twilio.com>
2016-10-20 13:14:42 -0700

    CMS update of rest/messages/generate-twiml-sms by kwhinnery@twilio.com

--- rest/messages/generate-twiml-sms/example-1.4.x.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Get the PHP helper library from twilio.com/docs/php/install
-
-// this line loads the library 
-require('/path/to/twilio-php/Services/Twilio.php'); 
-
-$response = new Services_Twilio_Twiml;
-$response->message("The Robots are coming! Head for the hills!");
-print $response;
