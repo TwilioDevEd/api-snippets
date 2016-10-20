30c09f1b1c26fef9c8f207704c4b724619faafc7
Agustin Camino <agustin.camino@gmail.com>
2016-10-20 15:37:15 -0500

    Add voice examples

---
+++ rest/voice/outbound-calls/example-1/example-1.5.x.rb
@@ -0,0 +1,15 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from http://twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+call = @client.calls.create(
+  :url  => 'http://demo.twilio.com/docs/voice.xml',
+  :to   => '+14155551212',
+  :from => '+15017250604')
+
+puts call.to
---
+++ rest/voice/outbound-calls/example-4/example-4.5.x.rb
@@ -0,0 +1,19 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from https://twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+call = @client.calls.create(
+  :url                    => "http://demo.twilio.com/docs/voice.xml",
+  :to                     => '+14155551212',
+  :from                   => '+18668675309',
+  :method                 => 'GET',
+  :status_callback        => 'https://www.myapp.com/events',
+  :status_callback_method => 'POST',
+  :status_callback_event  => ['initiated', 'ringing', 'answered', 'completed'])
+
+puts call.start_time
