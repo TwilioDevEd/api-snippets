123f4ecb5b567f615de0a5b76760cd8317bd258a
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-21 18:19:28 -0500

    Add twilio-ruby next-gen snippets to rest/making-calls-sip

---
+++ rest/making-calls-sip/example-1/example-1.5.x.rb
@@ -0,0 +1,22 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+
+# Initialize Twilio Client
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Get the accounts with the given sid
+@accounts = @client.api.v2010.accounts(account_sid)
+
+# Create a call w/ sip alias
+@call = @accounts.calls.create(
+  url: 'http://www.example.com/sipdial.xml',
+  to: 'sip:kate@example.com',
+  from: 'Jack'
+)
+
+# Print call direction
+puts @call.direction
