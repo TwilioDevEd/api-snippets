1018bde35386193bc278cdc8d2c840536789458c
juancarlospaco <juancarlospaco@gmail.com>
2016-10-23 22:40:38 -0300

    Add Tasksqueues examples

---
+++ rest/taskrouter/taskqueues/instance/post/example-1/example-1.5.x.rb
@@ -0,0 +1,16 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+workspace_sid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+taskqueue_sid = 'WQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+
+
+client = Twilio::REST::Client.new account_sid, auth_token
+
+taskqueue = client.taskrouter.v1.workspaces(workspace_sid).task_queues(taskqueue_sid)
+taskqueue = taskqueue.update(target_workers: 'languages HAS "english"')
+puts taskqueue.target_workers
