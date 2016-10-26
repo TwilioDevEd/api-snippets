52ad25b1ff95b8dac9aeb3a6164cb112ccc04bd4
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-26 14:56:33 -0500

    Add twilio-ruby next-gen snippets to rest/short_codes

---
+++ rest/short-codes/instance-get-example-1/instance-get-example-1.5.x.rb
@@ -0,0 +1,17 @@
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Get an object from its sid. If you do not have a sid,
+# check out the list resource examples on this page
+@short_code = @client.account
+                     .short_codes('SC6b20cb705c1e8f00210049b20b70fce2').fetch
+
+# Print short code
+puts @short_code.short_code
