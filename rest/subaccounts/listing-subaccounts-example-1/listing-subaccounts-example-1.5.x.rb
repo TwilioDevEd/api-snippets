33d844681b3c12e34a43014c822ad668297a7caa
Agustin Camino <agustin.camino@gmail.com>
2016-10-26 14:48:45 -0500

    Add subaccounts examples

---
+++ rest/subaccounts/listing-subaccounts-example-1/listing-subaccounts-example-1.5.x.rb
@@ -0,0 +1,13 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from https://www.twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Get an object from its sid. If you do not have a sid,
+# check out the list resource examples on this page
+@account = @client.api.v2010.accounts("ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").fetch
+puts @account.status
