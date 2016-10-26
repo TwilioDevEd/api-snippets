c04d9e06125e54e9ef5ef699cb995a69dfd93c45
José Oliveros <jose.oliveros.1983@gmail.com>
2016-10-26 14:05:31 -0500

    Add twilio-ruby next-gen snippets to rest/recording

---
+++ rest/recording/instance-delete-examples/instance-delete-examples.5.x.rb
@@ -0,0 +1,16 @@
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Get specific recording with sid
+@recording = @client.account
+                    .recordings('RE557ce644e5ab84fa21cc21112e22c485').fetch
+
+# Delete recording
+@recording.delete
---
+++ rest/recording/list-get-example-2/list-get-example-2.5.x.rb
@@ -0,0 +1,16 @@
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Loop over recordings and print out a property for each one
+@client.account.recordings.list(
+  date_created: Time.new('2009-07-06')
+).each do |recording|
+  puts recording.call_sid
+end
