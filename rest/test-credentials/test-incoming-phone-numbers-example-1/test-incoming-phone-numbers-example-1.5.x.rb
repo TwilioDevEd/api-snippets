1251521574152394186c33281d1cda6bb8af4a2b
Agustin Camino <agustin.camino@gmail.com>
2016-10-26 10:34:49 -0500

    Add 'test credentials' examples

---
+++ rest/test-credentials/test-incoming-phone-numbers-example-1/test-incoming-phone-numbers-example-1.5.x.rb
@@ -0,0 +1,13 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from https://www.twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+number = @client.incoming_phone_numbers.create(
+  :voice_url    => 'http://demo.twilio.com/docs/voice.xml',
+  :phone_number => '+15005550006')
+puts number.sms_url
---
+++ rest/test-credentials/test-sms-messages-example-2/test-sms-messages-example-2.5.x.rb
@@ -0,0 +1,15 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems' # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from https://www.twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+message = @client.messages.create(
+  :body => 'Hey Mr Nugget, you the bomb!',
+  :to   => '+15005550009',
+  :from => '+15005550006')
+
+puts message.sid
