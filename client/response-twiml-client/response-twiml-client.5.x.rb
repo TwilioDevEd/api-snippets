5bb4975d31b8d7717ebc2009225acf2ba13883b3
Agustin Camino <agustin.camino@gmail.com>
2016-10-27 17:03:05 -0500

    Add client examples [ci skip]

---
+++ client/response-twiml-client/response-twiml-client.5.x.rb
@@ -0,0 +1,25 @@
+require 'twilio-ruby'
+require 'sinatra'
+
+post '/voice' do
+  twiml = Twilio::TwiML::Response.new do |r|
+    if params['To']
+      r.Dial callerId: '+15017250604' do |d|
+        # wrap the phone number or client name in the appropriate TwiML verb
+        # by checking if the number given has only digits and format symbols
+        if params['To'] =~ /^[\d\+\-\(\) ]+$/
+          d.Number params['To']
+        else
+          d.Client params['To']
+        end
+      end
+    else
+      r.Say "Thanks for calling!"
+    end
+  end
+
+  content_type 'text/xml'
+  twiml.text
+end
+
+# TODO: get '/token' do
