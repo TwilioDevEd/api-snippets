20d1884c22c477c928b36b4116edc378a70fccaa
hector <hector.ortega>
2016-10-28 13:27:11 -0500

    Revert changes to ruby 4.x snippets

--- guides/voice/record-calls-guide/record-outgoing-call/example-1.4.x.rb
+++ guides/voice/record-calls-guide/record-outgoing-call/example-1.4.x.rb
@@ -1,5 +1,5 @@
 # Get twilio-ruby from twilio.com/docs/ruby/install
-require 'rubygems' # This line not needed for ruby > 1.8
+require 'rubygems'          # This line not needed for ruby > 1.8
 require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/user/account
@@ -7,8 +7,8 @@ account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 auth_token = 'your_auth_token'
 @client = Twilio::REST::Client.new account_sid, auth_token
 
-call = @client.account.calls.create(url: 'http://demo.twilio.com/docs/voice.xml',
-                                    to: '+14155551212',
-                                    from: '  +15017250604',
-                                    record: true)
+call = @client.account.calls.create(:url => "http://demo.twilio.com/docs/voice.xml",
+    :to => "+14155551212",
+    :from => "+15017250604",
+    :record => true)
 puts call.to
