d0c2e6c16acbba4112ff428966f6a11683c262ba
hector <hector.ortega>
2016-10-28 22:33:10 -0500

    Add next gen lookups ruby snippets

---
+++ lookups/lookup-get-basic-example-1/lookup-get-basic-example-1.5.x.rb
@@ -0,0 +1,12 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::LookupsClient.new account_sid, auth_token
+
+number = @client.phone_numbers.get('+15108675309', type: 'carrier')
+
+puts number.carrier['type']
+puts number.carrier['name']
---
+++ lookups/lookup-get-basic-example-2/lookup-get-basic-example-2.5.x.rb
@@ -0,0 +1,16 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::LookupsClient.new(account_sid, auth_token)
+
+number = @client.phone_numbers.get(
+  '(510) 867-5309',
+  country_code: 'US',
+  type: 'carrier'
+)
+
+puts number.carrier['type']
+puts number.carrier['name']
