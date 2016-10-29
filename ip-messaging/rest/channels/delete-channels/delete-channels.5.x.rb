98d0908442a2bcd06adea07f883bdd228bbec81d
hector <hector.ortega>
2016-10-28 21:02:08 -0500

    Add next gen ip-messaging ruby snippets

---
+++ ip-messaging/rest/channels/delete-channels/delete-channels.5.x.rb
@@ -0,0 +1,13 @@
+require "http"
+require 'twilio-ruby'
+
+#initialize the client
+account_sid = 'ACCOUNT_SID'
+auth_token = 'AUTH_TOKEN'
+ip_messaging_client = Twilio::REST::IpMessagingClient.new(account_sid, auth_token)
+
+#Delete the channel
+service = ip_messaging_client.services.get('SERVICE_SID')
+channel = service.channels.create()
+response = channel.delete()
+puts response
