60dc2a627aa11dd3f9b91fb0d8368aa01ecae4f3
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-02 09:43:07 -0600

    Add next-gen twilio-node snippet for rest/applications

---
+++ rest/applications/instance-get-example-1/instance-get-example-1.3.x.js
@@ -0,0 +1,10 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.applications('AP2a0747eba6abf96b7e3c3ff0b4530f6e')
+  .fetch()
+  .then((app) => console.log(app.smsUrl));
