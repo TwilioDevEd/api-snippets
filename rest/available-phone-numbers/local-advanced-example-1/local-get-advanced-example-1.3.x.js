a4eed59b88fdea2320e31fbef0f875c9cdd1be39
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-02 10:56:16 -0600

    Add next-gen twilio-node snippet for rest/available-phone-numbers

---
+++ rest/available-phone-numbers/local-advanced-example-1/local-get-advanced-example-1.3.x.js
@@ -0,0 +1,21 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.availablePhoneNumbers('US').local
+  .list({
+    nearLatLong: '37.840699,-122.461853',
+    distance: '50',
+    contains: '555',
+    inRegion: 'CA',
+  })
+  .then((data) => {
+    const number = data[0];
+    return client.incomingPhoneNumbers.create({
+      phoneNumber: number.phoneNumber,
+    });
+  })
+  .then((purchasedNumber) => console.log(purchasedNumber.sid));
