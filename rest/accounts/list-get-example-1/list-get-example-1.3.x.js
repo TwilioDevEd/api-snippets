075b79121515923a965ad4b4317e9836b7550ec4
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-01 18:42:45 -0600

    Add next-gen twilio-node snippet for rest/accounts

---
+++ rest/accounts/list-get-example-1/list-get-example-1.3.x.js
@@ -0,0 +1,11 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.accounts.list()
+  .then((data) => {
+    return data.forEach((account) => console.log(account.dateCreated));
+  });
