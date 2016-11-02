4685c9bb66f5cde02f8b267d5459cfd62a5dbdd0
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-02 17:08:23 -0600

    Add next-gen twilio-node snippet for rest/call

---
+++ rest/call/list-get-example-2/list-get-example-2.3.x.js
@@ -0,0 +1,12 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.calls
+  .each(
+    {status: 'completed', startTime: '2009-07-06'},
+    (call) => console.log(call.direction)
+  );
