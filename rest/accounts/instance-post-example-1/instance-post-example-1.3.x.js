e6387eec7c992be8c01a4c673c357925a0207058
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-01 18:42:45 -0600

    Add next-gen twilio-node snippet for rest/accounts

---
+++ rest/accounts/instance-post-example-1/instance-post-example-1.3.x.js
@@ -0,0 +1,9 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.accounts('ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+  .update({status: 'suspended'})
+  .then((account) => console.log(account.dateCreated));
