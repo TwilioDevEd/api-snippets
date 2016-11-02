a0bf53dc2a0b58acd44d01b39133445c98d0ea61
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-02 17:57:19 -0600

    Add next-gen twilio-node snippet for rest/call-feedback

---
+++ rest/call-feedback/instance-post-example-1/instance-post-example-1.3.x.js
@@ -0,0 +1,15 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+
+client.calls('CAe03b7cd806070d1f32bdb7f1046a41c0')
+  .feedback()
+  .create({
+    qualityScore: '3',
+    issue: 'imperfect-audio',
+  })
+  .then((feedback) => console.log(feedback.dateCreated));
