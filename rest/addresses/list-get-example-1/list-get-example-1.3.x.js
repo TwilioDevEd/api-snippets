9ed57b0e0e9340f0aff7386eaf93091e06c79d35
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-01 19:02:48 -0600

    Add next-gen twilio-node snippet for rest/addresses

---
+++ rest/addresses/list-get-example-1/list-get-example-1.3.x.js
@@ -0,0 +1,8 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.addresses.each((address) => console.log(address.friendlyName));
