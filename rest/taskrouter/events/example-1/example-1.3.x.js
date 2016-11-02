8fce568546227613f26d5d6934cdb017e13ee7e6
Hector Ortega <hortega@stackbuilders.com>
2016-11-02 13:21:05 -0500

    Add next-gen node snippets to taskrouter/events and jwts

---
+++ rest/taskrouter/events/example-1/example-1.3.x.js
@@ -0,0 +1,14 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const client = require('twilio')(accountSid, authToken);
+
+client.taskrouter.v1
+  .workspaces(workspaceSid)
+  .events
+  .list()
+  .then((data) => {
+    data.events.forEach((event) => console.log(event.event_type));
+  });
---
+++ rest/taskrouter/jwts/worker/example-1/example-1.3.x.js
@@ -0,0 +1,23 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const twilio = require('twilio');
+
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const workerSid = 'WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+
+const capability = new twilio.jwt.TaskRouterWorkerCapability(accountSid,
+  authToken,
+  workspaceSid,
+  workerSid);
+
+capability.allowActivityUpdates();
+capability.allowReservationUpdates();
+
+const token = capability.generate();
+
+// By default, tokens are good for one hour.
+// Override this default timeout by specifiying a new value (in seconds).
+// For example, to generate a token good for 8 hours:
+token = capability.generate(28800);  // 60 * 60 * 8
