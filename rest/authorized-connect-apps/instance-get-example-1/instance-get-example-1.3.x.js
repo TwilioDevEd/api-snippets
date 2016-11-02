365a84c5373bfdf5e140991a85c0885feac37f8c
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-02 10:12:53 -0600

    Add next-gen twilio-node snippet for rest/authorized-connect-apps

---
+++ rest/authorized-connect-apps/instance-get-example-1/instance-get-example-1.3.x.js
@@ -0,0 +1,10 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.authorizedConnectApps('CN47260e643654388faabe8aaa18ea6756')
+  .fetch()
+  .then((authorizedApp) => console.log(authorizedApp.connectAppHomepageUrl));
