83e6eb6af9cae2b3a591f96cafcf53c18ca560b3
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 13:24:22 -0600

    Correct rest/incoming-phone-numbers/list-post-example-1

--- rest/incoming-phone-numbers/list-post-example-1/list-post-example-1.3.x.js
+++ rest/incoming-phone-numbers/list-post-example-1/list-post-example-1.3.x.js
@@ -1,14 +1,13 @@
 // Download the Node helper library from twilio.com/docs/node/install
-// These vars are your accountSid and authToken from twilio.com/user/account
-var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-var authToken = "your_auth_token";
-var client = require('twilio')(accountSid, authToken);
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
 
 client.incomingPhoneNumbers.create({
-    friendlyName: "My Company Line",
-    voiceUrl: "http://demo.twilio.com/docs/voice.xml",
-    phoneNumber: "+15105647903",
-    voiceMethod: "GET"
-}, function(err, number) {
-    console.log(number.sid);
-});
+  friendlyName: 'My Company Line',
+  voiceUrl: 'http://demo.twilio.com/docs/voice.xml',
+  phoneNumber: '+15105647903',
+  voiceMethod: 'GET',
+})
+.then((number) => console.log(number.sid));
