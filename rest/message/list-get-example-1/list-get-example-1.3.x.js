fce4346724b88bff1f82689f4f48cd227c9aafbf
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 15:01:37 -0600

    Update  next-gen node snippet rest/message

--- rest/message/list-get-example-1/list-get-example-1.3.x.js
+++ rest/message/list-get-example-1/list-get-example-1.3.x.js
@@ -1,11 +1,7 @@
 // Download the Node helper library from twilio.com/docs/node/install
-// These vars are your accountSid and authToken from https://www.twilio.com/console
-var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-var authToken = "your_auth_token";
-var client = require('twilio')(accountSid, authToken);
+// These consts are your accountSid and authToken from https://www.twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
 
-client.messages.list(function(err, data) {
-    data.messages.forEach(function(message) {
-        console.log(message.body);
-    });
-});
+client.messages.each((message) => console.log(message.body));
