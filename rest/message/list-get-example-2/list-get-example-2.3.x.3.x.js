96bec6d150065d8f0302efb2a217429527595a23
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 12:33:02 -0600

    Update accountsid url

--- rest/message/list-get-example-2/list-get-example-2.3.x.3.x.js
+++ rest/message/list-get-example-2/list-get-example-2.3.x.3.x.js
@@ -1,5 +1,5 @@
 // Download the Node helper library from twilio.com/docs/node/install
-// These vars are your accountSid and authToken from twilio.com/user/account
+// These vars are your accountSid and authToken from https://www.twilio.com/console
 var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 var authToken = "your_auth_token";
 
