e1ae00212fe4721a90d370288371c8dab72546d0
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 08:44:13 -0600

    Add next-gen twilio-node snippet for rest/making-calls-sip

---
+++ rest/making-calls-sip/example-2/example-2.3.x.js
@@ -0,0 +1,15 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.calls.create({
+  url: 'http://www.example.com/sipdial.xml',
+  to: 'sip:kate@example.com',
+  from: 'Jack',
+  sipAuthPassword: 'secret',
+  sipAuthUsername: 'jack',
+})
+.then((call) => process.stdout.write(call.sid));
