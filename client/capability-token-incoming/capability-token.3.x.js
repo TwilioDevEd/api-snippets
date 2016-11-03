48bd4623c66778421743429cf0b5989b6e2f6d09
juancarlospaco <juancarlospaco@gmail.com>
2016-11-02 22:59:33 -0300

    Add client/capability-token snippets for node.

---
+++ client/capability-token-incoming/capability-token.3.x.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const twilio = require('twilio');
+
+const app = express();
+
+app.get('/token', function(req, res) {
+  // put your Twilio API credentials here
+  const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+  const authToken = 'your_auth_token';
+
+  let capability = new twilio.jwt.Capability(accountSid, authToken);
+  capability.allowClientIncoming('jenny');
+  const token = capability.generate();
+
+  res.set('Content-Type', 'application/jwt');
+  res.send(token);
+});
+
+app.post('/voice', function(req, res) {
+    // TODO: Create TwiML response
+});
+
+http.createServer(app).listen(1337, '127.0.0.1');
+console.log('Twilio Client app server running at http://127.0.0.1:1337/token/');
