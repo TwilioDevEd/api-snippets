a2898920952ba9d543a094712a7c0e35714755c6
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 08:48:25 -0600

    Add next-gen twilio-node snippet for rest/media

---
+++ rest/media/instance-delete-example-1/instance-delete-example-1.3.x.js
@@ -0,0 +1,15 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+const mediaSid = 'ME557ce644e5ab84fa21cc21112e22c485';
+
+client.messages('MM800f449d0399ed014aae2bcc0cc2f2ec').media(mediaSid)
+  .remove()
+  .then(() => {
+    console.log(`Sid ${mediaSid} deleted successfully.`);
+  })
+  .catch((err) => console.log(err));
---
+++ rest/media/list-get-example-1/list-get-example-1.3.x.js
@@ -0,0 +1,9 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages('MM800f449d0399ed014aae2bcc0cc2f2ec')
+  .media.each((media) => console.log(media.contentType));
