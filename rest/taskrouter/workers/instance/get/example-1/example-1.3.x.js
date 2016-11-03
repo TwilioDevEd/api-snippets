59b0deb757f2d87ffb34ef5f984322b52648e52f
Hector Ortega <hortega@stackbuilders.com>
2016-11-03 09:04:58 -0500

    Add next-gen node snippets to taskrouter/workers

---
+++ rest/taskrouter/workers/instance/get/example-1/example-1.3.x.js
@@ -0,0 +1,13 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from https://www.twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const workerSid = 'WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const client = require('twilio')(accountSid, authToken);
+
+client.taskrouter.v1
+  .workspaces(workspaceSid)
+  .workers(workerSid)
+  .fetch()
+  .then((worker) => console.log(worker.friendly_name));
