81f1c8e13e41ce037836a053c4a304a3229a4538
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 08:28:36 -0600

    Add next-gen twilio-node snippet for rest/making-calls

---
+++ rest/making-calls/example-3/example-3.3.x.js
@@ -0,0 +1,15 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.calls.create({
+  url: 'http://demo.twilio.com/docs/voice.xml',
+  to: '+14155551212',
+  sendDigits: '1234#',
+  from: '+18668675309',
+  method: 'GET',
+})
+.then((call) => process.stdout.write(call.sid));
