f74b5a12acf3df090cc0a9927f598cb5e77800d3
Hector Ortega <hortega@stackbuilders.com>
2016-11-03 01:31:11 -0500

    Add next-gen node snippets to taskrouter/tasks

---
+++ rest/taskrouter/tasks/instance/post/example-1/example-1.3.x.js
@@ -0,0 +1,29 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const taskSid = 'WTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const client = require('twilio')(accountSid, authToken);
+
+// update a task's attributes
+client.taskrouter.v1
+  .workspaces(workspaceSid)
+  .tasks(taskSid)
+  .update({
+    attributes: '{"type":"support"}',
+  })
+  .then((task) => console.log(task.attributes));
+
+// cancel a task
+client.taskrouter.v1
+  .workspaces(workspaceSid)
+  .tasks(taskSid)
+  .update({
+    assignmentStatus: 'canceled',
+    reason: 'waiting too long',
+  })
+  .then((task) => {
+    console.log(task.assignment_status);
+    console.log(task.reason);
+  });
