a46fc51999797f3322c0b019f7e8b7744d6b625f
Hector Ortega <hortega@stackbuilders.com>
2016-11-03 02:39:19 -0500

    Add next-gen node snippets to taskrouter/twiml

---
+++ rest/taskrouter/twiml/example3/example/example.3.x.js
@@ -0,0 +1,28 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from https://www.twilio.com/console
+const twilio = require('twilio');
+const express = require('express');
+const app = express();
+
+app.post('/enqueue_call', function(request, response) {
+    const resp = new twilio.TwimlResponse();
+
+    const arr = {account_number: '12345abcdef'};
+    const json = JSON.stringify(arr);
+
+    resp.enqueue(
+      {workflowSid: 'WW0123456789abcdef0123456789abcdef'},
+      (node) => {
+        node.task(json, {
+            priority: '5',
+            timeout: '200',
+        });
+      }
+    );
+
+    response.setHeader('Content-Type', 'application/xml');
+    response.write(resp.toString());
+    response.end();
+});
+
+app.listen(8080);
