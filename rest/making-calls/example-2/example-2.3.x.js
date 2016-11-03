fa2e9269f411cd3e8ab69dd3aba583e225a0a714
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 14:07:23 -0600

    Update next-gen twilio-node rest comments to point to twilio.com/console

--- rest/making-calls/example-2/example-2.3.x.js
+++ rest/making-calls/example-2/example-2.3.x.js
@@ -1,6 +1,6 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These identifiers are your accountSid and authToken from
-// twilio.com/user/account
+// https://www.twilio.com/console
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
