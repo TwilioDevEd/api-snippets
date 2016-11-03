cffe52a2f4d9d1c127c141c27d74d15621a2492a
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 14:10:14 -0600

    Lint twilio-node next-gen snippets

--- rest/addresses/list-post-example-1/list-post-example-1.3.x.js
+++ rest/addresses/list-post-example-1/list-post-example-1.3.x.js
@@ -6,12 +6,12 @@ const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
 
 client.addresses.create({
-      friendlyName: 'Billing - Customer 123',
-      customerName: 'Customer 123',
-      street: '1 Hasselhoff Lane',
-      city: 'Berlin',
-      region: 'Berlin',
-      postalCode: '10875',
-      isoCountry: 'DE',
+    friendlyName: 'Billing - Customer 123',
+    customerName: 'Customer 123',
+    street: '1 Hasselhoff Lane',
+    city: 'Berlin',
+    region: 'Berlin',
+    postalCode: '10875',
+    isoCountry: 'DE',
   })
   .then((address) => console.log(address.customerName));
--- rest/member/instance-post-example-1/instance-post-example-1.3.x.js
+++ rest/member/instance-post-example-1/instance-post-example-1.3.x.js
@@ -1,11 +1,12 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These identifiers are your accountSid and authToken from
-// twilio.com/user/account
+// https://www.twilio.com/console
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
 
-client.queues('QU5ef8732a3c49700934481addd5ce1659').members('Front')
+client.queues('QU5ef8732a3c49700934481addd5ce1659')
+  .members('Front')
   .update({
     url: 'http://demo.twilio.com/docs/voice.xml',
     method: 'POST',
