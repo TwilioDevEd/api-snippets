9cbcbd85c583bf1bdb2f14510816dc953fc4a5a6
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 15:47:53 -0600

    Add next-gen twilio-node snippet for rest/sending-messages

---
+++ rest/sending-messages/example-1/example-1.3.x.js
@@ -0,0 +1,13 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+  body: 'Jenny please?! I love you <3',
+  to: '+15558675309',
+  from: '+14158141829',
+  mediaUrl: 'http://www.example.com/hearts.png',
+})
+.then((message) => process.stdout.write(message.sid));
