a1d3fea2b789de0450d11ec7525a899b9ffef258
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 15:29:42 -0600

    Add next-gen twilio-node snippet for rest/queue

---
+++ rest/queue/instance-get-example-1/instance-get-example-1.3.x.js
@@ -0,0 +1,8 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.queues('QU5ef8732a3c49700934481addd5ce1659')
+  .fetch((queue) => console.log(queue));
---
+++ rest/queue/instance-post-example-1/instance-post-example-1.3.x.js
@@ -0,0 +1,9 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.queues('QU32a3c49700934481addd5ce1659f04d2')
+  .update({maxSize: '123'})
+  .then((queue) => console.log(queue.averageWaitTime));
