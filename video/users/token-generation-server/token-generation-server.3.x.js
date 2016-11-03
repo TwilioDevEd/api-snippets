276df25ab7911966faf698b54dc0df2a9faf2ab3
juancarlospaco <juancarlospaco@gmail.com>
2016-11-03 16:54:41 -0300

    Fix doble indentations, Use ES6 Default Parameters for port.

--- video/users/token-generation-server/token-generation-server.3.x.js
+++ video/users/token-generation-server/token-generation-server.3.x.js
@@ -13,34 +13,34 @@ let app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/token', function(request, response) {
-    let identity = randomUsername();
+  let identity = randomUsername();
 
-    // Create an access token which we will sign and return to the client,
-    // containing the grant we just created
-    let token = new AccessToken(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_API_KEY,
-        process.env.TWILIO_API_SECRET
-    );
+  // Create an access token which we will sign and return to the client,
+  // containing the grant we just created
+  let token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET
+  );
 
-    // Assign the generated identity to the token.
-    token.identity = identity;
+  // Assign the generated identity to the token.
+  token.identity = identity;
 
-    // Grant access to Video.
-    let grant = new VideoGrant();
-    grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
-    token.addGrant(grant);
+  // Grant access to Video.
+  let grant = new VideoGrant();
+  grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+  token.addGrant(grant);
 
-    // Serialize the token to a JWT string and include it in a JSON response
-    response.send({
-        identity: identity,
-        token: token.toJwt(),
-    });
+  // Serialize the token to a JWT string and include it in a JSON response
+  response.send({
+    identity: identity,
+    token: token.toJwt(),
+  });
 });
 
 // Create http server and run it
 const server = http.createServer(app);
-const port = process.env.PORT || 3000;
-server.listen(port, function() {
+const port = process.env.PORT;
+server.listen(port = 3000, function() {
     console.log('Express server running on *:' + port);
 });
