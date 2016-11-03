41f038f7cc0cd24ad5ebcf6bdf32a57a572d55b4
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 08:54:41 -0600

    Add next-gen twilio-node snippet for rest/member

---
+++ rest/member/list-get-example-1/list-get-example-1.3.x.js
@@ -0,0 +1,9 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These identifiers are your accountSid and authToken from
+// twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.queues('QU5ef8732a3c49700934481addd5ce1659').members
+  .each((member) => console.log(member.position));
