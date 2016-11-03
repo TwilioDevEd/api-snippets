74eedec463b8ce888c0073c54249f7031ecfff9c
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-03 14:10:14 -0600

    Lint twilio-node next-gen snippets

--- rest/call-feedback/summary-list-post-example-1/summary-list-post-example-1.3.x.js
+++ rest/call-feedback/summary-list-post-example-1/summary-list-post-example-1.3.x.js
@@ -6,9 +6,9 @@ const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
 
 client.calls.feedbackSummaries.create({
-    startDate: '2014-06-01',
-    endDate: '2014-06-30',
-    includeSubaccounts: 'true',
-    statusCallback: 'http://www.example.com/feedback',
+  startDate: '2014-06-01',
+  endDate: '2014-06-30',
+  includeSubaccounts: 'true',
+  statusCallback: 'http://www.example.com/feedback',
 })
 .then((summary) => console.log(summary.sid));
