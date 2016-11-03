710df2e45ef82237b809b4153461c43d800b68d9
Hector Ortega <hortega@stackbuilders.com>
2016-11-02 22:08:50 -0500

    Add next-gen node snippets to taskrouter/taskqueues

---
+++ rest/taskrouter/taskqueues/instance/delete/example-1/example-1.3.x.js
@@ -0,0 +1,12 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from twilio.com/user/account
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const taskQueueSid = 'WQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const client = require('twilio')(accountSid, authToken);
+
+client.taskrouter.v1
+  .workspaces(workspaceSid)
+  .taskQueues(taskQueueSid)
+  .remove();
