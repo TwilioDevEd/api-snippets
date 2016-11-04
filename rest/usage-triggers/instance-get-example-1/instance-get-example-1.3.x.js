22a751402a00fa7fbeed48a634c321baad8df79d
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-04 13:16:24 -0600

    Add next-gen twilio-node snippet for rest/usage-triggers

---
+++ rest/usage-triggers/instance-get-example-1/instance-get-example-1.3.x.js
@@ -0,0 +1,9 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from https://www.twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+client.usage.triggers('UT33c6aeeba34e48f38d6899ea5b765ad4')
+  .fetch()
+  .then((trigger) => trigger.currentValue);
---
+++ rest/usage-triggers/list-get-example-1/list-get-example-1.3.x.js
@@ -0,0 +1,13 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These consts are your accountSid and authToken from https://www.twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const client = require('twilio')(accountSid, authToken);
+
+const filterOpts = {
+  recurring: 'daily',
+  usageCategory: 'calls',
+};
+
+client.usage.triggers
+  .each(filterOpts, (trigger) => console.log(trigger.currentValue));
