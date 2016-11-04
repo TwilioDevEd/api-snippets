c3896a501e30c3f5cf4e040177fb145db0c10ea7
Hector Ortega <hortega@stackbuilders.com>
2016-11-03 23:03:05 -0500

    Reinstate double quotes to v2 node snippets

--- rest/taskrouter/activities/instance/post/example-1/example-1.3.x.js
+++ rest/taskrouter/activities/instance/post/example-1/example-1.3.x.js
@@ -1,10 +1,10 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These consts are your accountSid and authToken from https://www.twilio.com/console
-const accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-const authToken = "your_auth_token";
-const workspaceSid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-const activitySid = "WAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-const client = require("twilio")(accountSid, authToken);
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const activitySid = 'WAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const client = require('twilio')(accountSid, authToken);
 
  client.taskrouter.v1
   .workspaces(workspaceSid)
