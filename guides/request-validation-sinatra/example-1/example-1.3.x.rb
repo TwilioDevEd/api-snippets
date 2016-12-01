5d2b21559c0b000f650191088d7b503c92e31745
juancarlospaco <juancarlospaco@gmail.com>
2016-12-01 12:52:13 -0300

    Add comments, minor.

--- guides/request-validation-sinatra/example-1/example-1.3.x.rb
+++ guides/request-validation-sinatra/example-1/example-1.3.x.rb
@@ -1,3 +1,10 @@
+# You can find your Twilio Auth Token here: https://www.twilio.com/console
+# Set at runtime as follows:
+# $ TWILIO_AUTH_TOKEN="XXXXXXXXXXXXXXXXXXX" ruby index.rb
+#
+# This will not work unless you export the TWILIO_AUTH_TOKEN environment variable.
+#
+
 require 'sinatra'
 require 'twilio-ruby'
 require 'rack'
