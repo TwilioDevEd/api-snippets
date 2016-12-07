fcdfb7d2d9b7f759ba5a76397d2c9d9966613f37
juancarlospaco <juancarlospaco@gmail.com>
2016-12-07 18:08:41 -0300

    Add next gen lib of nodejs examples for get-voice-country

--- pricing/get-voice-country/get-voice-country.3.x.js
+++ pricing/get-voice-country/get-voice-country.3.x.js
@@ -1,14 +1,17 @@
-// Download the Node helper library from twilio.com/docs/node/install
-// These are your accountSid and authToken from https://www.twilio.com/console
+// Get your Account SID and Auth Token from twilio.com/console
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 
-const PricingClient = require('twilio').PricingClient;
-const client = new PricingClient(accountSid, authToken);
+// Get twilio-node from twilio.com/docs/libraries/node
+const client = require('twilio')(accountSid, authToken).pricing;
 
-client.voice.countries('EE').get((err, country) => {
-    const cpList = country.inboundCallPrices;
-    for (let i = 0; i < cpList.length; i++) {
-        console.log(cpList[i].numberType + ' ' + cpList[i].currentPrice);
-    }
-});
+client.voice.countries('EE').fetch()
+  .then((country) => {
+    country.inboundCallPrices.forEach((price) => {
+        console.log(`${price.number_type} ${price.current_price}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    throw error;
+  });
