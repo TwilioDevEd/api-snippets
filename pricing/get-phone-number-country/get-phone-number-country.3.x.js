485add2a4a924e4e1908266643240052a3eb58f3
juancarlospaco <juancarlospaco@gmail.com>
2016-12-07 17:50:09 -0300

    Use next gen node js lib to get phone calls price on the get-phone-number-country examples of pricing

--- pricing/get-phone-number-country/get-phone-number-country.3.x.js
+++ pricing/get-phone-number-country/get-phone-number-country.3.x.js
@@ -1,13 +1,17 @@
-// Download the Node helper library from twilio.com/docs/node/install
-// These are your accountSid and authToken from https://www.twilio.com/console
+// Get your Account SID and Auth Token from twilio.com/console
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 
-const PricingClient = require('twilio').PricingClient;
-const client = new PricingClient(accountSid, authToken);
+// Get twilio-node from twilio.com/docs/libraries/node
+const client = require('twilio')(accountSid, authToken).pricing;
 
-client.phoneNumbers.countries('US').get((error, country) => {
-	country.phoneNumberPrices.forEach((phonePrices) => {
-        console.log(phonePrices.numberType + ' ' + phonePrices.currentPrice);
-	});
-});
+client.voice.countries('US').fetch()
+  .then((country) => {
+    country.inboundCallPrices.forEach((price) => {
+        console.log(`Base:${price.base_price} Current:${price.current_price}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    throw error;
+  });
