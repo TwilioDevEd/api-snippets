22f7be57f0f232669098a02d288720993559d650
Andrew T. Baker <abaker@twilio.com>
2016-12-09 13:25:29 -0800

    Update Python 5.x code sample

--- rest/subaccounts/exchanging-numbers-example-1/exchanging-numbers-example-1.5.x.py
+++ rest/subaccounts/exchanging-numbers-example-1/exchanging-numbers-example-1.5.x.py
@@ -1,16 +1,15 @@
 # Download the Python helper library from twilio.com/docs/python/install
 from twilio.rest import TwilioRestClient
 
-# Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+# Use the Account Sid and Auth Token of the subaccount which currently
+# owns the number you want to transfer (twilio.com/console/account/subaccounts)
+account_sid = "ACyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
 auth_token  = "your_auth_token"
 client = TwilioRestClient(account_sid, auth_token)
 
 # In the case you want to transfer numbers between subaccounts, you need to 
-# know three things - the account SID of the phone number's current owner, 
-# the account SID of the account you'd like to transfer the number to, and 
-# the SID of the phone number you'd like to transfer
-current_owner_sid = "ACyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
+# know two things: the account SID of the account you'd like to transfer the
+# number to, and the SID of the phone number you'd like to transfer
 new_owner_sid = "ACzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
 number_sid = "PNyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy"
 
