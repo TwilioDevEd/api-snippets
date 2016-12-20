a8be1f1bb61246fb7fdd44fa6bb47d2f36b99429
Hector Ortega <hector.oj@gmail.com>
2016-12-20 13:42:18 -0500

    Add C# next-gen snippets for rest/authorized-connect-apps

---
+++ rest/authorized-connect-apps/instance-get-example-1/instance-get-example-1.5.x.cs
@@ -0,0 +1,19 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var authorizedApp = AuthorizedConnectAppResource.Fetch(
+      "CN47260e643654388faabe8aaa18ea6756");
+
+    Console.WriteLine(authorizedApp.ConnectAppHomepageUrl);
+  }
+}
