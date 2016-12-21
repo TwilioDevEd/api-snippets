da357be488a6b4e3ec28310a751cb498f5746f86
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-21 14:27:11 -0600

    Add csharp 5.x rest/taskrouter/twiml

---
+++ rest/taskrouter/twiml/example2/example/example.5.x.cs
@@ -0,0 +1,24 @@
+// Download the twilio-csharp library from
+// https://www.twilio.com/docs/libraries/csharp#installation
+using System;
+using System.Net;
+using Twilio.TwiML;
+
+class Example
+{
+    public static HttpListenerResponse SendResponse(HttpListenerContext ctx)
+    {
+        HttpListenerRequest request = ctx.Request;
+        HttpListenerResponse response = ctx.Response;
+
+        response.StatusCode = (int)HttpStatusCode.OK;
+        response.ContentType = "application/xml";
+
+        var twiml = new VoiceResponse();
+        twiml.Enqueue("{\"account_number\":\"12345abcdef\"}",
+            workflowSid: "WW0123456789abcdef0123456789abcdef");
+
+        response.StatusDescription = twiml.ToString();
+        return response;
+    }
+}
