0211b95c8102a7344ff993a9bd5c3c9652409f29
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-21 15:27:02 -0600

    Add csharp 5.x rest/taskrouter/worker-reservations

---
+++ rest/taskrouter/worker-reservations/list-get-example1/example-1.5.x.cs
@@ -0,0 +1,26 @@
+// Download the twilio-csharp library from
+// https://www.twilio.com/docs/libraries/csharp#installation
+using System;
+using System.Net;
+using Twilio;
+using Twilio.Rest.Taskrouter.V1.Workspace.Worker;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Auth Token at twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        const string workspaceSid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string WorkerSid = "WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        TwilioClient.Init(accountSid, authToken);
+
+        var reservations = ReservationResource.Read(workspaceSid, WorkerSid);
+
+        foreach(ReservationResource reservation in reservations) {
+            Console.WriteLine(reservation.ReservationStatus);
+            Console.WriteLine(reservation.WorkerName);
+        }
+    }
+}
