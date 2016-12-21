1da726de779f34da057d23bea89cf86cb19431d9
Hector Ortega <hector.oj@gmail.com>
2016-12-21 14:53:45 -0500

    Add C# next-gen snippets for rest/connect-apps

---
+++ rest/connect-apps/instance-get-example-1/instance-get-example-1.5.x.cs
@@ -0,0 +1,18 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var connectApp = ConnectAppResource.Fetch("CNb989fdd207b04d16aee578018ef5fd93");
+
+    Console.WriteLine(connectApp.HomepageUrl);
+  }
+}
