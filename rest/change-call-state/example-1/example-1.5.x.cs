ae2fcb18c9bb5e3352bd631150ad3f9ad86a4611
Hector Ortega <hector.oj@gmail.com>
2016-12-21 11:18:03 -0500

    Add C# next-gen snippets for rest/change-call-state

---
+++ rest/change-call-state/example-1/example-1.5.x.cs
@@ -0,0 +1,21 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Http;
+using Twilio.Rest.Api.V2010.Account;
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var url = new Uri("http://demo.twilio.com/docs/voice.xml");
+    var method = HttpMethod.Post;
+    CallResource.Update("CAe1644a7eed5088b159577c5802d8be38",
+                        url: url,
+                        method: method);
+  }
+}
