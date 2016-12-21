0d61166cc24171bc54bbbe1719517a777aa9d39d
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-20 23:30:04 -0600

    Add csharp 5.x snippets rest/taskrouter/taskqueues

---
+++ rest/taskrouter/taskqueues/list/get/example-1/example-1.5.x.cs
@@ -0,0 +1,23 @@
+// Download the twilio-csharp library from
+// https://www.twilio.com/docs/libraries/csharp#installation
+using System;
+using Twilio;
+using Twilio.Rest.Taskrouter.V1.Workspace;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Auth Token at twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        const string workspaceSid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        TwilioClient.Init(accountSid, authToken);
+
+        var taskQueues = TaskQueueResource.Read(workspaceSid);
+        
+        foreach(TaskQueueResource taskQueue in taskQueues) {
+            Console.WriteLine(taskQueue.FriendlyName);
+        }
+    }
+}
