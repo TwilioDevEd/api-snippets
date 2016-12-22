be68623695d6365eaaf76b3c609a348d8518f20a
Hector Ortega <hector.oj@gmail.com>
2016-12-22 08:18:58 -0500

    Add C# next-gen snippets for rest/keys

---
+++ rest/keys/instance-get-example/instance-get-example.5.x.cs
@@ -0,0 +1,18 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var key = KeyResource.Fetch("SK2a0747eba6abf96b7e3c3ff0b4530f6e");
+
+    Console.WriteLine(key.FriendlyName);
+  }
+}
