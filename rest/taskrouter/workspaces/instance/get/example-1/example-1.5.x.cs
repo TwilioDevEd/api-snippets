83ef44ca5e360ce1f8ba56b250df1224832e08f2
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-22 15:16:28 -0600

    Add next-gen 5.x c-sharp snippets rest/taskrouter/workspaces

---
+++ rest/taskrouter/workspaces/instance/get/example-1/example-1.5.x.cs
@@ -0,0 +1,20 @@
+// Download the twilio-csharp library from
+// https://www.twilio.com/docs/libraries/csharp#installation
+using System;
+using Twilio;
+using Twilio.Rest.Taskrouter.V1;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Auth Token at twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        const string workspaceSid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        TwilioClient.Init(accountSid, authToken);
+
+        var workspace = WorkspaceResource.Fetch(workspaceSid);
+        Console.WriteLine(workspace.FriendlyName);
+    }
+}
