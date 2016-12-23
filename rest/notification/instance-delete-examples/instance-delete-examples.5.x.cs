6dcfe6c09f452b9868c8e367205ac0772e4b3bbc
Hector Ortega <hector.oj@gmail.com>
2016-12-23 11:04:14 -0500

    Add C# next-gen snippets for rest/notification

---
+++ rest/notification/instance-delete-examples/instance-delete-examples.5.x.cs
@@ -0,0 +1,17 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    NotificationResource.Delete("NO5a7a84730f529f0a76b3e30c01315d1a");
+  }
+}
---
+++ rest/notification/list-get-example-1/list-get-example-1.5.x.cs
@@ -0,0 +1,22 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var notifications = NotificationResource.Read();
+
+    foreach (var notification in notifications)
+    {
+      Console.WriteLine(notification.MessageText);
+    }
+  }
+}
