92c6d4d3226c698a1b294f02ac56ca540e0f96cb
Hector Ortega <hector.oj@gmail.com>
2016-12-23 11:04:29 -0500

    Add C# next-gen snippets for rest/outgoing-caller-ids

---
+++ rest/outgoing-caller-ids/instance-delete/instance-delete.5.x.cs
@@ -0,0 +1,17 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    OutgoingCallerIdResource.Delete("PNe536d32a3c49700934481addd5ce1659");
+  }
+}
---
+++ rest/outgoing-caller-ids/instance-get-example-1/instance-get-example-1.5.x.cs
@@ -0,0 +1,19 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var callerId = OutgoingCallerIdResource.Fetch("PNe905d7e6b410746a0fb08c57e5a186f3");
+
+    Console.WriteLine(callerId.PhoneNumber);
+  }
+}
