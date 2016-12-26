609e99bad5bac58692a044867b497fb7edc74749
Hector Ortega <hector.oj@gmail.com>
2016-12-26 08:42:54 -0500

    Add C# next-gen snippets for rest/sending-messages

---
+++ rest/sending-messages/example-1/example-1.5.x.cs
@@ -0,0 +1,27 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+using System.Collections.Generic;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var mediaUrl = new List<Uri>() {
+      new Uri("http://www.example.com/hearts.png")
+    };
+    var message = MessageResource.Create(new PhoneNumber("+14158141829"),
+                                         from: new PhoneNumber("+15558675309"),
+                                         body: "Jenny please?! I love you <3",
+                                         mediaUrl: mediaUrl);
+
+    Console.WriteLine(message.Sid);
+  }
+}
