aca1a647d1f0c5c56807386f78346617844fb455
Hector Ortega <hector.oj@gmail.com>
2016-12-26 08:25:18 -0500

    Add C# next-gen snippets for rest/queue

---
+++ rest/queue/instance-post-example-1/instance-post-example-1.5.x.cs
@@ -0,0 +1,18 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    QueueResource.Update("QU32a3c49700934481addd5ce1659f04d2",
+                         maxSize: 123);
+  }
+}
