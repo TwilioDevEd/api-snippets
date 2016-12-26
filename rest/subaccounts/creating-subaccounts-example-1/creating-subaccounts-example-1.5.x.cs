978ab84ad408690ba6434a147d80f37c1ca9da70
Hector Ortega <hector.oj@gmail.com>
2016-12-26 15:03:08 -0500

    Add C# next-gen snippets for rest/subaccounts

---
+++ rest/subaccounts/creating-subaccounts-example-1/creating-subaccounts-example-1.5.x.cs
@@ -0,0 +1,20 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+
+    var account = AccountResource.Create("Submarine");
+
+    Console.WriteLine(account.Sid);
+  }
+}
