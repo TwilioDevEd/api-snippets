075483453b51965796af22ba1e88ff2dc5c4d9b9
Hector Ortega <hector.oj@gmail.com>
2016-12-27 08:23:23 -0500

    Add C# next-gen snippets for rest/usage-records

---
+++ rest/usage-records/list-get-example-5/list-get-example-5.5.x.cs
@@ -0,0 +1,22 @@
+// Download the twilio-csharp library from twilio.com/docs/csharp/install
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account.Usage;
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    TwilioClient.Init(accountSid, authToken);
+
+    var records = RecordResource.Read();
+
+    foreach (var record in records)
+    {
+      Console.WriteLine(record.Count);
+    }
+  }
+}
