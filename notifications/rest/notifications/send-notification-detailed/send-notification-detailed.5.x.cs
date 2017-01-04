414d671416d7aca51753ccd2055b2c687d031675
Agustin Camino <agustin.camino@gmail.com>
2017-01-04 17:43:39 -0500

    Add notifications examples for C# next-gen

---
+++ notifications/rest/notifications/send-notification-detailed/send-notification-detailed.5.x.cs
@@ -0,0 +1,28 @@
+// Download the twilio-csharp library from twilio.com/docs/libraries/csharp
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Notify.V1.Service;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        const string serviceSid = "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var notification = NotificationResource.Create(
+            serviceSid,
+            title: "Generic loooooooong title for all Bindings",
+            body: "This is the body for all Bindings",
+            identity: new List<string> { "Bob" },
+            gcm: "{\"notification\":{\"tag\":\"MyTag\"}}",
+            apn: "{\"aps\":{\"title\":\"Short title for Watch.\"}}");
+
+        Console.WriteLine(notification.Sid);
+    }
+}
