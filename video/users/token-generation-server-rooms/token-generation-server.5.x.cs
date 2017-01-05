b1cfa6eb2d77af2f212a72f9c14bb7c2d868c0fc
Agustin Camino <agustin.camino@gmail.com>
2017-01-05 15:27:20 -0500

    Add video examples for C# next-gen

---
+++ video/users/token-generation-server-rooms/token-generation-server.5.x.cs
@@ -0,0 +1,38 @@
+using System.Configuration;
+using System.Web.Mvc;
+using Faker;
+using Twilio.JWT;
+
+namespace VideoQuickstart.Controllers
+{
+    public class TokenController : Controller
+    {
+        // GET: Token
+        public ActionResult Index(string device)
+        {
+            // Load Twilio configuration from Web.config
+            var accountSid = ConfigurationManager.AppSettings["TwilioAccountSid"];
+            var apiKey = ConfigurationManager.AppSettings["TwilioApiKey"];
+            var apiSecret = ConfigurationManager.AppSettings["TwilioApiSecret"];
+
+            // Create a random identity for the client
+            var identity = Internet.UserName();
+
+            // Create an Access Token generator
+            var token = new AccessToken(accountSid, apiKey, apiSecret)
+            {
+                Identity = identity
+            };
+
+            // Create a video grant for this token
+            var grant = new ConversationsGrant();
+            token.AddGrant(grant);
+
+            return Json(new
+            {
+                identity,
+                token = token.ToJwt()
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
