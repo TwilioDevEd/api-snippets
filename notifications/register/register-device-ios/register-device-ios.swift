b0619728b34ada97d28c223ecd19d883f37b4e40
Tatjana Mihnovits <tmihnovits@2273-tmihnovits.home>
2017-01-18 19:40:07 +0000

    Improvements to curl examples. Mainly substituting 'XXX' type values with  attributes in brakets like {Service Instance SID}

--- notifications/register/register-device-ios/register-device-ios.swift
+++ notifications/register/register-device-ios/register-device-ios.swift
@@ -1,6 +1,6 @@
 func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
   // Override point for customization after application launch.
-  let settings = UIUserNotificationSettings(forTypes: .Alert, categories: nil)
+  let settings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
   UIApplication.sharedApplication().registerUserNotificationSettings(settings)
   UIApplication.sharedApplication().registerForRemoteNotifications()
   return true
