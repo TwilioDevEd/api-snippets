478886d2b06fdd4c5a06428ada0d752ea65d29f2
José Oliveros <jose.oliveros.1983@gmail.com>
2017-02-09 12:12:56 -0600

    Add facebook messenger notification credential snippet, other edits - remove unnecessary options in send-notification snippet

--- notifications/rest/credentials/create-fcm-credential/create-fcm-credential.3.x.js
+++ notifications/rest/credentials/create-fcm-credential/create-fcm-credential.3.x.js
@@ -5,14 +5,15 @@ const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
 
-const notificationOpts = {
-  identity: '00000001', // We recommend using a GUID or other anonymized identifier for Identity.
-  body: 'Knok-Knok! This is your first Notify SMS',
+const credentialOpts = {
+  friendlyName: 'MyGCMCredential',
+  sandbox: true,
+  secret: 'fcm_secret',
+  type: 'fcm',
 };
 
 client.notify.v1
   .services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-  .notifications
-  .create(notificationOpts)
-  .then(notification => console.log(notification.sid))
+  .credentials.create(credentialOpts)
+  .then(credential => console.log(credential.sid))
   .catch(error => console.log(error));
