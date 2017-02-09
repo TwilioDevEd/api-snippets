5cb0a24395d564b95238eed32aa13bc135579878
Samuel Mendes <samuel.mendes@gmail.com>
2017-02-09 18:36:22 +0100

    Add Python examples for Notify

---
+++ notifications/register/send-notification-1/send-notification-1.6.x.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Install the Python helper library from twilio.com/docs/python/install
+
+from twilio.rest import Client
+
+ACCOUNT_SID = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+AUTH_TOKEN = 'your_auth_token'
+
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+notification = client.notify.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+    .notifications.create(
+        tag='new_user',
+        body='Hello New Users',
+    )
+print(notification.sid)
