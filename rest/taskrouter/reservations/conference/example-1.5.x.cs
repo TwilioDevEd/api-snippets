ff1cc3172f7db23b27fff6883dae199bc3750727
Kris Gutta <kgutta@twilio.com>
2017-02-10 13:52:35 -0800

    Added code samples for conference instruction

---
+++ rest/taskrouter/reservations/conference/example-1.5.x.cs
@@ -0,0 +1,31 @@
+// Download the twilio-csharp library from
+// https://www.twilio.com/docs/libraries/csharp#installation
+using System;
+using Twilio;
+using Twilio.Rest.Taskrouter.V1.Workspace.Task;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Find your Account Sid and Auth Token at twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        const string workspaceSid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string taskSid = "WTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string reservationSid = "WRXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        // Update a Reservation with a Conference instruction
+        var reservation = ReservationResource.Fetch(
+                              workspaceSid, taskSid, reservationSid);
+
+        Console.WriteLine(reservation.ReservationStatus);
+        Console.WriteLine(reservation.WorkerName);
+
+        ReservationResource.Update(
+            workspaceSid, taskSid, reservationSid, instruction: "conference",
+            dequeueFrom: "+18001231234");
+    }
+}
