c231db44805ec594ba52affb7e696d6ec2d03218
Twilio CMS User <jlinwood@twilio.com>
2017-02-27 11:35:46 -0800

    CMS update of notifications/register/register-device-android by jlinwood@twilio.com

--- notifications/register/register-device-android/register-device-android.java
+++ notifications/register/register-device-android/register-device-android.java
@@ -1,8 +1,6 @@
 public class RegistrationIntentService extends IntentService {  
   @Override
   protected void onHandleIntent(Intent intent) {
-    InstanceID instanceID = InstanceID.getInstance(this);
-    String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
-        GoogleCloudMessaging.INSTANCE_SID_SCOPE, null);
+    String token = FirebaseInstanceId.getInstance().getToken();
   }
 }
