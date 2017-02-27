f12eef2ffdcccb96de7110f624fd8127ec053138
Twilio CMS User <jlinwood@twilio.com>
2017-02-27 11:43:42 -0800

    CMS update of notifications/register/create-binding-android-2 by jlinwood@twilio.com

--- notifications/register/create-binding-android-2/create-binding-android-2.java
+++ notifications/register/create-binding-android-2/create-binding-android-2.java
@@ -1,9 +1,18 @@
 public class RegistrationIntentService extends IntentService {
   private BindingResource bindingResource;
+  
+  @Override
+  public void onCreate(){
+      super.onCreate();
+      Retrofit retrofit = new Retrofit.Builder()
+              .baseUrl(AppConstants.BASE_URL).addConverterFactory(JacksonConverterFactory.create())
+              .build();
+      bindingResource = retrofit.create(BindingResource.class);
+  }
+  
   private void sendRegistrationToServer(String identity, String endpoint, String token) throws IOException {
-    BindingRegistration bindingRegistration = new BindingRegistration(
-      identity, endpoint, token, "gcm");
-    Call<Void> call = bindingResource.createBinding(bindingRegistration);
+    Binding binding = new Binding(identity, endpoint, token, "fcm");
+    Call<Void> call = bindingResource.createBinding(binding);
     Response<Void> response = call.execute();
   }
 }
