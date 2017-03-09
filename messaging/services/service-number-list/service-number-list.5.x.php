9d00657853dce263c74e318218de85cba49f8bbe
Twilio CMS User <dprothero@twilio.com>
2017-03-09 09:05:00 -0800

    CMS update of messaging/services/service-number-list by dprothero@twilio.com

---
+++ messaging/services/service-number-list/service-number-list.5.x.php
@@ -0,0 +1,16 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once '/path/to/vendor/autoload.php'; // Loads the library
+
+use Twilio\Rest\Client;
+
+$account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+$auth_token = 'your_auth_token'
+$twilio = new Twilio($account_sid, $auth_token);
+
+$phoneNumbers = $twilio->messaging->v1->services("MG2172dd2db502e20dd981ef0d67850e1a")
+                                      ->phoneNumbers->read();
+
+foreach ($phoneNumbers as $phoneNumber) {
+    print($phoneNumber->phoneNumber);
+}
