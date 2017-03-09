13372e384fd45a1dd1e52450b6d5f9121342d5a9
Samuel Mendes <samuel.mendes@gmail.com>
2017-03-09 14:39:02 +0100

    Add snippet for creating access token with voice grant (Python and Node.js)

---
+++ rest/access-tokens/voice-example/voice-example.6.x.py
@@ -0,0 +1,22 @@
+from twilio.jwt.access_token import AccessToken, VoiceGrant
+
+# required for all twilio access tokens
+account_sid = 'ACxxxxxxxxxxxx'
+api_key = 'SKxxxxxxxxxxxx'
+api_secret = 'xxxxxxxxxxxxxx'
+
+# required for Voice grant
+outgoing_application_sid = 'APxxxxxxxxxxxxx'
+identity = 'user'
+
+# Create access token with credentials
+token = AccessToken(account_sid, api_key, api_secret, identity)
+
+# Create a Voice grant and add to token
+voice_grant = VoiceGrant(
+    outgoing_application_sid=outgoing_application_sid
+)
+token.add_grant(voice_grant)
+
+# Return token info as JSON
+print(token.to_jwt())
