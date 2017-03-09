5f533a88e01dcf249ee9957d0be78f2bd8bdf236
Elaine Tsai <et.elainetsai@gmail.com>
2017-03-09 07:36:34 -0800

    php & curl examples w/ output

---
+++ messaging/services/service-create/service-create-1.5.x.php
@@ -0,0 +1,11 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once '/path/to/vendor/autoload.php'; // Loads the library
+
+use Twilio\Rest\Client;
+
+$account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+$auth_token = 'your_auth_token'
+$twilio = new Twilio($account_sid, $auth_token);
+
+$service = $twilio->messaging->v1->services->create("My Company");
