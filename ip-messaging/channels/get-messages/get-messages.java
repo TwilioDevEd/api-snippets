e48179404b0fd4f78d050c1e8f2ab9a1a0f7b8d7
Twilio CMS User <jlinwood@twilio.com>
2017-03-15 11:26:16 -0700

    CMS update of ip-messaging/channels/get-messages by jlinwood@twilio.com

--- ip-messaging/channels/get-messages/get-messages.java
+++ ip-messaging/channels/get-messages/get-messages.java
@@ -1,1 +1,8 @@
-channel.getMessages();
+  channel.getMessages().getLastMessages(100, new CallbackListener<List<Message>>() {
+    @Override
+    public void onSuccess(List<Message> messages) {
+      for (Message message : messages) {
+        Log.d(TAG, "Message Body: " + message.getMessageBody());
+      }
+    }
+  });
