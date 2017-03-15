7df1c4a75c5c438d5ea86440d7ad54acdb3b6f1c
Evan Cummack <cummack@twilio.com>
2017-03-15 15:11:09 -0700

    added Java helper examples for Sim resource

---
+++ wireless/sims/instance-get-example-1/instance-get-example-1.java
@@ -0,0 +1,19 @@
+// Install the Java preview helper library from:
+// https://www.twilio.com/docs/libraries/java#accessing-preview-twilio-features
+
+import com.twilio.Twilio;
+import com.twilio.rest.preview.wireless.*;
+
+public class Example {
+
+        // Find your Account Sid and Token at twilio.com/user/account
+        public static final String ACCOUNT_SID = "AC8caa2afb9d5279926619c458dc7099a8";
+        public static final String AUTH_TOKEN = "your_auth_token";
+
+        public static void main(String[] args) {
+            Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+            Sim sim = Sim.fetcher("DEd3f4ec6c85ded1a5f64f10afc067895c").fetch();
+            System.out.println(sim.getStatus());
+        }
+
+}
---
+++ wireless/sims/list-example-1/list-example-1.java
@@ -0,0 +1,22 @@
+// Install the Java preview helper library from:
+// https://www.twilio.com/docs/libraries/java#accessing-preview-twilio-features
+
+import com.twilio.Twilio;
+import com.twilio.base.ResourceSet;
+import com.twilio.rest.preview.wireless.*;
+
+public class Example {
+
+    // Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "AC8caa2afb9d5279926619c458dc7099a8";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        ResourceSet<Sim> sims = Sim.reader().read();
+        for (Sim sim : sims) {
+            System.out.println(sim.getSid() + ": " + sim.getStatus());
+        }
+    }
+
+}
