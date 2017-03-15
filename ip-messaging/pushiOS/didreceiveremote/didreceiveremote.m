296ddad66eb0e93f309181005a8c32c778ff869f
Twilio CMS User <jlinwood@twilio.com>
2017-03-15 08:54:44 -0700

    CMS update of ip-messaging/pushiOS/didreceiveremote by jlinwood@twilio.com

--- ip-messaging/pushiOS/didreceiveremote/didreceiveremote.m
+++ ip-messaging/pushiOS/didreceiveremote/didreceiveremote.m
@@ -1,9 +1,9 @@
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
     if(application.applicationState != UIApplicationStateActive) { // Only address notifications received when not active
-        // If your application supports multiple types of push notifications, you may wish to limit which ones you send to the TwilioIPMessagingClient here
-            if (self.ipMessagingClient && userInfo) {
-                // If your reference to the IP Messaging client exists and is initialized, send the notification to it
-                [self.ipMessagingClient handleNotification:userInfo];
+        // If your application supports multiple types of push notifications, you may wish to limit which ones you send to the TwilioChatClient here
+            if (self.chatClient && userInfo) {
+                // If your reference to the Chat client exists and is initialized, send the notification to it
+                [self.chatClient handleNotification:userInfo];
             } else {
                 // Store the notification for later handling
                 self.receivedNotification = userInfo;
