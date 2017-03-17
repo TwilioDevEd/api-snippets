2297eb4fc112e4845eaf1ff656721e618c4c9412
Twilio CMS User <jlinwood@twilio.com>
2017-03-17 09:19:48 -0700

    CMS update of ip-messaging/channels/detect-channel-states by jlinwood@twilio.com

--- ip-messaging/channels/detect-channel-states/detect-channel-states.java
+++ ip-messaging/channels/detect-channel-states/detect-channel-states.java
@@ -13,32 +13,3 @@ public void onMessageDelete(Message message) {
   Log.d(TAG, "Message deleted");
 }
 
-@Override
-public void onMemberJoin(Member member) {
-  Log.d(TAG, "Member joined: " + member.getUserInfo().getIdentity());
-}
-
-@Override
-public void onMemberChange(Member member) {
-  Log.d(TAG, "Member changed: " + member.getUserInfo().getIdentity());
-}
-
-@Override
-public void onMemberDelete(Member member) {
-  Log.d(TAG, "Member deleted: " + member.getUserInfo().getIdentity());
-}
-
-@Override
-public void onTypingStarted(Member member) {
-  Log.d(TAG, "Started Typing: " + member.getUserInfo().getIdentity());
-}
-
-@Override
-public void onTypingEnded(Member member) {
-  Log.d(TAG, "Ended Typing: " + member.getUserInfo().getIdentity());
-}
-
-@Override
-public void onSynchronizationChange(Channel channel) {
-
-}
