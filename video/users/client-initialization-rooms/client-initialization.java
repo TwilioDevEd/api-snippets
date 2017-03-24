00157cf3640e13bf8a5919e17d4b927e5ca382dd
Twilio CMS User <ktoraskar@twilio.com>
2017-03-24 11:42:03 -0700

    CMS update of video/users/client-initialization-rooms by ktoraskar@twilio.com

--- video/users/client-initialization-rooms/client-initialization.java
+++
@@ -1,6 +0,0 @@
-// Connect to a room
-ConnectOptions connectOptions = new ConnectOptions.Builder(accessToken)
-                .roomName("my-room")
-                .localMedia(localMedia)
-                .build();
-Room room = VideoClient.connect(context, connectOptions, roomListener());
