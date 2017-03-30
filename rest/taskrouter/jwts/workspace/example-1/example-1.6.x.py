7c287953f979540c031bb01e13c374349d7cc182
Samuel Mendes <samuel.mendes@gmail.com>
2017-03-30 17:41:07 +0200

    Fix TaskRouter capabilities are in twilio.jwt.taskrouter.capabilities Rename generate_token to to_jwt to_jwt ttl must be a named arg

--- rest/taskrouter/jwts/workspace/example-1/example-1.6.x.py
+++ rest/taskrouter/jwts/workspace/example-1/example-1.6.x.py
@@ -1,26 +1,26 @@
 # Download the Python helper library from twilio.com/docs/python/install
-from twilio.task_router import TaskRouterWorkspaceCapability
+from twilio.jwt.taskrouter.capabilities import WorkspaceCapabilityToken
 
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 auth_token = "your_auth_token"
 workspace_sid = "WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-capability = TaskRouterWorkspaceCapability(
+capability = WorkspaceCapabilityToken(
     account_sid=account_sid, auth_token=auth_token, workspace_sid=workspace_sid
 )
 
 capability.allow_fetch_subresources()
-capability.allow_updates_subresources()
+capability.allow_update_subresources()
 capability.allow_delete_subresources()
 
-token = capability.generate_token()
+token = capability.to_jwt()
 
 # By default, tokens are good for one hour.
 # Override this default timeout by specifiying a new value (in seconds).
 # For example, to generate a token good for 8 hours:
 
 # 60 * 60 * 8 = 28800
-token = capability.generate_token(28800)
+token = capability.to_jwt(ttl=28800)
 
 print(token)
