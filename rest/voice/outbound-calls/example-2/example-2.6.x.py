bc6211de68c7b7216ca019f9f4b922cc8853aaa2
Samuel Mendes <samuel.mendes@gmail.com>
2017-03-30 17:51:35 +0200

    Fix client.account is accessible through client.api.v2010.account now

--- rest/voice/outbound-calls/example-2/example-2.6.x.py
+++ rest/voice/outbound-calls/example-2/example-2.6.x.py
@@ -7,7 +7,7 @@ auth_token = "your_auth_token"
 
 client = Client(account_sid, auth_token)
 
-call = client.account.calls.create(url="http://demo.twilio.com/docs/voice.xml",
+call = client.api.v2010.account.calls.create(url="http://demo.twilio.com/docs/voice.xml",
                                    to="client:tommy",
                                    from_="+15017250604")
 
