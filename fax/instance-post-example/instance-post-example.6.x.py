ac65290fc9f1a877300e723f113817cd8598ded2
Hector Ortega <hector.oj@gmail.com>
2017-04-06 12:48:30 -0500

    Add list-get fax samples

---
+++ fax/instance-post-example/instance-post-example.6.x.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+# put your own credentials here
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+
+client = Client(account_sid, auth_token)
+
+fax = client.fax.v1
+    .faxes(sid="FXc37d4ed12343484f901bde9a758408bc")
+    .update(status="canceled")
+
+print(fax.status)
