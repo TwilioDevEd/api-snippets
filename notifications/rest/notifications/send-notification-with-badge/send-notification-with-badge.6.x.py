641e55add36e5fa718207e2a536c238f747b08d5
Hector Ortega <hector.oj@gmail.com>
2017-04-18 09:34:42 -0500

    Add send-notification-with-badge samples

---
+++ notifications/rest/notifications/send-notification-with-badge/send-notification-with-badge.6.x.py
@@ -0,0 +1,16 @@
+# NOTE: This example uses the next generation Twilio helper library - for more
+# information on how to download and install this version, visit
+# https://www.twilio.com/docs/libraries/python
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+token = "your_auth_token"
+client = Client(account, token)
+
+notification = client.notify.services("ISxxx")\
+    .notifications.create(identity="00000001",
+                          apn='{"aps" : { "alert": {"title":"Bob alert","body" :'\
+                           '"Bob, you just received a badge"}, "badge" : 1 }}')
+
+print(notification)
