b64e6426e48d5156b6cfe3cb1de8765d31fca492
Twilio CMS User <szolotarjov@twilio.com>
2017-04-28 02:37:41 -0700

    CMS update of ip-messaging/rest/members/retrieve-member by szolotarjov@twilio.com

--- ip-messaging/rest/members/retrieve-member/retrieve-member.5.x.cs
+++ ip-messaging/rest/members/retrieve-member/retrieve-member.5.x.cs
@@ -1,1 +1,24 @@
-Your code here
+// Download the twilio-csharp library from twilio.com/docs/libraries/csharp
+using System;
+using Twilio;
+using Twilio.Rest.Chat.V2.Service.Channel;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        const string accountSid = "accountSid";
+        const string authToken = "authToken";
+        const string serviceSid = "serviceSid";
+        const string channelSid = "channelSid";
+        const string memberSid = "memberSid";
+
+        TwilioClient.Init(accountSid, authToken);
+
+        // Retrieve the member
+        var member = MemberResource.Fetch(serviceSid, channelSid, memberSid);
+        
+        Console.WriteLine(member.Identity);
+    }
+}
