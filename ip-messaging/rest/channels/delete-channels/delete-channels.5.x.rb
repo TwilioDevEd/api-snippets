226319eef645586c5ec476344014283930475795
Andres Jaan Tack <ajtack@twilio.com>
2017-04-28 13:37:54 +0300

    Update snippets to use REST v2 and twilio-ruby v5.0.0rc

--- ip-messaging/rest/channels/delete-channels/delete-channels.5.x.rb
+++ ip-messaging/rest/channels/delete-channels/delete-channels.5.x.rb
@@ -1,13 +1,13 @@
-require 'http'
 require 'twilio-ruby'
 
 # Get your Account SID and Auth Token from twilio.com/console
 account_sid = 'ACCOUNT_SID'
 auth_token = 'AUTH_TOKEN'
+service_sid = 'SERVICE_SID'
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-# Delete the channel
-service = @client.ip_messaging.v1.services('SERVICE_SID').fetch
-channel = service.channels.create
-response = channel.delete
+# Delete a particular 
+channel_to_delete = 'CHANNEL_SID'
+service = @client.chat.v2.services(service_sid)
+response = service.channels(channel_to_delete).delete
 puts response
