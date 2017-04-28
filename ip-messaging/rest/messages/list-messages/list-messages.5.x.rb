3d0985eb0fba9e41b05143669e5024cd6155d45e
Twilio CMS User <zmiao@twilio.com>
2017-04-28 01:29:15 -0700

    CMS update of ip-messaging/rest/messages/list-messages by zmiao@twilio.com

--- ip-messaging/rest/messages/list-messages/list-messages.5.x.rb
+++ ip-messaging/rest/messages/list-messages/list-messages.5.x.rb
@@ -7,7 +7,7 @@ auth_token = 'AUTH_TOKEN'
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
 # List the messages
-service = @client.ip_messaging.v1.services('SERVICE_SID')
+service = @client.ip_messaging.v2.services('SERVICE_SID')
 channel = service.channels('CHANNEL_SID')
 messages = channel.messages
 puts messages
