4b33de172722ae0f75f00290926cf25c21a13307
Twilio CMS User <zmiao@twilio.com>
2017-04-28 02:06:52 -0700

    CMS update of ip-messaging/rest/messages/retrieve-messages by zmiao@twilio.com

--- ip-messaging/rest/messages/retrieve-messages/retrieve-messages.5.x.rb
+++ ip-messaging/rest/messages/retrieve-messages/retrieve-messages.5.x.rb
@@ -1,13 +1,15 @@
 require 'http'
 require 'twilio-ruby'
 
-# Get your Account SID and Auth Token from twilio.com/console
-account_sid = 'ACCOUNT_SID'
-auth_token = 'AUTH_TOKEN'
+# Get your Account SID, Auth Token, Service SID and Channel SID from twilio.com/console
+account_sid = '<ACCOUNT_SID>'
+auth_token = '<AUTH_TOKEN>'
+service_sid = '<SERVICE_SID>'
+channel_sid = '<CHANNEL_SID>'
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-# Retrieve the message
-service = @client.ip_messaging.v1.services('SERVICE_SID')
-channel = service.channels('CHANNEL_SID')
-message = channel.messages('MESSAGE_SID').fetch
+# List the messages
+service = @client.ip_messaging.v2.services(service_sid)
+channel = service.channels(channel_sid) 
+message = channel.messages('<MESSAGE_SID>').fetch
 puts message
