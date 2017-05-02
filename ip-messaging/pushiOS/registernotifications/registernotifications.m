fb9d722159612eea2289473f434b65afea11d8dd
Twilio CMS User <rbeiter@twilio.com>
2017-05-02 04:26:51 -0700

    CMS update of ip-messaging/pushiOS/registernotifications by rbeiter@twilio.com

--- ip-messaging/pushiOS/registernotifications/registernotifications.m
+++ ip-messaging/pushiOS/registernotifications/registernotifications.m
@@ -1,7 +1,17 @@
 if (self.updatedPushToken) {
-    [self.chatClient registerWithToken:self.updatedPushToken];
+    [self.chatClient registerWithNotificatrionToken:self.updatedPushToken
+    completion:^(TCHResult *result) {
+      if (![result isSuccessful]) {
+        // try registration again or verify token
+      }
+    }];
 }
 
 if (self.receivedNotification) {
-    [self.chatClient handleNotification:self.receivedNotification];
+    [self.chatClient handleNotification:self.receivedNotification
+                             completion:^(TCHResult *result) {
+      if (![result isSuccessful]) {
+        // try handling again?
+      }
+    }];
 }
