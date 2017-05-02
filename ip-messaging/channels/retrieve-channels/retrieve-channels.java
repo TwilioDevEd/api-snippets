28e5eb7e90ab8a439072ab51429a4969d281a8b7
Twilio CMS User <skarchebnyy@twilio.com>
2017-05-02 04:09:39 -0700

    CMS update of ip-messaging/channels/retrieve-channels by skarchebnyy@twilio.com

--- ip-messaging/channels/retrieve-channels/retrieve-channels.java
+++ ip-messaging/channels/retrieve-channels/retrieve-channels.java
@@ -1,8 +1,4 @@
-chatClient.getChannels().getUserChannels(new CallbackListener<Paginator<Channel>>() {
-  @Override
-  public void onSuccess(Paginator<Channel> channelPaginator) {
-    for (Channel channel : channelPaginator.getItems()) {
-      Log.d(TAG, "Channel named: " + channel.getFriendlyName());
-    }
-  }
-});
+List<Channel> channels = chatClient.getChannels().getSubscribedChannels();
+for (Channel channel : channels) {
+    Log.d(TAG, "Channel named: " + channel.getFriendlyName());
+}
