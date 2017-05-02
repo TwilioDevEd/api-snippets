eeb17658862cb21fa5d1d8dfb683607c801c1a22
Twilio CMS User <rbeiter@twilio.com>
2017-05-02 03:13:29 -0700

    CMS update of ip-messaging/accessmanager/initialization by rbeiter@twilio.com

--- ip-messaging/accessmanager/initialization/initialization.m
+++ ip-messaging/accessmanager/initialization/initialization.m
@@ -1,9 +1,18 @@
 self.accessManager = [TwilioAccessManager accessManagerWithToken:token
                                                         delegate:self];
-self.client = [TwilioChatClient chatClientWithToken:accessManager.currentToken
+[TwilioChatClient chatClientWithToken:accessManager.currentToken
                                          properties:properties
-                                           delegate:delegate];
-__weak typeof(client) weakClient = client;
-[accessManager registerClient:client forUpdates:^(NSString *updatedToken) {
-    [weakClient updateToken:updatedToken];
-}];
+                                           delegate:delegate
+                                           completion:(TCHResult *result, TwilioChatClient *client) {
+  if ([result isSuccessful]) {
+    self.client = client;
+    __weak typeof(client) weakClient = client;
+    [accessManager registerClient:client forUpdates:^(NSString *updatedToken) {
+        [weakClient updateToken:updatedToken completion:^(TCHResult *result) {
+          if (![result isSuccessful]) {
+            // warn the user the update didn't succeed
+          }
+        }];
+    }];
+  }
+ }];
