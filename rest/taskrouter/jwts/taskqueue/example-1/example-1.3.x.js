d77033acc685666289b1eecf39d8d0a7400458ea
Samuel Mendes <samuel.mendes@gmail.com>
2017-05-04 16:40:49 +0200

    Update the policy setup in Node 3.x

--- rest/taskrouter/jwts/taskqueue/example-1/example-1.3.x.js
+++ rest/taskrouter/jwts/taskqueue/example-1/example-1.3.x.js
@@ -1,22 +1,36 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These consts are your accountSid and authToken from https://www.twilio.com/console
-const twilio = require('twilio');
+const taskrouter = require('twilio').jwt.taskrouter;
 
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const taskQueueSid = 'WQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-const capability = new twilio.jwt.TaskRouterTaskQueueCapability(accountSid,
-    authToken,
-    workspaceSid,
-    taskQueueSid);
+const capability = new taskrouter.TaskRouterCapability({
+    accountSid: accountSid,
+    authToken: authToken,
+    workspaceSid: workspaceSid,
+    channelId: taskQueueSid,
+    // By default, tokens are good for one hour.
+    // Override this default timeout by specifiying a new value (in seconds).
+    // For example, to generate a token good for 8 hours:
+    ttl: 28800
+});
 
-capability.allowFetchSubresources();
-capability.allowUpdates();
-const token = capability.generate();
+const workspaceUrl = 'https://taskrouter.twilio.com/v1/Workspaces/' + workspaceSid;
+const allowFetchSubresources = new Policy({
+    url: _.join([workspaceUrl, 'TaskQueues', taskQueueSid, '**'], '/'),
+    method: 'GET',
+    allow: true
+});
+const allowUpdates = new Policy({
+    url: workspaceUrl,
+    method: 'POST',
+    allow: true
+});
+capability.addPolicy(allowFetchSubresources);
+capability.addPolicy(allowUpdates);
 
-// By default, tokens are good for one hour.
-// Override this default timeout by specifiying a new value (in seconds).
-// For example, to generate a token good for 8 hours:
-token = capability.generate(28800);  // 60 * 60 * 8
+const token = capability.toJwt();
+console.log(token);
--- rest/taskrouter/jwts/worker/example-1/example-1.3.x.js
+++ rest/taskrouter/jwts/worker/example-1/example-1.3.x.js
@@ -1,23 +1,43 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These consts are your accountSid and authToken from https://www.twilio.com/console
-const twilio = require('twilio');
+const taskrouter = require('twilio').jwt.taskrouter;
 
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const authToken = 'your_auth_token';
 const workspaceSid = 'WSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 const workerSid = 'WKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
 
-const capability = new twilio.jwt.TaskRouterWorkerCapability(accountSid,
-  authToken,
-  workspaceSid,
-  workerSid);
+const capability = new taskrouter.TaskRouterCapability({
+  accountSid: accountSid,
+  authToken: authToken,
+  workspaceSid: workspaceSid,
+  channelId: workerSid,
+  // By default, tokens are good for one hour.
+  // Override this default timeout by specifiying a new value (in seconds).
+  // For example, to generate a token good for 8 hours:
+  ttl: 28800
+});
 
-capability.allowActivityUpdates();
-capability.allowReservationUpdates();
+const workspaceUrl = 'https://taskrouter.twilio.com/v1/Workspaces/' + workspaceSid;
+const allowActivityUpdates = new Policy({
+    url: _.join([workspaceUrl, 'Workers', workerSid], '/'),
+    method: 'POST',
+    allow: true,
+    postFilter: {ActivitySid: {required: true}}
+});
+const allowTasksUpdates = new Policy({
+    url: _.join([workspaceUrl, 'Tasks', '**'], '/'),
+    method: 'POST',
+    allow: true
+});
+const allowReservationsUpdates = new Policy({
+    url: _.join([workspaceUrl, 'Workers', workerSid, 'Reservations', '**'], '/'),
+    method: 'POST',
+    allow: true
+});
+capability.addPolicy(allowActivityUpdates);
+capability.addPolicy(allowTasksUpdates);
+capability.addPolicy(allowReservationsUpdates);
 
-const token = capability.generate();
-
-// By default, tokens are good for one hour.
-// Override this default timeout by specifiying a new value (in seconds).
-// For example, to generate a token good for 8 hours:
-token = capability.generate(28800);  // 60 * 60 * 8
+const token = capability.toJwt();
+console.log(token);
