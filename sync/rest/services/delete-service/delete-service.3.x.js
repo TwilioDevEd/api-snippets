e255f85f64fd11424c41a803217e1eee52b8483f
Andrei Birjukov <abirjukov@twilio.com>
2017-05-16 16:17:53 +0300

    Add missing Node.js "Services" snippets

---
+++ sync/rest/services/delete-service/delete-service.3.x.js
@@ -0,0 +1,12 @@
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var authToken = 'your_auth_token';
+var Twilio = require('twilio').Twilio;
+
+var client = new Twilio(accountSid, authToken);
+
+client.sync.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+  .remove().then(function(response) {
+    console.log(response);
+  }).catch(function(error) {
+    console.log(error);
+  });
