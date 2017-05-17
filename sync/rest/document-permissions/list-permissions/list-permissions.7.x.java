01306408dc372a05bcef65d1b7da41ad28dc25ff
Hector Ortega <hector.oj@gmail.com>
2017-05-17 12:35:15 -0500

    Implement /sync document, list, and map permission Java code samples

---
+++ sync/rest/document-permissions/list-permissions/list-permissions.7.x.java
@@ -0,0 +1,28 @@
+// Install the Java helper library from twilio.com/docs/libraries/java
+
+import com.twilio.Twilio;
+import com.twilio.base.ResourceSet;
+import com.twilio.rest.preview.sync.service.document.DocumentPermission;
+
+import java.net.URISyntaxException;
+
+public class Example {
+    // Find your Account Sid and Token at twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) throws URISyntaxException {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        String serviceSid = "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        String documentSid = "ETXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+        ResourceSet<DocumentPermission> documentPermissions = DocumentPermission
+                .reader(serviceSid, documentSid).read();
+
+        for (DocumentPermission documentPermission : documentPermissions) {
+            System.out.println(documentPermission);
+        }
+
+    }
+}
