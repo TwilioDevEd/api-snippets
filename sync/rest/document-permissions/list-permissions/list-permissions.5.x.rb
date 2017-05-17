f2256f1eb39552396067ed06459c2dbe6557fe8f
Juan Carlos Pazmiño <juankpro@outlook.com>
2017-05-17 10:43:18 -0500

    Implement sync permissions in Ruby

---
+++ sync/rest/document-permissions/list-permissions/list-permissions.5.x.rb
@@ -0,0 +1,15 @@
+require('twilio-ruby')
+
+accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+authToken = 'your_auth_token'
+
+client = Twilio::REST::Client.new(accountSid, authToken)
+service = client.preview.sync.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+
+begin
+  response = service.documents('ETXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    .document_permissions.list
+  puts response
+rescue Twilio::REST::TwilioException => err
+  puts err
+end
