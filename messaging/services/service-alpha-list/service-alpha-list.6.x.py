77508117e0d2994ece26b7a5a534bdebb5a4d4c1
Hector Ortega <hector.oj@gmail.com>
2017-05-18 07:55:55 -0500

    Add messaging service Python samples

---
+++ messaging/services/service-alpha-list/service-alpha-list.6.x.py
@@ -0,0 +1,15 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+alpha_senders = client.messaging \
+                      .services(sid="MG2172dd2db502e20dd981ef0d67850e1a") \
+                      .alpha_senders \
+                      .list()
+
+for alpha_sender in alpha_senders:
+    print(alpha_senders.sid)
