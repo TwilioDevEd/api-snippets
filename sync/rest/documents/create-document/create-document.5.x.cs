a7b37b83521e10bc24404f16a0b2cdcb207d9ab1
Silver Kuusk <skuusk@twilio.com>
2017-05-23 18:32:11 +0300

    Sync C#: Remove Json serialization from examples

--- sync/rest/documents/create-document/create-document.5.x.cs
+++ sync/rest/documents/create-document/create-document.5.x.cs
@@ -1,6 +1,5 @@
 // Download the twilio-csharp library from twilio.com/docs/libraries/csharp
 using System;
-using Newtonsoft.Json;
 using Twilio;
 using Twilio.Rest.Sync.V1.Service;
 
@@ -26,7 +25,7 @@ public class Example
 
         var doc = DocumentResource.Create(serviceSid,
                                           "MyFirstDocument",
-                                          JsonConvert.SerializeObject(data));
+                                          data);
 
         Console.WriteLine(doc.Sid);
     }
--- sync/rest/lists/create-list-item/create-list-item.5.x.cs
+++ sync/rest/lists/create-list-item/create-list-item.5.x.cs
@@ -1,6 +1,5 @@
 // Download the twilio-csharp library from twilio.com/docs/libraries/csharp
 using System;
-using Newtonsoft.Json;
 using Twilio;
 using Twilio.Rest.Sync.V1.Service.SyncList;
 
@@ -24,7 +23,7 @@ public class Example
 
         var item = SyncListItemResource.Create(serviceSid,
                                                "MyCollection",
-                                               JsonConvert.SerializeObject(data));
+                                               data);
 
         Console.WriteLine(item.Index);
     }
