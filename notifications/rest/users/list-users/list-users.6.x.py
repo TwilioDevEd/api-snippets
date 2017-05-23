ccf346e5e6baa48442d61161f9d21dc04faedbd1
Twilio CMS User <jcao@twilio.com>
2017-05-23 15:22:15 -0700

    CMS update of notifications/rest/users/list-users by jcao@twilio.com

---
+++ notifications/rest/users/list-users/list-users.6.x.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# Install the Python helper library from twilio.com/docs/python/install
+
+from twilio.rest import Client
+
+ACCOUNT_SID = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+AUTH_TOKEN = 'your_auth_token'
+
+
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+users = client.notify.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+              .users.stream(segment='premium')
+
+for user in users:
+    print(users.sid)
