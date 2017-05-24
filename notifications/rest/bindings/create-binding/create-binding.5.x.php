08721f02bc52e88b97ef8650b14710709ac16c7f
Twilio CMS User <jlinwood@twilio.com>
2017-05-24 16:23:58 -0700

    CMS update of notifications/rest/bindings/create-binding by jlinwood@twilio.com

--- notifications/rest/bindings/create-binding/create-binding.5.x.php
+++ notifications/rest/bindings/create-binding/create-binding.5.x.php
@@ -19,11 +19,10 @@ $client = new Client($accountSid, $authToken);
 $binding = $client
     ->notify->services($serviceSid)
     ->bindings->create(
-        "eyJjdHkiOiJ0d2lsaW8tZnBhO3Y9MSIsInR5cCI6IkpXVCIsImFsZyI6IkhTMjU2In0.eyJncmFudHMiOnsiY2hhdCI6eyJzZXJ2aWNlX3NpZCI6IklTNmIwZmE2MDgzOWM1MTFlNWExNTFmZWZmODE5Y2RjOWYiLCJlbmRwb2ludF9pZCI6InR3aTEtMTdjNTkyM2Y5NTBiNGJhYjllZjAwZTJlZjdkYTBkYTIifSwiaWRlbnRpdHkiOiJOb3RpZmljYXRpb25MaWJyYXJ5VW5pdFRlc3RlcjEifSwic3ViIjoiQUM3OGU4ZTY3ZmMwMjQ2NTIxNDkwZmI5OTA3ZmQwYzE2NSIsImlzcyI6IlNLMmQxYWRmOGIxYzY0YTYzM2U1ZjE4NjU5ZjdjZDE5MTEiLCJleHAiOjE0NzgyNTI4NTl9.1i256CwtacP859Xh-6cEZVio4oohxL-od9krzsa4DnY",
-        "0000001",
-        "apn",
-        "483161f7afe6f012aa5f172d3ca93f5c245e3daf34d3a911a41f41f6af3639f6",
-        ["tag" => "preferred device"]
+        "identity",
+        "type",
+        "address",
+        ["tag" => "preferred device", "endpoint" => "endpoint"]
     );
 
 echo $binding->sid;
