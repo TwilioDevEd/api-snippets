3e3a5176cde8c6a578121a372da6780f5eb68b29
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-05-24 09:39:27 -0500

    Fixing python snippets

--- rest/message/instance-get-example-1/instance-get-example-1.6.x.py
+++ rest/message/instance-get-example-1/instance-get-example-1.6.x.py
@@ -9,4 +9,4 @@ client = Client(account_sid, auth_token)
 message = client.messages("MM800f449d0399ed014aae2bcc0cc2f2ec") \
                 .fetch()
 
-print(message.body)
+print(message.body.encode('utf-8'))
