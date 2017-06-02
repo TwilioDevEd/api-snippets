669b41b9fa0a5019a924b8ff7906c14eb4e8eb73
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-06-02 16:11:43 -0500

    Fix conference snippet errors

--- rest/participant/instance-post-example-2/instance-post-example-2.3.x.js
+++ rest/participant/instance-post-example-2/instance-post-example-2.3.x.js
@@ -6,5 +6,5 @@ const client = require('twilio')(accountSid, authToken);
 
 client.conferences('CF203597f9bc241da2c1e9387946b89a63')
   .participants('CA386025c9bf5d6052a1d1ea42b4d16662')
-  .update({hold: true, holdUrl: 'www.myapp.com/hold'})
+  .update({hold: 'true', holdUrl: 'www.myapp.com/hold'})
   .then((participant) => console.log(participant.hold));
--- rest/participant/instance-post-example-2/instance-post-example-2.6.x.py
+++ rest/participant/instance-post-example-2/instance-post-example-2.6.x.py
@@ -1,13 +1,14 @@
 # Download the Python helper library from twilio.com/docs/python/install
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
 account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 auth_token  = "your_auth_token"
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
 participant = client \
-    .participants('CFbbe4632a3c49700934481addd5ce1659') \
-    .update('CA386025c9bf5d6052a1d1ea42b4d16662', hold=True, holdUrl='www.myapp.com/hold')
+    .conferences("CFbbe4632a3c49700934481addd5ce1659") \
+    .participants("CA386025c9bf5d6052a1d1ea42b4d16662") \
+    .update(hold=True, hold_url="www.myapp.com/hold")
 
 print(participant.hold)
