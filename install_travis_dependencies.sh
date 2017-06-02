c8981871bf71bb92c3398fe4e9af6d38dacf63ea
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-06-02 14:36:18 -0500

    Move php and node into one container

--- install_travis_dependencies.sh
+++ install_travis_dependencies.sh
@@ -26,3 +26,9 @@ if [[ $languages == *"php"* ]]; then
   curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 fi
 
+if [[ $languages == *"node"* ]]; then
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  nvm install --lts
+fi
