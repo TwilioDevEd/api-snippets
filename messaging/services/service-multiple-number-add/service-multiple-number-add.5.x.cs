3b90d62126a469fcf8f64eeba45794fab28cc827
Twilio CMS User <kkrier@twilio.com>
2017-06-06 16:43:26 -0700

    CMS update of messaging/services/service-multiple-number-add by kkrier@twilio.com

---
+++ messaging/services/service-multiple-number-add/service-multiple-number-add.5.x.cs
@@ -0,0 +1,31 @@
+// Download the twilio-csharp library from twilio.com/docs/libraries/csharp
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Messaging.V1.Service;
+
+
+class Example
+{
+  static void Main(string[] args)
+  {
+    // Find your Account SID and Auth Token at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    const string pathServiceSid = "MG2172dd2db502e20dd981ef0d67850e1a";
+
+    var phoneNumberSids = new List<string> {
+      "PN2a0747eba6abf96b7e3c3ff0b4530f6e",
+      "PN557ce644e5ab84fa21cc21112e22c485",
+      "PN2a0747eba6abf96b7e3c3ff0b4530f6e"
+    };
+
+    TwilioClient.Init(accountSid, authToken);
+
+    foreach(var phoneNumberSid in phoneNumberSids)
+    {
+        var phoneNumber = PhoneNumberResource.Create(pathServiceSid, phoneNumberSid);
+        Console.WriteLine(phoneNumber.Sid);
+    }
+  }
+}
