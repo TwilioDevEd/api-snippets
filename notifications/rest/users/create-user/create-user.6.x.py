8c4168082c7082abc47181aee8f36ab5bf576bdc
viktormuller <viktormuller@users.noreply.github.com>
2017-06-07 17:57:41 +0100

    Fix Create User python example
    Segments --> Segment

--- notifications/rest/users/create-user/create-user.6.x.py
+++ notifications/rest/users/create-user/create-user.6.x.py
@@ -10,6 +10,6 @@ AUTH_TOKEN = 'your_auth_token'
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 user = client.notify.services('ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
              .users.create(identity='User0001',
-                           segments=['premium', 'fitness-lifestyle'])
+                           segment=['premium', 'fitness-lifestyle'])
 
 print(user.sid)
