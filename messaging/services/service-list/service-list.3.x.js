7f5cb373684ac3344376af59b6f6f18784f54bfd
Paul Kamp <pkamp@twilio.com>
2017-06-07 15:29:18 -0700

    Fetch

--- messaging/services/service-list/service-list.3.x.js
+++ messaging/services/service-list/service-list.3.x.js
@@ -7,7 +7,7 @@ const client = require('twilio')(accountSid, authToken);
 
 client.messaging
       .services('MG2172dd2db502e20dd981ef0d67850e1a')
-      .list()
+      .fetch()
       .then(function(response) {
         console.log(response);
       }).catch(function(error) {
