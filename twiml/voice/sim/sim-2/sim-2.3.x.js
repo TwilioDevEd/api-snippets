ee34dbd38eecbfa36a044998d6af3bc776be855b
Samuel Mendes <samuel.mendes@gmail.com>
2017-06-08 19:33:12 +0200

    Add snippets for Sim verb in TwiML

---
+++ twiml/voice/sim/sim-2/sim-2.3.x.js
@@ -0,0 +1,9 @@
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+
+const response = new VoiceResponse();
+const dial = response.dial({
+    record: 'record-from-ringing'
+});
+dial.sim('DE8caa2afb9d5279926619c458dc7098a8');
+
+console.log(response.toString());
