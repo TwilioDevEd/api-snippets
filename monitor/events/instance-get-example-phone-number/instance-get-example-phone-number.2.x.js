2990990f5f3fb6cd702e28a00e876682ec5950ff
Hector Ortega <hector.oj@gmail.com>
2017-06-09 05:49:28 -0500

    Fix failing snippets in /monitor and activate testing

--- monitor/events/instance-get-example-phone-number/instance-get-example-phone-number.2.x.js
+++ monitor/events/instance-get-example-phone-number/instance-get-example-phone-number.2.x.js
@@ -5,6 +5,6 @@ var authToken = "your_auth_token";
 var MonitorClient = require('twilio').MonitorClient;
 var client = new MonitorClient(accountSid, authToken);
 
-client.events.get("AE21f24380625e4aa4abec76e39b14458d", function(error, event) {
-    process.stdout.write(event.description);
+client.events('AE21f24380625e4aa4abec76e39b14458d').get(function(error, event) {
+    process.stdout.write(event.eventType);
 });
