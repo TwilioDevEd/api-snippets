69d711824e222dd97e13c3b0df9d23044747cf1e
José Oliveros <jose.oliveros.1983@gmail.com>
2017-06-09 12:02:07 -0500

    c# proxy snippets

---
+++ proxy/add-phonenumber/add-phonenumber.5.x.cs
@@ -0,0 +1,17 @@
+using System;
+using Twilio;
+using Twilio.Rest.Preview.Proxy.Service;
+
+static void Main(string[] args)
+{
+    // Find your Account Sid, Auth Token and Proxy Service sid at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    const string proxyServiceSid = "KSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string twilioPhoneNumberSid = "PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    TwilioClient.Init(accountSid, authToken);
+
+    var proxyNumber = PhoneNumberResource.Create(proxyServiceSid, twilioPhoneNumberSid);
+
+    Console.WriteLine(proxyNumber.Sid);
+}
---
+++ proxy/create-participant/create-participant.5.x.cs
@@ -0,0 +1,21 @@
+using System;
+using Twilio;
+using Twilio.Rest.Preview.Proxy.Service.Session;
+
+static void Main(string[] args)
+{
+    // Find your Account Sid, Auth Token and Proxy Service sid at twilio.com/console
+    const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string authToken = "your_auth_token";
+    const string proxyServiceSid = "KSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    const string sessionSid = "KCXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    TwilioClient.Init(accountSid, authToken);
+
+    var participant = ParticipantResource.Create(
+        proxyServiceSid,
+        sessionSid,
+        "+15558675309",
+        "Alice");
+
+    Console.WriteLine(participant.Sid);
+}
