00571910abe810e76f2aa0cd7e6a1b97cc34d2ea
Hector Ortega <hector.oj@gmail.com>
2017-06-09 05:49:45 -0500

    Add monitor/events Node 3.x snippets

---
+++ monitor/events/list-get-example-actorsid-resourcesid-error/list-get-example-actorsid-resourcesid-error.3.x.js
@@ -0,0 +1,9 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These are your accountSid and authToken from https://www.twilio.com/console
+const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+const authToken = 'your_auth_token';
+
+const client = require('twilio')(accountSid, authToken);
+
+client.monitor.v1.events.list()
+  .then((response) => console.log(response));
