e37a347c16d6545e1a98d2430f7535672b88b9e3
Hector Ortega <hector.oj@gmail.com>
2017-06-19 11:36:51 -0500

    Add video/rest Node snippets

---
+++ video/rest/rooms/create-room/create-room.3.x.js
@@ -0,0 +1,14 @@
+// NOTE: This example uses the next generation Twilio helper library - for more
+// information on how to download and install this version, visit
+// https://www.twilio.com/docs/libraries/node
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var authToken = 'your_auth_token';
+var Twilio = require('twilio');
+
+var client = new Twilio(accountSid, authToken);
+
+client.video.rooms.create({
+    uniqueName: 'DailyStandup'
+  }).then((room) => {
+    console.log(room.sid);
+  });
---
+++ video/rest/rooms/retrieve-room-recording/retrieve-room-recording.3.x.js
@@ -0,0 +1,15 @@
+// NOTE: This example uses the next generation Twilio helper library - for more
+// information on how to download and install this version, visit
+// https://www.twilio.com/docs/libraries/node
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var authToken = 'your_auth_token';
+var Twilio = require('twilio');
+
+var client = new Twilio(accountSid, authToken);
+
+client.video.rooms('RMXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+  .recordings('RTXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+  .fetch()
+  .then((recording) => {
+    console.log(recording.type);
+  });
