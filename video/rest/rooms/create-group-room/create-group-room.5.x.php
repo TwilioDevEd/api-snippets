bde96b6319d0de46397d9e1408b27356f274f191
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-06-19 13:32:55 -0500

    Add video rooms PHP snippets

---
+++ video/rest/rooms/create-group-room/create-group-room.5.x.php
@@ -0,0 +1,18 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once '/path/to/vendor/autoload.php'; // Loads the library
+use Twilio\Rest\Client;
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$authToken = "your_auth_authToken";
+$client = new Client($accountSid, $authToken);
+
+$groupRoom = $client->video->rooms->create([
+    'uniqueName' => 'DailyStandup',
+    'type' => 'group',
+    'recordParticipantsOnConnect' => true,
+    'statusCallback' => 'http://example.org'
+]);
+
+echo $groupRoom->sid;
---
+++ video/rest/rooms/list-rooms-filtered-by-name/list-rooms-filtered-by-name.5.x.php
@@ -0,0 +1,17 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once '/path/to/vendor/autoload.php'; // Loads the library
+use Twilio\Rest\Client;
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$authToken = "your_auth_authToken";
+$client = new Client($accountSid, $authToken);
+
+$roomsByName = $client->video->rooms->read([
+    'uniqueName' => 'DailyStandup'
+]);
+
+foreach ($roomsByName as $room) {
+    echo $room->sid;
+}
