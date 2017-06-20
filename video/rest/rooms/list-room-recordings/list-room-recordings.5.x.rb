1a9b0696fb2e792b685f34a5106204bd5938c1e2
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-06-20 11:41:30 -0500

    Implement room recording snippets for py, rb, php

---
+++ video/rest/rooms/list-room-recordings/list-room-recordings.5.x.rb
@@ -0,0 +1,15 @@
+# Download the Ruby helper library from twilio.com/docs/libraries/ruby
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from https://www.twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+
+client = Twilio::REST::Client.new account_sid, auth_token
+
+room_recordings = client.video.rooms(sid: "RMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+                              .recordings.read()
+
+room_recordings.each do |recording|
+  puts recording.sid
+end
---
+++ video/rest/rooms/retrieve-room-recording/retrieve-room-recording.6.x.py
@@ -0,0 +1,14 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+from datetime import date
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+room_recording = client.video \
+  .rooms("RMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX") \
+  .recordings("RTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX").fetch()
+
+print(room_recording.sid)
