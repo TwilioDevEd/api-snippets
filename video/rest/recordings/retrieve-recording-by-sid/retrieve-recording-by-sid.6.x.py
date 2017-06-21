658c1de971e11e7ea7fec54fb41c273d8a0dff97
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-06-20 19:25:47 -0500

    Add video recording snippets for php, rb, py

---
+++ video/rest/recordings/retrieve-recording-by-sid/retrieve-recording-by-sid.6.x.py
@@ -0,0 +1,8 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+from datetime import date
+
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
