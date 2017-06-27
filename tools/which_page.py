483b298fe1826e277a31a51b5f807ef88e4ec6b3
Samuel Mendes <samuel.mendes@gmail.com>
2017-06-27 16:35:57 +0200

    Add support for findings pages from a repository

--- tools/which_page.py
+++ tools/which_page.py
@@ -40,7 +40,9 @@ class PostgresConnection(object):
 def get_arg_parser():
     """Get the CLI argument parser for this tool."""
     parser = argparse.ArgumentParser(description='Find which page is using a snippet or tutorial.')
-    parser.add_argument('snippet', help='Snippet filename or filepath from api-snippet')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--snippet', help='Snippet filename or filepath from api-snippet')
+    group.add_argument('--repo', help='Find pages for a repository')
     parser.add_argument('--dbname', help='Postgres database name', default='twilio_docs')
     parser.add_argument('--host', help='Postgres host name', default='localhost')
     parser.add_argument('--port', help='Postgres port number', default='5432')
@@ -61,6 +63,28 @@ def get_code_sample(sample_name, cur):
         """SELECT {fields} FROM code_samples_codesample WHERE path like '%{sample_name}'; """.format(
             fields=', '.join(fields), sample_name=sample_name))
     return map(CodeSample._make, cur.fetchall())
+
+
+def get_code_sample_for_code_repository(code_repository_id, cur):
+    """Return an iterator to all the CodeSample found for a given name."""
+    fields = ['id', 'title', 'path', 'sample_type', 'description', 'code_repository_id']
+    CodeSample = namedtuple('CodeSample', fields)
+    cur.execute(
+        """SELECT {fields} FROM code_samples_codesample WHERE code_repository_id = {code_repository_id}; """.format(
+            fields=', '.join(fields), code_repository_id=code_repository_id))
+    return map(CodeSample._make, cur.fetchall())
+
+
+def get_code_repository(repo, cur):
+    """Return an iterator to all the CodeSample found for a given name."""
+    fields = ['id', 'repo', 'repo_branch']
+    CodeRepository = namedtuple('CodeRepository', fields)
+    if not repo.startswith('TwilioDevEd/'):
+        repo = 'TwilioDevEd/' + repo
+    cur.execute(
+        """SELECT {fields} FROM github_tools_coderepository WHERE repo = '{repo}'; """.format(
+            fields=', '.join(fields), repo=repo))
+    return map(CodeRepository._make, cur.fetchall())
 
 
 def _get_pages_for_code_sample(code_sample_id, cur, table):
@@ -97,6 +121,45 @@ def get_pages_for_code_sample(code_sample_id, cur):
     return {k: v for k, v in pages.items() if len(v) > 0}
 
 
+def _find_pages_for_code_samples(code_samples, cur, depth=0):
+    def dprint(depth, *args, **kwargs):
+        print('  ' * depth, end='')
+        print(*args, **kwargs)
+
+    # Get all the code samples possible for the given path
+    for code_sample in code_samples:
+        dprint(depth, '')
+        dprint(depth, 'Code Sample:')
+        dprint(depth + 1, '#{code_sample.id} - {code_sample.title}'.format(code_sample=code_sample))
+        dprint(depth + 1, '-> https://www.twilio.com/docs/admin/snippets/code_samples/codesample/{id}/'.format(id=code_sample.id))
+
+        # Look up pages where this code sample is used
+        for kind, pages in get_pages_for_code_sample(code_sample.id, cur).items():
+            dprint(depth, '')
+            dprint(depth + 2, kind + ':')
+            for page in pages:
+                dprint(depth + 3, '#{page.id} - {page.title}'.format(page=page))
+                dprint(depth + 3, '-> https://www.twilio.com/docs/admin/pages/{page.id}/edit/'.format(page=page))
+                dprint(depth + 3, '-> https://www.twilio.com{page.url_path}'.format(page=page))
+
+        # Twilio Quest MissionObjective can also embed CodeSample (unlikely from api-snippets)
+        for mission_objective in get_mission_objective_for_code_sample(code_sample.id, cur):
+            dprint(depth + 2, '#{mission_objective.id} - Twilio Quest Mission Objective - {mission_objective.title}'.format(mission_objective=mission_objective))
+
+
+def find_pages_for_snippet(snippet, cur):
+    code_samples = get_code_sample(snippet, cur)
+    _find_pages_for_code_samples(code_samples, cur)
+
+
+def find_pages_for_repo(repo, cur):
+    for code_repository in get_code_repository(repo, cur):
+        print('Code Repository:')
+        print('#{code_repository.id} - {code_repository.repo} [{code_repository.repo_branch}]'.format(code_repository=code_repository))
+        code_samples = get_code_sample_for_code_repository(code_repository.id, cur)
+        _find_pages_for_code_samples(code_samples, cur, depth=1)
+
+
 if __name__ == '__main__':
     # Get argparser and parse CLI options
     parser = get_arg_parser()
@@ -112,20 +175,7 @@ if __name__ == '__main__':
     )
 
     with conn as cur:
-        # Get all the code samples possible for the given path
-        for code_sample in get_code_sample(args.snippet, cur):
-            print('Code Sample:')
-            print('#{code_sample.id} - {code_sample.title}'.format(code_sample=code_sample))
-            print('-> https://www.twilio.com/docs/admin/snippets/code_samples/codesample/{id}/'.format(id=code_sample.id))
-
-            # Look up pages where this code sample is used
-            for kind, pages in get_pages_for_code_sample(code_sample.id, cur).items():
-                print('\n' + kind + ':')
-                for page in pages:
-                    print('#{page.id} - {page.title}'.format(page=page))
-                    print('-> https://www.twilio.com/docs/admin/pages/{page.id}/edit/'.format(page=page))
-                    print('-> https://www.twilio.com{page.url_path}'.format(page=page))
-
-            # Twilio Quest MissionObjective can also embed CodeSample (unlikely from api-snippets)
-            for mission_objective in get_mission_objective_for_code_sample(code_sample.id, cur):
-                print('#{mission_objective.id} - Twilio Quest Mission Objective - {mission_objective.title}'.format(mission_objective=mission_objective))
+        if args.snippet:
+            find_pages_for_snippet(args.snippet, cur)
+        elif args.repo:
+            find_pages_for_repo(args.repo, cur)
