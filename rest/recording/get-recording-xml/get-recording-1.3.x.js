cd5f2626cd3b9c1d3d159dca289941a42ec37006
Ronualdo Maciel <raxmac@gmail.com>
2017-07-04 11:41:25 -0300

    Adds get-recording examples for other languages

---
+++ rest/recording/get-recording-xml/get-recording-1.3.x.js
@@ -0,0 +1,11 @@
+// Download the Node helper library from twilio.com/docs/node/install
+// These vars are your accountSid and authToken from twilio.com/user/account
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var recordingId = 'RE557ce644e5ab84fa21cc21112e22c485'
+var authToken = "your_auth_token";
+var client = require('twilio')(accountSid, authToken);
+
+var promise = client.api.v2010.accounts(accountSid).recordings(recordingId).fetch();
+promise.then(function(response) {
+  console.log(response);
+});
---
+++ rest/recording/get-recording-xml/get-recording-1.5.x.rb
@@ -0,0 +1,13 @@
+# Get twilio-ruby from twilio.com/docs/ruby/install
+require 'rubygems'          # This line not needed for ruby > 1.8
+require 'twilio-ruby'
+
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+
+@client.api.v2010.accounts(account_sid)
+  .recordings(recording_id)
+  .fetch()
