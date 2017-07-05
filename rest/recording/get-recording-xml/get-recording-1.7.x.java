18928d6c2897759c04ef54e2d739854331fde539
Ronualdo Maciel <raxmac@gmail.com>
2017-07-05 11:29:31 -0300

    Fix identation for java get-recording example
    The previous versions was mistakenly using 2 spaces instead of 4

--- rest/recording/get-recording-xml/get-recording-1.7.x.java
+++ rest/recording/get-recording-xml/get-recording-1.7.x.java
@@ -4,14 +4,14 @@ import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.account.Recording;
 
 public class Example {
-  /// Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String AUTH_TOKEN = "your_auth_token";
-  public static final String RECORDING_ID = "RE557ce644e5ab84fa21cc21112e22c485";
+    /// Find your Account Sid and Token at twilio.com/user/account
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+    public static final String RECORDING_ID = "RE557ce644e5ab84fa21cc21112e22c485";
 
-  public static void main(String[] args) {
-    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    Recording recording = Recording.fetcher(ACCOUNT_SID, AUTH_TOKEN).fetch();
-  }
+        Recording recording = Recording.fetcher(ACCOUNT_SID, AUTH_TOKEN).fetch();
+    }
 }
