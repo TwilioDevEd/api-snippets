65a31fd7fd74423230188b3d63b155764e323d27
José Oliveros <jose.oliveros.1983@gmail.com>
2017-07-13 09:46:31 -0500

    fix rest/messages/generate-twiml-mms ruby 5.x

--- rest/messages/generate-twiml-mms/generate-twiml-mms.5.x.rb
+++ rest/messages/generate-twiml-mms/generate-twiml-mms.5.x.rb
@@ -3,14 +3,12 @@ require 'sinatra'
 
 # Respond to incoming sms with an MMS message
 post '/sms' do
-  twiml = Twilio::TwiML::Response.new do |resp|
-    resp.Message do |m|
-      m.Body 'The Robots are coming! Head for the hills!'
-      m.Media 'https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg'
+  twiml = Twilio::TwiML::MessagingResponse.new do |resp|
+    resp.message(body: 'The Robots are coming! Head for the hills!') do |m|
+      resp.media 'https://farm8.staticflickr.com/7090/6941316406_80b4d6d50e_z_d.jpg'
     end
-  end
 
   content_type 'text/xml'
-  
-  twiml.text
+
+  twiml.to_s
 end
