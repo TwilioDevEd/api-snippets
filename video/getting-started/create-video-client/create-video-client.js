e7c7b7ee125c4a514c280403bbf7ac03a9303f2b
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-08-03 16:40:23 -0500

    Fix js snippet to not use async/await

--- video/getting-started/create-video-client/create-video-client.js
+++ video/getting-started/create-video-client/create-video-client.js
@@ -8,9 +8,21 @@
 //
 
 // 1.0.0-beta4 and earlier
-// const client = new Video.Client(accessToken);  
+// const client = new Video.Client(accessToken);
 
-// 1.0.0-beta5 onwards the Client class has been removed. 
-// Instead of constructing a Client using an Access Token and then calling connect 
+// 1.0.0-beta5 onwards the Client class has been removed.
+// Instead of constructing a Client using an Access Token and then calling connect
 // on it, you can simply call connect and pass it an Access Token directly.
-const room = await Twilio.Video.connect('your-token');
+const Video = Twilio.Video;
+
+Video.connect('your-token', { name: 'room-name' }).then(function(room) {
+  console.log('Connected to Room "%s"', room.name);
+
+  room.participants.forEach(participantConnected);
+  room.on('participantConnected', participantConnected);
+
+  room.on('participantDisconnected', participantDisconnected);
+  room.once('disconnected', function(error) {
+    return room.participants.forEach(participantDisconnected);
+  });
+});
