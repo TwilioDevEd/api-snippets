0fb9a286aeaef0460439177e599bd5b67fe7a779
José Oliveros <jose.oliveros.1983@gmail.com>
2017-08-08 13:43:12 -0500

    Add examples for getting recordings for a given call

---
+++ rest/recording/list-get-example-5/list-get-example-5.5.x.php
@@ -0,0 +1,20 @@
+<?php
+// Get the PHP helper library from twilio.com/docs/php/install
+require_once '/path/to/vendor/autoload.php'; // Loads the library
+use Twilio\Rest\Client;
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$token = "your_auth_token";
+$client = new Client($sid, $token);
+
+$recordings = $client->recordings->read(
+    array(
+        "callSid" => "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    )
+);
+
+// Loop over the list of recordings and echo a property for each one
+foreach ($recordings as $recording) {
+    echo $recording;
+}
