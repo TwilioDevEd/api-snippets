3eb8ab0bb44b4b02dc45fb841102e56a5f200bfa
Hector Ortega <hector.oj@gmail.com>
2017-08-10 06:29:21 -0500

    Fix Address instance create Java 6.x sample

--- rest/addresses/instance-create-example/instance-create-example.6.x.java
+++ rest/addresses/instance-create-example/instance-create-example.6.x.java
@@ -2,28 +2,32 @@
 import com.twilio.sdk.TwilioRestClient;
 import com.twilio.sdk.TwilioRestException;
 import com.twilio.sdk.resource.instance.Address;
-import java.util.HashMap;
+import com.twilio.sdk.resource.factory.AddressFactory;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
 public class Example {
 
-  // Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String AUTH_TOKEN = "your_auth_token";
+  	// Find your Account Sid and Token at twilio.com/user/account
+  	public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  	public static final String AUTH_TOKEN = "your_auth_token";
 
-  public static void main(String[] args) throws TwilioRestException {
-    TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+  	public static void main(String[] args) throws TwilioRestException {
+    		TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
 
-    // Get an object from its sid. If you do not have a sid,
-    // check out the list resource examples on this page
-    Address address = client.getAccount().create(new HashMap<String, String>() {{
-        put("customerName", "Customer 500");
-        put("street", "Elm Street");
-        put("city", "Racoon");
-        put("region", "Mordor");
-        put("postalCode", "150");
-        put("isoCountry", "AX");
-    }});
+    		List<NameValuePair> params = new ArrayList<NameValuePair>();
+    		params.add(new BasicNameValuePair("FriendlyName", "Customer 500"));
+    		params.add(new BasicNameValuePair("CustomerName", "Customer 500"));
+    		params.add(new BasicNameValuePair("Street", "Elm Street"));
+    		params.add(new BasicNameValuePair("City", "Racoon"));
+    		params.add(new BasicNameValuePair("Region", "Mordor"));
+    		params.add(new BasicNameValuePair("PostalCode", "150"));
+    		params.add(new BasicNameValuePair("IsoCountry", "AX"));
+    		AddressFactory factory = client.getAccount().getAddressFactory();
+    		Address address = factory.create(params);
 
-    System.out.println(address.getCustomerName());
-  }
+    		System.out.println(address.getCustomerName());
+  	}
 }
