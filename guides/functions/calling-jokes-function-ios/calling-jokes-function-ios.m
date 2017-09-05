62c56cfe42dabed9b58dee2448e91d6e4f03c8f6
Jeffrey Linwood <jlinwood@gmail.com>
2017-09-05 12:37:29 -0500

    Added Twilio function that returns JSON, and iOS snippets for calling it

--- guides/functions/calling-jokes-function-ios/calling-jokes-function-ios.m
+++ guides/functions/calling-jokes-function-ios/calling-jokes-function-ios.m
@@ -1,11 +1,12 @@
-NSString* functionURL = @"https://yourdomain.twil.io/joke";
+NSString* functionURL = @"https://yourdomain.twil.io/jokes";
 NSURL *url = [NSURL URLWithString:functionURL];
 NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
     if (error) {
         NSLog(@"Error: %@",error);
     } else {
-        NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        NSLog(@"Response: %@", responseString);
+        NSError *error;
+        id responseObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        NSLog(@"Response: %@", responseObject);
     }
 }];
 [task resume];
