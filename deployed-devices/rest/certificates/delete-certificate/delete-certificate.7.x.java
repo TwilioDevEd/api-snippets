9ae7a89f7703abe2b7b7ced342e1064d7a3171f7
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-09-15 14:34:33 -0500

    Add snippets for delete-certificate

---
+++ deployed-devices/rest/certificates/delete-certificate/delete-certificate.7.x.java
@@ -0,0 +1,25 @@
+// Get the Node helper library from https://twilio.com/docs/libraries/java
+import com.twilio.Twilio;
+import com.twilio.base.ResourceSet;
+import com.twilio.rest.preview.deployedDevices.fleet;
+
+public class Example {
+    // Get your Account SID and Auth Token from https://twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        String fleetSid = "FLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        String certSid = "CYXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        CertificateDeleter certificateDeleter = CertificateDeleter(
+            fleetSid,
+            certSid
+        );
+
+        boolean wasDeleted = certificateDeleter.delete();
+
+        System.out.println(wasDeleted);
+    }
+}
