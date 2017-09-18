4af9b7fb2f630bad0d32a75a49a5032910832219
José Oliveros <jose.oliveros.1983@gmail.com>
2017-09-18 16:28:24 -0500

    Add deployed-devices/rest/deployments snippets

---
+++ deployed-devices/rest/deployments/create-deployment/create-deployment.5.x.cs
@@ -0,0 +1,22 @@
+// Get the C# helper library from https://twilio.com/docs/libraries/csharp
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Preview.DeployedDevices.Fleet.DeploymentResource;
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        // Get your Account SID and Auth Token from https://twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        TwilioClient.Init(accountSid, authToken);
+
+        var deployment = DeploymentResource.Create(
+            friendlyName: "My Fleet deployment."
+        );
+
+        Console.WriteLine(deployment.Sid);
+    }
+}
---
+++ deployed-devices/rest/deployments/list-deployments/list-deployments.5.x.php
@@ -0,0 +1,16 @@
+<?php
+// Get the PHP helper library from https://twilio.com/docs/libraries/php
+require_once '/path/to/vendor/autoload.php'; // Loads the library
+
+use Twilio\Rest\Client;
+
+// Get your Account SID and Auth Token from https://twilio.com/console
+$accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+$authToken = 'your_auth_token';
+$client = new Client($accountSid, $authToken);
+
+$deployments = $client->preview->deployedDevices->deployments->read();
+
+foreach ($deployment as $deployments) {
+  echo $deployment->sid;
+}
