62d031d63722f8e122663438a04eaf5efa272e93
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-09-18 11:09:55 -0500

    Add certificates/retrieve-certificate snippets

---
+++ deployed-devices/rest/certificates/retrieve-certificate/retrieve-certificate.5.x.cs
@@ -0,0 +1,23 @@
+// Get the Node helper library from https://twilio.com/docs/libraries/csharp
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Preview.DeployedDevices.Fleet
+
+public class Example
+{
+    public static void Main(string[] args)
+    {
+        // Get your Account SID and Auth Token from https://twilio.com/console
+        const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string authToken = "your_auth_token";
+        TwilioClient.Init(accountSid, authToken);
+
+        const string fleetSid = "FLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const string certSid = "CYXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        const bool certiricate = CertificateResource
+            .Fetch(fleetSid, certSid);
+
+        Console.WriteLine(certiricate.Sid);
+    }
+}
