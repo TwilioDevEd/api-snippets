01ed8c86ba2350b7f5f22bea965344adc9f0536d
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-09-18 13:28:06 -0500

    Add device/update-device snippets

---
+++ deployed-devices/rest/devices/update-device/update-device.5.x.rb
@@ -0,0 +1,17 @@
+# Get the Node helper library from https://twilio.com/docs/libraries/ruby
+require 'twilio-ruby'
+
+# Get your Account SID and Auth Token from https://twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Twilio::REST::Client.new(account_sid, auth_token)
+
+fleet_sid = 'FLXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+fleet_service = client.preview.deployed_devices.fleets(fleet_sid)
+
+device = fleet_service
+  .devices('THXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+  .fetch()
+  .update(friendly_name: "My New Device", identity: "bob@twilio.com")
+
+puts device.friendly_name
