76b942f18d79ddefc08f37e39f3f4082e921210c
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-09-25 16:18:51 -0500

    Add java snippets for quickstart guides

---
+++ quickstart/java/sms/example-1/SmsSender.7.x.java
@@ -0,0 +1,22 @@
+// Install the Java helper library from twilio.com/docs/libraries/java
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class SmsSender {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message message = Message
+            .creator(new PhoneNumber("+14159352345"), // to
+                     new PhoneNumber("+14158141829"), // from
+                     "Where's Wallace?")
+            .create();
+
+        System.out.println(message.getSid());
+    }
+}
---
+++ quickstart/java/sms/example-2/MmsSender.7.x.java
@@ -0,0 +1,25 @@
+// Install the Java helper library from twilio.com/docs/libraries/java
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MmsSender {
+    // Find your Account Sid and Auth Token at twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) throws URISyntaxException {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        String uri = "https://static0.twilio.com/recources/logos/twilio-loco-circle-50x50.png";
+
+        Message message = Message
+            .creator(new PhoneNumber("+14159352345"), // to
+                     new PhoneNumber("+14158141829"), // from
+                     "Where's Wallace?")
+            .setMediaUrl(new URI(uri))
+            .create();
+    }
+}
