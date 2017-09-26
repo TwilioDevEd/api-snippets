86d580dac84b44d5ee4a7d752ffeb5fe3af093d4
Devid Liik <dliik@twilio.com>
2017-09-26 15:53:17 +0300

    notify:java: SDK notification parameters are objects now

--- notifications/rest/notifications/send-notification-detailed/send-notification-detailed.7.x.java
+++ notifications/rest/notifications/send-notification-detailed/send-notification-detailed.7.x.java
@@ -1,6 +1,9 @@
 // NOTE: This example uses the next generation Twilio helper library - for more
 // information on how to download and install this version, visit
 // https://www.twilio.com/docs/libraries/java
+import java.util.HashMap;
+import java.util.Map;
+
 import com.twilio.Twilio;
 import com.twilio.rest.notify.v1.service.Notification;
 
@@ -15,19 +18,24 @@ public class Example {
         // Initialize the client
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
+        Map<String, Object> data = new HashMap<>();
+        data.put("custom_key1", "custom value 1");
+        data.put("custom_key2", "custom value 2");
+
+        Map<String, Object> fcmParams = new HashMap<>();
+        fcmParams.put("notification", "{\"title\":\"New alert\", \"body\" : \"Hello Bob!\"}}");
+
+        Map<String, Object> apnParams = new HashMap<>();
+        apnParams.put("aps","{ \"alert\": {\"title\":\"New alert\", \"body\" : \"Hello Bob!\"}}");
+
         Notification notification = Notification
                 .creator(SERVICE_SID)
                 .setTitle("Generic loooooooong title for all Bindings")
                 .setBody("This is the body for all Bindings")
                 .setIdentity("00000001")
-                .setData("{\"custom_key1\":\"custom value 1\"," +
-                         "\"custom_key2\":\"custom value 2\"}")
-                .setFcm("{\"notification\":{\"title\":\"New alert\"," +
-                        "\"body\" : \"Hello Bob!\"}}")
-                .setApn("{\"aps\" : " +
-                        "{ \"alert\": " +
-                        "{\"title\":\"New alert\"," +
-                        "\"body\" : \"Hello Bob!\"}}}")
+                .setData(data)
+                .setFcm(fcmParams)
+                .setApn(apnParams)
                 .create();
 
         System.out.println(notification.getSid());
--- notifications/rest/notifications/send-notification-with-badge/send-notification-with-badge.7.x.java
+++ notifications/rest/notifications/send-notification-with-badge/send-notification-with-badge.7.x.java
@@ -1,6 +1,9 @@
 // NOTE: This example uses the next generation Twilio helper library - for more
 // information on how to download and install this version, visit
 // https://www.twilio.com/docs/libraries/java
+import java.util.HashMap;
+import java.util.Map;
+
 import com.twilio.Twilio;
 import com.twilio.rest.notify.v1.service.Notification;
 
@@ -15,14 +18,17 @@ public class Example {
     // Initialize the client
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    Notification notification = Notification
-        .creator(SERVICE_SID)
-        .setIdentity("00000001")
-        .setApn("{\"aps\" : " +
+    Map<String, Object> apnParams = new HashMap<>();
+    apnParams.put("aps",
                     "{ \"alert\": " +
                         "{\"title\":\"Bob alert\"," +
                          "\"body\" : \"Bob, you just received a badge\"}, " +
-                    "\"badge\" : 1 }}")
+                      "\"badge\" : 1 }}");
+
+    Notification notification = Notification
+        .creator(SERVICE_SID)
+        .setIdentity("00000001")
+        .setApn(apnParams)
         .create();
 
     System.out.println(notification.getSid());
