f6bc71db3e959131c6f6a3bcfbda9e22bf3deb6f
Wellington Mendoza <wellington.mendoza.contact@gmail.com>
2017-10-02 20:10:15 -0500

    Add retrieve recording media snippets

---
+++ video/rest/recordings/retrieve-recording-binary-data/retrieve-recording-binary-data.7.x.java
@@ -0,0 +1,29 @@
+import org.json.JSONObject;
+import com.twilio.Twilio;
+import com.twilio.rest.video.v1.Recording;
+
+
+public class Example {
+    // Find your Account Sid and Token at twilio.com/console
+    public static final String API_KEY_SID = "SKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String API_KEY_SECRET = "your_api_key_secret";
+
+    public static void main(String args[]) {
+
+        // Initialize the client
+        Twilio.init(API_KEY_SID, API_KEY_SECRET);
+
+        String recordingSid = "RTXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        TwilioRestClient restClient = Twilio.getRestClient();
+        Request request = new Request(
+            HttpMethod.POST,
+            Domains.VIDEO.toString(),
+            "/v1/Recordings/" + recordingSid + "/Media/",
+            restClient.getRegion()
+        );
+        Response response = restClient.request(request);
+        String mediaLocation = json.getJSONObject(response.getStream()).getString("location");
+
+        System.out.println(mediaLocation);
+    }
+}
