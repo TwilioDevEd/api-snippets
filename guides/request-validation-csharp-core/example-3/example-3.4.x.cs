931bda2a71377a98b3ce2fac36f342c7c37c16d5
Agustin Camino <agustin.camino@gmail.com>
2017-10-11 07:56:42 -0500

    Add request validation for C# .NET Core

---
+++ guides/request-validation-csharp-core/example-3/example-3.4.x.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Twilio.Security;
+
+namespace ValidateRequestExample.Filters
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ValidateTwilioRequestAttribute : ActionFilterAttribute
+    {
+        private readonly RequestValidator _requestValidator;
+        private static IConfigurationRoot Configuration =>
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        private static bool IsTestEnvironment =>
+            bool.Parse(Configuration["IsTestEnvironment"]);
+
+        public ValidateTwilioRequestAttribute()
+        {
+            var authToken = Configuration["TwilioAuthToken"];
+            _requestValidator = new RequestValidator(authToken);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            var context = actionContext.HttpContext;
+            if (!IsValidRequest(context.Request) || !IsTestEnvironment)
+            {
+                actionContext.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+            }
+
+            base.OnActionExecuting(actionContext);
+        }
+
+
+        private bool IsValidRequest(HttpRequest request) {
+            var requestUrl = RequestRawUrl(request);
+            var parameters = ToDictionary(request.Form);
+            var signature = request.Headers["X-Twilio-Signature"];
+            return _requestValidator.Validate(requestUrl, parameters, signature);
+        }
+
+        private static string RequestRawUrl(HttpRequest request)
+        {
+            return $"{request.Scheme}://{request.Host}{request.Path}{request.QueryString}";
+        }
+
+        private static IDictionary<string, string> ToDictionary(IFormCollection collection)
+        {
+            return collection.Keys
+                .Select(key => new { Key = key, Value = collection[key] })
+                .ToDictionary(p => p.Key, p => p.Value.ToString());
+        }
+    }
+}
