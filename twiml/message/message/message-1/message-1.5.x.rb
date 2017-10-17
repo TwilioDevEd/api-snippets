afacaa134bf4d3a699558a8ba64f1aac169bca6d
Evan Fossier <evan.fossier@gmail.com>
2017-10-17 14:52:25 -0700

    Update body parameter of Message twiml

--- twiml/message/message/message-1/message-1.5.x.rb
+++ twiml/message/message/message-1/message-1.5.x.rb
@@ -1,6 +1,6 @@
 require 'twilio-ruby'
 
 response = Twilio::TwiML::MessagingResponse.new
-response.message(message: 'Store Location: 123 Easy St.')
+response.message(body: 'Store Location: 123 Easy St.')
 
 puts response
