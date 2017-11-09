8199f4020f3ca381cc3bd542bdf46938970ae844
Silenceamryus <33526268+Silenceamryus@users.noreply.github.com>
2017-11-10 05:44:03 +1100

    PK ONline-1.6.x.py
    sfghjkdfghj

--- twiml/voice/say/say-1/say-1.6.x.py
+++ twiml/voice/say/say-1/say-1.6.x.py
@@ -1,6 +1,6 @@
 from twilio.twiml.voice_response import VoiceResponse, Say
 
 response = VoiceResponse()
-response.say('Chapeau!', voice='woman', language='fr')
+response.say('Sout Pal Gyi Fuck You!', voice='woman', language='fr')
 
 print(response)
