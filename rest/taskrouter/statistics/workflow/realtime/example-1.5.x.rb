acac1439ddd68db0632c72336329465e42e6cdc9
Justin Witz <jwitz@twilio.com>
2017-11-15 14:17:13 -0800

    Stats endpoints utilizing latest helper libs methods in a friendlier manner

--- rest/taskrouter/statistics/workflow/realtime/example-1.5.x.rb
+++ rest/taskrouter/statistics/workflow/realtime/example-1.5.x.rb
@@ -11,7 +11,7 @@ client = Twilio::REST::Client.new(account_sid, auth_token)
 
 statistics = client.taskrouter.v1.workspaces(workspace_sid)
                    .workflows(workflow_sid)
-                   .workflow_real_time_statistics.fetch
+                   .real_time_statistics.fetch
 
 puts statistics.tasks_by_status['pending']
 puts statistics.tasks_by_status['assigned']
