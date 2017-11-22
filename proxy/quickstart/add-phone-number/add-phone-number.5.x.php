927b33b478b3a66edabd1751d56fb1cb88e34869
Paul Kamp <pkamp@twilio.com>
2017-11-22 11:31:57 -0800

    Add phone number snippets

--- proxy/quickstart/add-phone-number/add-phone-number.5.x.php
+++ proxy/quickstart/add-phone-number/add-phone-number.5.x.php
@@ -8,7 +8,6 @@ $token = "your_auth_token";
 // Initialize the client
 $client = new Client($sid, $token);
 $phoneNumber = $client
-    ->preview
     ->proxy
     ->services("KSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     ->phoneNumbers->create(
--- proxy/quickstart/add-phone-number/add-phone-number.5.x.rb
+++ proxy/quickstart/add-phone-number/add-phone-number.5.x.rb
@@ -8,7 +8,7 @@ auth_token = 'your_auth_token'
 # Initialize Twilio Client
 client = Twilio::REST::Client.new account_sid, auth_token
 
-phone_number = client.preview.proxy
+phone_number = client.proxy
                      .services('KSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
                      .phone_numbers
                      .create(sid: 'PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
