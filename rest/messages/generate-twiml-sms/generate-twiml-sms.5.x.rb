9b8f45bbafc4ec683e18248f8bd11f17852e7c5e
Phil Nash <philnash@gmail.com>
2017-12-01 07:25:11 +1100

    Fixes `MessagingResponse`
    The latest `Message` object in TwiML requires a hash with a `body` rather than plain text.

--- rest/messages/generate-twiml-sms/generate-twiml-sms.5.x.rb
+++ rest/messages/generate-twiml-sms/generate-twiml-sms.5.x.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 # Respond to incoming calls with a simple text message
 post '/sms' do
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
-    r.message 'The Robots are coming! Head for the hills!'
+    r.message body: 'The Robots are coming! Head for the hills!'
   end
 
   content_type 'text/xml'
