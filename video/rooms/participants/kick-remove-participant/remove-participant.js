231296db34045529636f4e00f2b57bf38aa0ae1e
Kedar Toraskar <ktoraskar@twilio.com>
2017-12-12 22:33:37 -0800

    Fixed merge issues with kick-remove-participant.js

--- video/rooms/participants/kick-remove-participant/remove-participant.js
+++ video/rooms/participants/kick-remove-participant/remove-participant.js
@@ -9,10 +9,6 @@ var Twilio = require('twilio');
 var client = new Twilio(apiKeySid, apiKeySecret, {accountSid: accountSid});
 
 client.video.rooms('RMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX').participants.get('Alice').fetch()
-<<<<<<< HEAD
-  .then((participant) => {
-=======
   .then(participant => {
->>>>>>> a0220aea7ed39b844ab4f39bb8376c9e2965dd44
     console.log(participant.duration);
   });
