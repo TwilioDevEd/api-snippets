c0b6f065052cd7f374a4c1491c9540832a594a24
Andrew Tork Baker <andrew.tork.baker@gmail.com>
2018-01-04 12:01:00 -0800

    Fix Ruby sample

--- rest/sending-messages/example-1/example-1.5.x.rb
+++ rest/sending-messages/example-1/example-1.5.x.rb
@@ -8,11 +8,11 @@ auth_token = 'your_auth_token'
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new(account_sid, auth_token)
 
-message = @client.messages.create(
-  body: 'Jenny please?! I love you <3',
+message = @client.create(
+  body: 'Ahoy, World!',
   to: '+15558675309',
   from: '+14158141829',
   media_url: 'http://www.example.com/hearts.png'
 )
 
-puts message.to
+puts message.sid
