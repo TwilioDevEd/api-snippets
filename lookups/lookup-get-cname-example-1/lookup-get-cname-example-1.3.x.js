20e4f6e7eca58f525b41953404fa98166d28aea7
Andrew Tork Baker <andrew.tork.baker@gmail.com>
2018-01-26 14:57:09 -0800

    Update lookup-get-cname-example-1.3.x.js

--- lookups/lookup-get-cname-example-1/lookup-get-cname-example-1.3.x.js
+++ lookups/lookup-get-cname-example-1/lookup-get-cname-example-1.3.x.js
@@ -8,4 +8,4 @@ const client = require('twilio')(accountSid, authToken);
 client.lookups.v1
   .phoneNumbers('+16502530000')
   .fetch({ type: 'caller-name' })
-  .then(number => console.log(number.carrier.type, number.carrier.name));
+  .then(number => console.log(number));
