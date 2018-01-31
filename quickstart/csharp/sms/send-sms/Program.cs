142b0834090e14322f7558d71d1648d48d67b6d2
David Prothero <david@prothero.com>
2018-01-31 10:55:15 -0800

    Create Program.cs

---
+++ quickstart/csharp/sms/send-sms/Program.cs
@@ -0,0 +1,27 @@
+// Download the twilio-csharp library from twilio.com/docs/libraries/csharp
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace YourNewConsoleApp
+{
+    class Example
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account Sid and Auth Token at twilio.com/console
+            const string accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            const string authToken = "your_auth_token";
+            TwilioClient.Init(accountSid, authToken);
+
+            var to = new PhoneNumber("+15017122661");
+            var message = MessageResource.Create(
+                to,
+                from: new PhoneNumber("+15558675310"),
+                body: "This is the ship that made the Kessel Run in fourteen parsecs?");
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
