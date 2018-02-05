880014173175c7ebab78bb0ce280dc79340babfe
Kat King <katkingcodes@gmail.com>
2018-02-05 14:36:50 -0500

    New code samples for Voice quickstart (Python)

---
+++ quickstart/python/voice/example-1-make-call/outgoing_call.6.x.py
@@ -0,0 +1,15 @@
+# Download the Python helper library from twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token can be found at https://www.twilio.com/console
+account_sid = "AC6062f793ce5918fef56b1681e6446e87"
+auth_token = "your_auth_token"
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+    to="+15558675310",
+    from_="+15017122661",
+    url="http://demo.twilio.com/docs/voice.xml"
+)
+
+print(call.sid)
