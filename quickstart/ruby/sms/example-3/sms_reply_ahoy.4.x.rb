142a7b7cf056ef21b8bc46fbd62d92f03156dcc2
Jen Aprahamian <j.aprahamian@gmail.com>
2018-02-13 13:48:33 -0800

    update Ruby SMS samples for consistency with new quickstart

--- quickstart/ruby/sms/example-3/sms_reply_ahoy.4.x.rb
+++ quickstart/ruby/sms/example-3/sms_reply_ahoy.4.x.rb
@@ -5,10 +5,7 @@ require 'sinatra'
 
 get '/sms-quickstart' do
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Message do |m|
-      m.Body 'Hello, Mobile Monkey'
-      m.Media 'https://demo.twilio.com/owl.png'
-    end
+    r.Message 'Ahoy! Thanks so much for your message.'
   end
 
   content_type 'text/xml'
--- quickstart/ruby/sms/example-4/mms_hello_friend.5.x.rb
+++ quickstart/ruby/sms/example-4/mms_hello_friend.5.x.rb
@@ -5,7 +5,9 @@ require 'sinatra'
 
 get '/sms-quickstart' do
   twiml = Twilio::TwiML::MessagingResponse.new do |r|
-    r.message(body: 'Hello, Mobile Monkey')
+    r.Message(body: 'Hello, friend') do |message|
+      message.media 'https://demo.twilio.com/owl.png'
+    end
   end
 
   twiml.to_s
