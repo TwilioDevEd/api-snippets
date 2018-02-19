ab334957c987006c4bc8df7e833b6abbbdba3983
Randy Beiter <rbeiter@twilio.com>
2018-02-19 08:51:19 -0500

    Remvoe endpoint from token generation samples

--- rest/access-tokens/ip-messaging-example/ip-messaging-example.2.x.js
+++ rest/access-tokens/ip-messaging-example/ip-messaging-example.2.x.js
@@ -8,16 +8,12 @@ const twilioApiSecret = 'xxxxxxxxxxxx';
 
 // Used specifically for creating IP Messaging tokens
 const serviceSid = 'ISxxxxxxxxxxxxx';
-const appName = 'HipFlowSlackDockRC';
 const identity = 'user@example.com';
-const deviceId = 'someiosdeviceid';
-const endpointId = appName + ':' + identity + ':' + deviceId;
 
 // Create a "grant" which enables a client to use IPM as a given user,
 // on a given device
 const ipmGrant = new IpMessagingGrant({
   serviceSid: serviceSid,
-  endpointId: endpointId,
 });
 
 // Create an access token which we will sign and return to the client,
--- rest/access-tokens/ip-messaging-example/ip-messaging-example.4.x.rb
+++ rest/access-tokens/ip-messaging-example/ip-messaging-example.4.x.rb
@@ -7,9 +7,7 @@ api_secret = 'xxxxxxxxxxxxxx'
 
 # Required for IP Messaging
 service_sid = 'ISxxxxxxxxxxxx'
-device_id = 'someiosdevice'
 identity = 'user@example.com'
-endpoint_id = "HipFlowSlackDockRC:#{identity}:#{device_id}"
 
 # Create an Access Token
 token = Twilio::Util::AccessToken.new account_sid, api_key, api_secret,
@@ -18,7 +16,6 @@ token = Twilio::Util::AccessToken.new account_sid, api_key, api_secret,
 # Create IP Messaging grant for our token
 grant = Twilio::Util::AccessToken::IpMessagingGrant.new
 grant.service_sid = service_sid
-grant.endpoint_id = endpoint_id
 token.add_grant grant
 
 # Generate the token
