393dd893212847f9c7f685391fe373f2f46b50ad
Jeffrey Linwood <jlinwood@gmail.com>
2018-02-20 14:03:30 -0600

    Update Java SMS Quickstart generate TwiML snippet, delete 6.x version

--- rest/messages/generate-twiml-sms/generate-twiml-sms.7.x.java
+++ rest/messages/generate-twiml-sms/generate-twiml-sms.7.x.java
@@ -1,29 +1,20 @@
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
+import com.twilio.twiml.MessagingResponse;
 import com.twilio.twiml.messaging.Body;
 import com.twilio.twiml.messaging.Message;
-import com.twilio.twiml.MessagingResponse;
-import com.twilio.twiml.TwiMLException;
 
-public class TwilioServlet extends HttpServlet {
+import static spark.Spark.*;
 
-  // service() responds to both GET and POST requests.
-  // You can also use doGet() or doPost()
-  public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Message sms =
-      new Message.Builder().body(new Body("The Robots are coming! Head for the hills!")).build();
+public class SmsApp {
+    public static void main(String[] args) {
+        get("/", (req, res) -> "Hello Web");
 
-    MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
-
-    response.setContentType("application/xml");
-
-    try {
-      response.getWriter().print(twiml.toXml());
-    } catch (TwiMLException e) {
-      e.printStackTrace();
+        post("/sms", (req, res) -> {
+            res.type("application/xml");
+            Body body = new Body.Builder("The Robots are coming! Head for the hills!").build();
+            Message sms =
+                    new Message.Builder().body(body).build();
+            MessagingResponse twiml = new MessagingResponse.Builder().message(sms).build();
+            return twiml.toXml();
+        });
     }
-  }
 }
