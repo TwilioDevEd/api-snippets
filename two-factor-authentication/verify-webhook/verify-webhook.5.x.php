e1eb942538bb3ea120e7270009b0e5464aa15e5d
Carlos Villavicencio <carlos.po5i@gmail.com>
2018-02-28 10:16:44 -0500

    Fixed 2FA webhook PHP snippets

--- two-factor-authentication/verify-webhook/verify-webhook.5.x.php
+++ two-factor-authentication/verify-webhook/verify-webhook.5.x.php
@@ -1,15 +1,29 @@
 <?php
-
+/**
+ *
+ * Verify Authy signature
+ *
+ * @param    string  $apiKey Authy app API KEY
+ * @return   bool
+ *
+ */
 function verifyWebhook($apiKey) {
     // Read the nonce from the request
-    $nonce = $_SERVER['x-authy-signature-nonce'];
+    $nonce = $_SERVER['HTTP_X_AUTHY_SIGNATURE_NONCE'];
     $method = $_SERVER['REQUEST_METHOD'];
     $proto = isset($_SERVER['HTTPS']) ? "https" : "http";
     $url = "{$proto}://{$_SERVER[HTTP_HOST]}{$_SERVER[REQUEST_URI]}";
-    $params = implode('&', array_map(function($k, $v) {
-        return "$k=$v";
-    }, array_keys($_POST), array_values($_POST)));
-    sort($params);
+
+    // get the Json string sent by Authy
+    $json = file_get_contents('php://input');
+    $decoded = json_decode($json, true);
+    $normalized = normalizeArray($decoded);
+
+    //  sort the parameters by keys
+    $query = http_build_query($normalized);
+    $exploded = explode('&', $query);
+    sort($exploded);
+    $params = implode('&', $exploded);
 
     // concatenate all together and separate them by '|'
     $data = "$nonce|$method|$url|$params";
@@ -18,8 +32,29 @@ function verifyWebhook($apiKey) {
     $computedSig = base64_encode(hash_hmac('sha256', $data, $apiKey, true));
 
     // get the authy signature
-    $sig = $_SERVER['x-authy-signature'];
+    $sig = $_SERVER['HTTP_X_AUTHY_SIGNATURE'];
 
     // compare the message signature with your calculated signature
     return hash_equals($computedSig, $sig);
+}
+
+/**
+ *
+ * Replace boolean values with strings.
+ *   In PHP methods like http_build_query converts values from an array
+ *   to 0 and 1 integers instead of 'false' and 'true'.
+ *
+ * @param    array  $array Multidimensional array with boolean values
+ * @return   array
+ *
+ */
+function normalizeArray($array) {
+  foreach ($array as &$el) {
+      if (is_bool($el)) {
+        $el = ($el) ? "true" : "false";
+      } elseif (is_array($el)) {
+        $el = normalizeArray($el);
+      }
+  }
+  return $array;
 }
