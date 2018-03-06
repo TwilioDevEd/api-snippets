97dd977f6dc7a047431cc7d83c4305c08078eb4f
Twilio CMS User <jlinwood@twilio.com>
2018-03-06 13:09:22 -0800

    CMS update of rest/voice/generate-twiml-say by jlinwood@twilio.com

--- rest/voice/generate-twiml-say/generate-twiml-say.7.x.java
+++ rest/voice/generate-twiml-say/generate-twiml-say.7.x.java
@@ -5,6 +5,9 @@ import static spark.Spark.*;
 
 public class VoiceApp {
     public static void main(String[] args) {
+
+        get("/hello", (req, res) -> "Hello Web");
+
         post("/", (request, response) -> {
             Say say  = new Say.Builder(
                     "Hello from your pals at Twilio! Have fun.")
