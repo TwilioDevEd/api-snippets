ff5668e475fce5b087f3d7f92684895abf5204cd
Hector Ortega <hector.oj@gmail.com>
2018-03-06 00:42:14 -0500

    Fix Authy webhook signature validation Java snippet

--- two-factor-authentication/verify-webhook/verify-webhook.7.x.java
+++ two-factor-authentication/verify-webhook/verify-webhook.7.x.java
@@ -10,38 +10,44 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class CallbackVerifier {
-
-    private boolean verify(HttpServletRequest request, String apiKey) {
-        String url = String.format("%s://%s%s", request.getScheme(),
+  
+    private boolean verify(HttpServletRequest request, String authyApiKey)
+            throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String forwardedProtocol = request.getHeader("X-Forwarded-Proto");
+        String protocol = forwardedProtocol != null ? forwardedProtocol :
+                request.getScheme();
+        String url = String.format("%s://%s%s", protocol,
                 request.getServerName(), request.getServletPath());
 
-        // Sort the params
-        Map<String, String[]> sortedParameters =
-                new TreeMap<>(request.getParameterMap());
+        // Fetch Json body
+        String body = IOUtils.toString(request.getReader());
+        Map<String, Object> bodyJson = objectMapper.readValue(body,
+                new TypeReference<Map<String, Object>>() {
+                });
 
-        // Join and encode parameters
-        List<String> flattenedParameters = new ArrayList<>();
-        for (Map.Entry<String, String[]> entry : sortedParameters.entrySet()) {
-            for (String value : entry.getValue()) {
-                flattenedParameters.add(entry.getKey() + "=" + value);
-            }
-        }
-        String parameterString = String.join("&", flattenedParameters);
+        // Flatten and sort JSON elements
+        String flattenJson = bodyJson.entrySet()
+                .stream()
+                .flatMap(FlatMap::flatten)
+                .map(e -> e.getKey() + "=" + e.getValue())
+                .sorted()
+                .collect(Collectors.joining("&"));
 
         // Read the nonce from the request
-        String nonce = request.getHeader("x-authy-signature-nonce");
+        String nonce = request.getHeader("X-Authy-Signature-Nonce");
 
         // Join all the request bits using '|'
         String data = String.format("%s|%s|%s|%s",
-                                    nonce,
-                                    request.getMethod(),
-                                    url,
-                                    parameterString);
+                nonce,
+                request.getMethod(),
+                url,
+                flattenJson);
 
         try {
             // Compute the signature
             Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
-            SecretKeySpec secret_key = new SecretKeySpec(apiKey.getBytes(),
+            SecretKeySpec secret_key = new SecretKeySpec(authyApiKey.getBytes(),
                     "HmacSHA256");
             sha256_HMAC.init(secret_key);
             String hash = Base64
@@ -49,13 +55,68 @@ public class CallbackVerifier {
                     .encodeToString(sha256_HMAC.doFinal(data.getBytes()));
 
             // Extract the actual request signature
-            String signature = request.getHeader("x-authy-signature");
+            String signature = request.getHeader("X-Authy-Signature");
 
-            // Compute the request signature with your computed signature
+            // Compare the request signature with your computed signature
             return hash.equals(signature);
         } catch (NoSuchAlgorithmException | InvalidKeyException e) {
             e.printStackTrace();
             return false;
         }
+    }
+}
+
+class FlatMap {
+
+    private static String openBracket;
+    private static String closeBracket;
+
+    static {
+        try {
+            openBracket = encode("[", "UTF-8");
+            closeBracket = encode("]", "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String prependKeyName(String keyName, String prefix) {
+        StringBuffer result = new StringBuffer();
+        result.append(prefix);
+        int indexOfOpenBracket = keyName.indexOf(openBracket);
+        if(indexOfOpenBracket == -1) {
+            indexOfOpenBracket = keyName.length();
+        }
+        result.append(openBracket);
+        result.append(keyName.substring(0, indexOfOpenBracket));
+        result.append(closeBracket);
+        result.append(keyName.substring(indexOfOpenBracket));
+        return result.toString();
+
+    }
+
+    public static Stream<Map.Entry<?, ?>> flatten(Map.Entry<?, ?> e) {
+        Object value = e.getValue();
+        if (value instanceof Map<?, ?>) {
+            return ((Map<?, ?>) value).entrySet()
+                    .stream()
+                    .flatMap(FlatMap::flatten)
+                    .map(el -> {
+                        String newKeyName = FlatMap.prependKeyName(
+                                (String) el.getKey(), (String) e.getKey());
+                        return new AbstractMap.SimpleEntry<>(newKeyName, el.getValue());
+                    });
+        } else if(value == null) {
+            return Stream.of(new AbstractMap.SimpleEntry<>(e.getKey(), ""));
+        } else if(value instanceof String) {
+            try {
+                String encodedValue = URLEncoder.encode((String) value, "UTF-8");
+                return Stream.of(new AbstractMap.SimpleEntry<>(e.getKey(),
+                        encodedValue.replace(" ", "+")));
+            } catch (UnsupportedEncodingException exception) {
+                throw new RuntimeException(exception);
+            }
+        }
+        return Stream.of(e);
     }
 }
