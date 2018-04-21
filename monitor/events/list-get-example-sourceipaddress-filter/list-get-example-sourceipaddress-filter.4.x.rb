43b39fd0bc71dba68d5e645eb1c233ef604ab7ee
Dorian Marié <dorian@doma.io>
2018-04-21 11:42:21 +0200

    start making prettier ruby code

--- monitor/events/list-get-example-sourceipaddress-filter/list-get-example-sourceipaddress-filter.4.x.rb
+++ monitor/events/list-get-example-sourceipaddress-filter/list-get-example-sourceipaddress-filter.4.x.rb
@@ -4,7 +4,7 @@ require 'twilio-ruby'
 # Get your Account Sid and Auth Token from twilio.com/user/account
 account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 auth_token = 'your_auth_token'
-@client = Twilio::REST::MonitorClient.new account_sid, auth_token
+@client = Twilio::REST::MonitorClient.new(account_sid, auth_token)
 
 @params = {
   source_ip_address: '104.14.155.29',
