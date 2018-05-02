b5854ef6aaa6877d7169adf608b3b99ca903c38d
Twilio CMS User <nnovakovic@twilio.com>
2018-05-02 13:22:16 -0700

    CMS update of rest/sip-in/map-list-domain by nnovakovic@twilio.com

--- rest/sip-in/map-list-domain/map-list-domain.6.x.py
+++ rest/sip-in/map-list-domain/map-list-domain.6.x.py
@@ -7,8 +7,8 @@ auth_token = "your_auth_token"
 client = Client(account_sid, auth_token)
 
 ip_access_control_list_mapping = client.sip \
-    .domains("SD32a3c49700934481addd5ce1659f04d2") \
-    .ip_access_control_list_mappings \
-    .create("AL95a47094615fe05b7c17e62a7877836c")
+        .domains("SD32a3c49700934481addd5ce1659f04d2") \
+        .ip_access_control_list_mappings \
+        .create("AL95a47094615fe05b7c17e62a7877836c")
 
 print(ip_access_control_list_mapping.sid)
