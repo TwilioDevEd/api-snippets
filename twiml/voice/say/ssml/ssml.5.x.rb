63bf59cb8b363d96bf40ed929e26f90f35297d4b
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-08-17 12:55:29 +0200

    Add PHP and Ruby examples for SSML

--- twiml/voice/say/ssml/ssml.5.x.rb
+++ twiml/voice/say/ssml/ssml.5.x.rb
@@ -1,1 +1,18 @@
-# SSML support for Ruby coming soon.
+require 'twilio-ruby'
+
+response = Twilio::TwiML::VoiceResponse.new
+response.say(voice: 'Polly.Joanna', message: 'Hi') do |say|
+    say.break(strength: 'x-weak', time: '100ms')
+    say.emphasis('Words to emphasize', level: 'moderate')
+    say.p('Words to speak')
+    say.add_text('aaaaaa')
+    say.phoneme('Words to speak', alphabet: 'x-sampa', ph: 'pɪˈkɑːn')
+    say.add_text('bbbbbbb')
+    say.prosody('Words to speak', pitch: '-10%', rate: '85%', volume: '-6dB')
+    say.s('Words to speak')
+    say.say_as('Words to speak', interpretAs: 'spell-out', role: 'yyyymmdd')
+    say.sub('Words to be substituted', alias: 'alias')
+    say.w('Words to speak')
+end
+
+puts response
