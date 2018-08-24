d05471a51df70548be27e132aa4a7aa4ce4bcf64
Twilio CMS User <mstaples@twilio.com>
2018-08-24 11:23:51 -0700

    CMS update of rest/messages/generate-twiml-sms by mstaples@twilio.com

--- rest/messages/generate-twiml-sms/generate-twiml-sms.5.x.php
+++ rest/messages/generate-twiml-sms/generate-twiml-sms.5.x.php
@@ -1,7 +1,7 @@
 <?php
 // Get the PHP helper library from https://twilio.com/docs/libraries/php
 
-require_once '/path/to/vendor/autoload.php'; // Loads the library
+require_once 'vendor/autoload.php'; // Loads the library
 use Twilio\Twiml;
 
 $response = new Twiml;
