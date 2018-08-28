c0bb234f30026b7d9438a2ed815e25ecc688549d
David Prothero <dprothero@twilio.com>
2018-08-28 14:29:04 -0700

    Create SmsController.cs

---
+++ quickstart/csharp/sms/receive-sms-core/SmsController.cs
@@ -0,0 +1,22 @@
+// Code sample for ASP.NET Core on .NET Core
+// From command prompt, run:
+// dotnet add package Twilio.AspNet.Core
+
+using Twilio.AspNet.Common;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace YourNewWebProject.Controllers
+{
+    public class SmsController : TwilioController
+    {
+        public TwiMLResult Index(SmsRequest incomingMessage)
+        {
+            var messagingResponse = new MessagingResponse();
+            messagingResponse.Message("The copy cat says: " +
+                                      incomingMessage.Body);
+
+            return TwiML(messagingResponse);
+        }
+    }
+}
