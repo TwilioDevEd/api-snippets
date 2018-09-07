d3ec4c63d214650d86671192841dfa3b111a867c
Twilio CMS User <alex@twilio.com>
2018-09-07 10:59:03 -0700

    CMS update of quickstart/php/sms/reply_sms by alex@twilio.com

--- quickstart/php/sms/reply_sms/reply_sms.5.x.php
+++ quickstart/php/sms/reply_sms/reply_sms.5.x.php
@@ -1,11 +1,11 @@
 <?php
 require_once "vendor/autoload.php";
-use Twilio\TwiML;
+use Twilio\TwiML\MessagingResponse;
 
 // Set the content-type to XML to send back TwiML from the PHP Helper Library
 header("content-type: text/xml");
 
-$response = new TwiML();
+$response = new MessagingResponse();
 $response->message(
     "I'm using the Twilio PHP library to respond to this SMS!"
 );
