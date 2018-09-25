1530c19c59a79290bb1ad078ad70bfe8421e0839
Twilio CMS User <smendes@twilio.com>
2018-09-25 05:34:43 -0700

    CMS update of rest/messages/sms-conversation-tracking by smendes@twilio.com

--- rest/messages/sms-conversation-tracking/sms-conversation-tracking.2.x.js
+++ rest/messages/sms-conversation-tracking/sms-conversation-tracking.2.x.js
@@ -1,27 +1,27 @@
-const http = require('http');
-const express = require('express');
-const session = require('express-session');
-const twilio = require('twilio');
+var http = require('http'),
+    express = require('express'),
+    session = require('express-session'),
+    twilio = require('twilio');
 
-const app = express();
+var app = express();
 app.use(session({ secret: 'anything-you-want-but-keep-secret' }));
 
-app.post('/sms', (req, res) => {
-  const smsCount = req.session.counter || 0;
+app.post('/sms', function(req, res) {
+  var smsCount = req.session.counter || 0;
 
-  let message = 'Hello, thanks for the new message.';
-  if (smsCount > 0) {
+  var message = 'Hello, thanks for the new message.';
+  if(smsCount > 0) {
     message = 'Hello, thanks for message number ' + (smsCount + 1);
   }
-
+  
   req.session.counter = smsCount + 1;
 
-  const twiml = new twilio.TwimlResponse();
+  var twiml = new twilio.TwimlResponse();
   twiml.message(message);
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
+http.createServer(app).listen(1337, function () {
+  console.log("Express server listening on port 1337");
 });
--- rest/messages/sms-conversation-tracking/sms-conversation-tracking.4.x.rb
+++ rest/messages/sms-conversation-tracking/sms-conversation-tracking.4.x.rb
@@ -2,22 +2,22 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
-use Rack::Session::Cookie, key: 'rack.session',
-                           path: '/',
-                           secret: 'can-be-anything-but-keep-a-secret'
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'can-be-anything-but-keep-a-secret'
 
 post '/sms' do
-  session['counter'] ||= 0
-  sms_count = session['counter']
-  message = if sms_count.zero?
-              'Hello, thanks for the new message.'
-            else
-              "Hello, thanks for message number #{sms_count + 1}"
-            end
+  session["counter"] ||= 0
+  sms_count = session["counter"]
+  if sms_count == 0
+    message = "Hello, thanks for the new message."
+  else
+    message = "Hello, thanks for message number #{sms_count + 1}"
+  end
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message message
   end
-  session['counter'] += 1
+  session["counter"] += 1
 
   content_type 'text/xml'
   twiml.text
