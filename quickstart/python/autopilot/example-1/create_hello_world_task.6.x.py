d623d360ddce96852fb88d05236ba5b4efe5242d
Kat King <kking@twilio.com>
2018-10-15 13:05:41 -0700

    Update hello world task creation code

--- quickstart/python/autopilot/example-1/create_hello_world_task.6.x.py
+++ quickstart/python/autopilot/example-1/create_hello_world_task.6.x.py
@@ -6,18 +6,19 @@ account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 auth_token = 'your_auth_token'
 client = Client(account_sid, auth_token)
 
-# Update your assistant's initiation action task to say something and listen for a repsonse.
-update_initiation_action = {
+# Build task actions that say something and listens for a repsonse.
+hello_world_task_actions = {
     'actions': [
         {'say': 'Hi there, I\'m your virtual assistant! How can I help you?'},
         {'listen': True}
     ]
 }
 
-# Update the default intent to use your new actions.
+# Create the hello_world task
 # Replace 'UAXXX...' with your Assistant's unique SID https://www.twilio.com/console/autopilot/list
-client.preview.understand \
-    .assistants('UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
-    .update(initiation_actions=update_initiation_action)
+task = client.autopilot.assistants('UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+                       .tasks \
+                       .create(unique_name='hello_world') \
+                       .actions(hello_world_task_actions)
 
-print("Intent actions updated")
+print("Hello-world task created!")
