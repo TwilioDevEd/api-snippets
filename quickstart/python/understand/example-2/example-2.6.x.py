fe0cd6a2e8bef1185f43507f881bd8ee9ac12484
Twilio CMS User <krobinson@twilio.com>
2018-10-15 21:10:22 -0700

    CMS update of quickstart/python/understand/example-2 by krobinson@twilio.com

---
+++ quickstart/python/understand/example-2/example-2.6.x.py
@@ -0,0 +1,25 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+# Provide actions for the new task
+joke_actions = {
+    'actions': [
+        {'say': 'I was going to look for my missing watch, but I could never find the time.'}
+    ]
+}
+
+# Create a new task named 'tell_a_joke'
+# Replace 'UAXXX...' with your Assistant's unique SID https://www.twilio.com/console/autopilot/list
+task = client.preview.understand \
+                       .assistants('UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+                       .tasks \
+                       .create(
+                           unique_name='tell-a-joke',
+                           actions=joke_actions)
+
+print(task.sid)
