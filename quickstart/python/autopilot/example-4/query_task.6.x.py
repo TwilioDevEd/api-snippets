ea4d4b83ad380f5b2085f295e044549ec44e7569
Kat King <kking@twilio.com>
2018-10-16 21:13:42 -0700

    Include custom snippet for printing query result

---
+++ quickstart/python/autopilot/example-4/query_task.6.x.py
@@ -0,0 +1,16 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+auth_token = 'your_auth_token'
+client = Client(account_sid, auth_token)
+
+# Replace 'UAXXX...' with your Assistant's unique SID https://www.twilio.com/console/autopilot/list
+query = client.preview.understand \
+                      .assistants('UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+                      .queries \
+                      .create(language='en-US', query='Tell me a joke')
+
+print(query.results.get('task'))
