4db4278608c5d8377f085f3a538dcd3eb2787524
Jonatas Baldin <jonatas.baldin@gmail.com>
2018-11-05 16:35:53 -0200

    Add Autopilot snippets for its Java quickstart

---
+++ quickstart/java/autopilot/create-first-task/create_hello_world_task.7.x.java
@@ -0,0 +1,40 @@
+package com.mycompany.app;
+
+// Download the helper library from https://www.twilio.com/docs/java/install
+import com.twilio.Twilio;
+import com.twilio.rest.autopilot.v1.assistant.Task;
+
+import java.util.HashMap;
+
+public class Example {
+    // Your Account Sid and Auth Token from twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        // Create the hello_world task
+        // Replace 'UAXXX...' with your Assistant's unique SID https://www.twilio.com/console/autopilot/list
+        Task task = Task.creator("UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", "hello-world")
+            .setFriendlyName("hello-world")
+            .setActions(
+                // Build task actions that say something and listens for a repsonse.
+                new HashMap<String, Object>()
+                {{
+                    put("actions", new HashMap[] {
+                        new HashMap<String, String>()
+                        {{
+                            put("say", "Hi there, I'm your virtual assistant! How can I help you?");
+                        }},
+                        new HashMap<String, Boolean>()
+                        {{
+                            put("listen", true);
+                        }}
+                    });
+                }})
+            .create();
+            
+        System.out.println(task.getSid());
+    }
+}
---
+++ quickstart/java/autopilot/create-joke-samples/create_joke_samples.7.x.java
@@ -0,0 +1,41 @@
+package com.mycompany.app;
+
+// Download the helper library from https://www.twilio.com/docs/java/install
+import com.twilio.Twilio;
+import com.twilio.rest.autopilot.v1.assistant.task.Sample;
+
+public class Example {
+    // Your Account Sid and Auth Token from twilio.com/console
+    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "your_auth_token";
+
+    public static String[] phrases = {
+        "Tell me a joke",
+        "Tell me a joke",
+        "I'd like to hear a joke",
+        "Do you know any good jokes?",
+        "Joke",
+        "Tell joke",
+        "Tell me something funny",
+        "Make me laugh",
+        "I want to hear a joke",
+        "Can I hear a joke?",
+        "I like jokes",
+        "I'd like to hear a punny joke"
+    };
+
+    public static void main(String[] args) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        // Replace 'UAXXX...' with your Assistant's unique SID https://www.twilio.com/console/autopilot/list
+        for (String phrase:phrases) {
+            Sample sample = Sample.creator(
+                    "UAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                    "UDXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                    "en-US",
+                    phrase)
+                .create();
+            System.out.println(sample.getSid());
+        }
+    }
+}
