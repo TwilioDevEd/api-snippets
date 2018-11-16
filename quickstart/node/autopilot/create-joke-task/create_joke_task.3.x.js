25ec03b4ba0c339eef133c88ab3e407ef12db520
Kat King <katkingcodes@gmail.com>
2018-11-16 09:51:36 -0500

    Update comment to point to node install

--- quickstart/node/autopilot/create-joke-task/create_joke_task.3.x.js
+++ quickstart/node/autopilot/create-joke-task/create_joke_task.3.x.js
@@ -1,4 +1,4 @@
-// Download the helper library from https://www.twilio.com/docs/python/install
+// Download the helper library from https://www.twilio.com/docs/node/install
 
 // Your Account Sid and Auth Token from twilio.com/console
 const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
