29bf66bbeb08a3cde65f92b9900fbc87a3f688bd
Luis Lopez <llopez@twilio.com>
2018-12-07 18:26:06 +0100

    Compositions c-sharp snippets

---
+++ video/rest/compositions/compose-mosaic/compose-mosaic.5.x.cs
@@ -0,0 +1,56 @@
+// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using System.Collections.Generic;
+using Twilio;
+using Twilio.Rest.Video.V1;
+using static Twilio.Rest.Video.V1.CompositionResource;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Find your API Key SID and Secret at twilio.com/console
+        const string apiKeySid = "SKXXXX";
+        const string apiKeySecret = "your_api_key_secret";
+
+        TwilioClient.Init(apiKeySid, apiKeySecret);
+
+        var layout = new
+        {
+            interviewed = new
+            {
+                z_pos = 2,
+                x_pos = 400,
+                y_pos = 180,
+                width = 480,
+                height = 360,
+                video_sources = new string[]{"interviewed-video"}
+            },
+            interviewers = new {
+                z_pos = 1,
+                x_pos = 10,
+                y_pos = 0,
+                width = 1260,
+                height = 720,
+                max_rows = 3,
+                max_columns = 3,
+                reuse = "show_newest",
+                cells_excluded = new int[]{4},
+                video_sources = new string[]{"interviewer-*"}
+            }
+        };
+
+        var composition = CompositionResource.Create(
+          roomSid: "RMXXXX",
+          audioSources: new List<string>{"*"},
+          audioSourcesExcluded: new List<string>{"advisor-audio"},
+          videoLayout: layout,
+          statusCallback: new Uri('http://my.server.org/callbacks'),
+          resolution: "1280x720",
+          format: FormatEnum.Mp4
+        );
+
+        Console.WriteLine($"Created composition with SID: {composition.Sid}");
+    }
+}
---
+++ video/rest/compositions/get-composition-media-file/get-composition-media-file.5.x.cs
@@ -0,0 +1,30 @@
+// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+class Example
+{
+    static void Main(string[] args)
+    {
+      // Find your API Key SID and Secret at twilio.com/console
+      const string apiKeySid = "SKXXXX";
+      const string apiKeySecret = "your_api_key_secret";
+
+      const string compositionSid = "CJXXXX";
+      const string uri = $"https://video.twilio.com/v1/Compositions/{compositionSid}/Media";
+
+      var request = (HttpWebRequest)WebRequest.Create(uri);
+      equest.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(apiKeySid + ":" + apiKeySecret)));
+      request.AllowAutoRedirect = false;
+      string responseBody = new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
+      var mediaLocation = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseBody)["redirect_to"];
+
+      Console.WriteLine(mediaLocation);
+      new WebClient().DownloadFile(mediaLocation, $"{compositionSid}.out");
+    }
+}
