763dc40d62ced133af4f6a3d10725db22e484826
Andrei Birjukov <abirjukov@twilio.com>
2019-03-04 20:43:13 +0200

    Listener fix

--- ip-messaging/tokens/initialization/initialization.java
+++ ip-messaging/tokens/initialization/initialization.java
@@ -3,8 +3,8 @@ import com.twilio.chat.ChatClientListener;
 
 ChatClient.Properties.Builder builder = new ChatClient.Properties.Builder();
 ChatClient.Properties properties = builder.createProperties();
-// See below for event listener implementation
-ChatClientListener listener = new ChatClientListener();
+// See below for client event listener implementation
+ChatClientListener listener = new Listener();
 
 // Make a secure request to your backend to retrieve an access token.
 // Use an authentication mechanism to prevent token exposure to 3rd parties.
