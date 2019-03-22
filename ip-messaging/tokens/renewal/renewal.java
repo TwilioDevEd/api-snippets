0311263f5f1d0b5f39fbee5f171233f7e231f767
Andrei Birjukov <abirjukov@twilio.com>
2019-03-22 22:33:55 +0200

    Make fetchToken async

--- ip-messaging/tokens/renewal/renewal.java
+++ ip-messaging/tokens/renewal/renewal.java
@@ -1,15 +1,14 @@
 public class Listener implements ChatClientListener {
-
   @Override public void onTokenAboutToExpire()
   {
-    // Make a secure request to your backend to retrieve a refreshed access token.
+    // Implement fetchToken() to make a secure request to your backend to retrieve a refreshed access token.
     // Use an authentication mechanism to prevent token exposure to 3rd parties.
-    String updatedToken = fetchToken();
-
-    chatClient.updateToken(updatedToken, new StatusListener {
-      @Override public void onSuccess() {
-        System.out.println("Token renewed successfully");
-      }
+    fetchToken((String updatedToken) -> {
+      chatClient.updateToken(updatedToken, new StatusListener {
+        @Override public void onSuccess() {
+          System.out.println("Token renewed successfully");
+        }
+      });
     });
   }
 }
