ceabffc4e6c86c39dacf70ce1bea32147dfe2337
PatillaCode <patillacode@gmail.com>
2019-03-27 16:04:51 +0100

    Fix comments

--- api-auth/api-auth.java
+++ api-auth/api-auth.java
@@ -3,8 +3,9 @@ import com.twilio.base.ResourceSet;
 import com.twilio.rest.api.v2010.Account;
 
 public class Example {
-    // Find your Account Sid and Token at twilio.com/user/account
+    // Find your Account Sid at twilio.com/user/account
     public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    // You can generate these from www.twilio.com/console/runtime/api-keys/create
     public static final String API_KEY = "SKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
     public static final String API_SECRET = "your_api_secret";
 
