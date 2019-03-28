c574183639b7057e05a272f224f1c7c22b27feda
Twilio CMS User <mswyers@twilio.com>
2019-03-28 06:36:47 -0700

    CMS update of rest/media/instance-delete-example-1 by mswyers@twilio.com

--- rest/media/instance-delete-example-1/instance-delete-example-1.2.x.js
+++ rest/media/instance-delete-example-1/instance-delete-example-1.2.x.js
@@ -1,19 +1,14 @@
 // Download the Node helper library from twilio.com/docs/node/install
 // These vars are your accountSid and authToken from twilio.com/user/account
-const accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-const authToken = 'your_auth_token';
-const client = require('twilio')(accountSid, authToken);
+var accountSid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+var authToken = "your_auth_token";
+var client = require('twilio')(accountSid, authToken);
 
-client
-  .messages('MM800f449d0399ed014aae2bcc0cc2f2ec')
-  .media('ME557ce644e5ab84fa21cc21112e22c485')
-  .delete((err, data) => {
+client.messages("MM800f449d0399ed014aae2bcc0cc2f2ec").media("ME557ce644e5ab84fa21cc21112e22c485").delete(function(err, data) {
     if (err) {
-      console.log(err.status);
-      throw err.message;
+        console.log(err.status);
+        throw err.message;
     } else {
-      console.log(
-        'Sid ME557ce644e5ab84fa21cc21112e22c485 deleted successfully.'
-      );
+        console.log("Sid ME557ce644e5ab84fa21cc21112e22c485 deleted successfully.");
     }
-  });
+});
