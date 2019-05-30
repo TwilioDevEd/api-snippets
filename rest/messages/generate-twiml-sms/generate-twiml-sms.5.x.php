d7e36155787f8e70c461bba5bca4d9d8b0a2b74f
Gary Hockin <gary@hock.in>
2019-05-30 16:01:26 +0100

    Updated deprecated code in sms Twiml generation

--- rest/messages/generate-twiml-sms/generate-twiml-sms.5.x.php
+++ rest/messages/generate-twiml-sms/generate-twiml-sms.5.x.php
@@ -2,8 +2,8 @@
 // Get the PHP helper library from https://twilio.com/docs/libraries/php
 
 require_once 'vendor/autoload.php'; // Loads the library
-use Twilio\Twiml;
+use Twilio\Twiml\MessagingResponse;
 
-$response = new Twiml;
+$response = new MessagingResponse();
 $response->message("The Robots are coming! Head for the hills!");
 print $response;
