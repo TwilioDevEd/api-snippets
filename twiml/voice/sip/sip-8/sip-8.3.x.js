bdd181db10cd66c4a3afc70ef60495d3a4d48fe0
mmathiyalagan <53189122+mmathiyalagan@users.noreply.github.com>
2019-07-22 12:54:02 -0400

    Updated sip-8.3.x.js with "x-"

--- twiml/voice/sip/sip-8/sip-8.3.x.js
+++ twiml/voice/sip/sip-8/sip-8.3.x.js
@@ -3,6 +3,6 @@ const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 const response = new VoiceResponse();
 const dial = response.dial();
-dial.sip('sip:kate@example.com?mycustomheader=foo&myotherheader=bar');
+dial.sip('sip:kate@example.com?x-mycustomheader=foo&x-myotherheader=bar');
 
 console.log(response.toString());
