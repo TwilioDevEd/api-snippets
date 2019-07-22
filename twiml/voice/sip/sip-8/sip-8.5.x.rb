4bb78dd99b1656d16caff2ead5624c57a9578b33
mmathiyalagan <53189122+mmathiyalagan@users.noreply.github.com>
2019-07-22 12:55:57 -0400

    updated sip-8.5.x.rb with x-

--- twiml/voice/sip/sip-8/sip-8.5.x.rb
+++ twiml/voice/sip/sip-8/sip-8.5.x.rb
@@ -2,7 +2,7 @@ require 'twilio-ruby'
 
 response = Twilio::TwiML::VoiceResponse.new
 response.dial do |dial|
-  dial.sip('sip:kate@example.com?mycustomheader=foo&myotherheader=bar')
+  dial.sip('sip:kate@example.com?x-mycustomheader=foo&x-myotherheader=bar')
 end
 
 puts response
