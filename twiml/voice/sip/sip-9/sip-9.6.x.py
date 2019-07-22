461144a65da23ad4a2421374944fd0a0c6bfe39b
mmathiyalagan <53189122+mmathiyalagan@users.noreply.github.com>
2019-07-22 13:22:06 -0400

    updated sip-9.3.x.py with x-

--- twiml/voice/sip/sip-9/sip-9.6.x.py
+++ twiml/voice/sip/sip-9/sip-9.6.x.py
@@ -10,7 +10,7 @@ dial = Dial(
     action='/handle_post_dial'
 )
 dial.sip(
-    'sip:kate@example.com?customheader=foo',
+    'sip:kate@example.com?x-customheader=foo',
     method='POST',
     url='/handle_screening_on_answer'
 )
