5319232b992a8f8d5c0231fa5aa08d8219804f94
mmathiyalagan <53189122+mmathiyalagan@users.noreply.github.com>
2019-07-22 12:56:30 -0400

    Updated sip-8.5.x.py with x-

--- twiml/voice/sip/sip-8/sip-8.6.x.py
+++ twiml/voice/sip/sip-8/sip-8.6.x.py
@@ -2,7 +2,7 @@ from twilio.twiml.voice_response import Dial, VoiceResponse, Sip
 
 response = VoiceResponse()
 dial = Dial()
-dial.sip('sip:kate@example.com?mycustomheader=foo&myotherheader=bar')
+dial.sip('sip:kate@example.com?x-mycustomheader=foo&x-myotherheader=bar')
 response.append(dial)
 
 print(response)
