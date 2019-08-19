f8acf28f83acf901a1dec28354e8b37556664603
Ricky <evholtz@gmail.com>
2019-08-19 11:56:52 -0700

    Replace phone numbers (#795)

--- verify/verifications/approve-verification/approve-verification.3.x.js
+++ verify/verifications/approve-verification/approve-verification.3.x.js
@@ -5,6 +5,6 @@ const authToken = 'your_auth_token';
 const client = require('twilio')(accountSid, authToken);
 
 client.verify.services('VAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-             .verifications('+14159373912')
+             .verifications('+15017122661')
              .update({status: 'approved'})
              .then(verification => console.log(verification.to));
