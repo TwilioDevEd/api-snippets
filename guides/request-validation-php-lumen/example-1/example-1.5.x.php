faac5264fcdde69872e73f8a7a5ad106ce6d803b
childish-sambino <sharrison@twilio.com>
2019-09-24 17:16:12 -0500

    Add JSON body request validation support to PHP (#766)

--- guides/request-validation-php-lumen/example-1/example-1.5.x.php
+++ guides/request-validation-php-lumen/example-1/example-1.5.x.php
@@ -21,10 +21,17 @@ class TwilioRequestValidator
       // You can get your authentication token in your twilio console https://www.twilio.com/console
       $requestValidator = new RequestValidator(env('TWILIO_AUTH_TOKEN'));
 
+      $requestData = $request->toArray();
+
+      // Switch to the body content if this is a JSON request.
+      if (array_key_exists('bodySHA256', $requestData)) {
+        $requestData = $request->getContent();
+      }
+
       $isValid = $requestValidator->validate(
         $request->header('X-Twilio-Signature'),
         $request->fullUrl(),
-        $request->toArray()
+        $requestData
       );
 
       if ($isValid) {
