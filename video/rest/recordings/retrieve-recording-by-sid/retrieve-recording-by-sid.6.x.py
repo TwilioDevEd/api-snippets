fe6344bbb11d1379a047eaa6b5a1717f0fde172c
Luis Lopez <llopez@twilio.com>
2019-09-27 17:01:01 +0200

    Fixed typo retreiving video recording

--- video/rest/recordings/retrieve-recording-by-sid/retrieve-recording-by-sid.6.x.py
+++ video/rest/recordings/retrieve-recording-by-sid/retrieve-recording-by-sid.6.x.py
@@ -7,7 +7,7 @@ api_key_secret = "your_api_key_secret"
 client = Client(api_key_sid, api_key_secret)
 
 recording = client.video \
-    .recordings('RMXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
+    .recordings('RTXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') \
     .fetch()
 
 print(recording.type)
