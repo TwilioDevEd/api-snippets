0b38bde971546398e790d1c83d3f0e39037f83e7
Gray-Wind <ilya.kolo@gmail.com>
2019-11-06 14:45:07 +0200

    Update iOS push notification code snippets

--- ip-messaging/pushiOS/didreceiveremote/didreceiveremote.m
+++ ip-messaging/pushiOS/didreceiveremote/didreceiveremote.m
@@ -1,3 +1,23 @@
+// For iOS 10 and later; do not forget to set up a delegate for UNUserNotificationCenter
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler {
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    // If your application supports multiple types of push notifications, you may wish to limit which ones you send to the TwilioChatClient here
+    if (self.chatClient) {
+        // If your reference to the Chat client exists and is initialized, send the notification to it
+        [self.chantClient handleNotification:userInfo completion:^(TCHResult *result) {
+            if (![result isSuccessful]) {
+                // Handling of notification was not successful, retry?
+            }
+        }];
+    } else {
+         // Store the notification for later handling
+         self.receivedNotification = userInfo;
+     }
+}
+
+// For iOS versions before 10
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
   // If your application supports multiple types of push notifications, you may wish to limit which ones you send to the TwilioChatClient here
   if (self.chatClient && userInfo) {
--- ip-messaging/pushiOS/usernotificationsettings/usernotificationsettings.m
+++ ip-messaging/pushiOS/usernotificationsettings/usernotificationsettings.m
@@ -1,3 +1,15 @@
+// For iOS 10 and later add this to the didFinishLaunchingWithOptions function or a similar place
+// once you get granted permissions
+if (@available(iOS 10.0, *)) {
+    UNUserNotificationCenter *currentNotificationCenter = [UNUserNotificationCenter currentNotificationCenter];
+    [currentNotificationCenter getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings *settings) {
+        if (settings.authorizationStatus == UNAuthorizationStatusAuthorized) {
+            [UIApplication.sharedApplication registerForRemoteNotifications];
+        }
+    }];
+}
+
+// For iOS versions before 10 you should add such implementation
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
     if(notificationSettings.types == UIUserNotificationTypeNone) {
         NSLog(@"Failed to get token, error: Notifications are not allowed");
@@ -7,6 +19,6 @@
             self.updatedPushToken = nil;
         }
     } else {
-        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        [UIApplication.sharedApplication registerForRemoteNotifications];
     }
 }
