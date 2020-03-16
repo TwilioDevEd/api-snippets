82f996e74f4b1765b7c7ebce7b4ccec9e7035c90
Craig Dennis <cdennis@twilio.com>
2020-03-16 13:36:00 -0700

    Adds MessagingResponses

--- quickstart/php/sms/example-3/sms-hello-monkey.5.x.php
+++ quickstart/php/sms/example-3/sms-hello-monkey.5.x.php
@@ -2,9 +2,9 @@
 // Get the PHP helper library from https://twilio.com/docs/libraries/php
 // following the instructions to install it with Composer.
 require_once "vendor/autoload.php";
-use Twilio\TwiML;
+use Twilio\TwiML\MessagingResponse;
 
-$response = new TwiML();
+$response = new MessagingResponse();
 $response->message('Hello, Mobile Monkey');
 
 header("content-type: text/xml");
--- quickstart/php/sms/example-4/mms-hello-monkey.5.x.php
+++ quickstart/php/sms/example-4/mms-hello-monkey.5.x.php
@@ -2,9 +2,9 @@
 // Get the PHP helper library from https://twilio.com/docs/libraries/php
 // following the instructions to install it with Composer.
 require_once "vendor/autoload.php";
-use Twilio\TwiML\VoiceResponse;
+use Twilio\TwiML\MessagingResponse;
 
-$response = new VoiceResponse();
+$response = new MessagingResponse();
 $message = $response->message();
 $message->body('Hello, Mobile Monkey');
 $message->media('https://demo.twilio.com/owl.png');
