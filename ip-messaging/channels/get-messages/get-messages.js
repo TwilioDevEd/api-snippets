1e844a7b1b0192110761fe240be85bd52f70f45b
Anees kodappana <aneeskodappana@gmail.com>
2020-06-22 10:33:26 +0530

    Updated code snippet
    variable initialization missing, ```undefined variable i ```

--- ip-messaging/channels/get-messages/get-messages.js
+++ ip-messaging/channels/get-messages/get-messages.js
@@ -1,7 +1,7 @@
 // Get Messages for a previously created channel
 channel.getMessages().then(function(messages) {
   const totalMessages = messages.items.length;
-  for (i = 0; i < totalMessages; i++) {
+  for (var i = 0; i < totalMessages; i++) {
     const message = messages.items[i];
     console.log('Author:' + message.author);
   }
