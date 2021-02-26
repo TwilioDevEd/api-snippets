1e836039085ed7460ef8230a0a68e3b390b4d651
Jeff Linwood <jlinwood@gmail.com>
2021-02-26 17:52:53 -0600

    iOS Examples for downloading conversations media

--- ip-messaging/media/retrieve-message-media/retrieve-message-media.swift
+++ ip-messaging/media/retrieve-message-media/retrieve-message-media.swift
@@ -1,24 +1,9 @@
-// Set up output stream for media content
-let tempFilename = (NSTemporaryDirectory() as NSString).appendingPathComponent(message.mediaFilename ?? "file.dat")
-let outputStream = OutputStream(toFileAtPath: tempFilename, append: false)
-
-// Request the start of the download
-if let outputStream = outputStream {
-    message.getMediaWith(outputStream,
-                         onStarted: {
-                            // Called when download of media begins.
-    },
-                         onProgress: { (bytes) in
-                            // Called as download progresses, with the current byte count.
-    },
-                         onCompleted: { (mediaSid) in
-                            // Called when download is completed, with the new mediaSid if successful.
-                            // Full failure details will be provided through the completion block below.
-    }) { (result) in
-        if !result.isSuccessful() {
-            print("Download failed: \(String(describing: result.error))")
-        } else {
-            print("Download successful")
+if message.hasMedia() {
+    message.getMediaContentTemporaryUrl { (result, mediaContentUrl) in
+        guard let mediaContentUrl = mediaContentUrl else {
+            return
         }
+        // Use the url to download an image or other media
+        print(mediaContentUrl)
     }
 }
