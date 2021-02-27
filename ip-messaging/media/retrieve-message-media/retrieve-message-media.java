4ba50774398bb343f78dd4ccb568bada191a106f
Jeff Linwood <jlinwood@gmail.com>
2021-02-26 18:10:48 -0600

    Update Android retrieve conversations media example

--- ip-messaging/media/retrieve-message-media/retrieve-message-media.java
+++ ip-messaging/media/retrieve-message-media/retrieve-message-media.java
@@ -1,41 +1,8 @@
 if (message.hasMedia()) {
-    Message.Media media = message.getMedia();
-
-    String sid = media.getSid();
-    String type = media.getType();
-    String fn = media.getFileName();
-    long size = media.getSize();
-
-    Timber.d("This is a media message with SID "+sid+", type "+type+", name "+fn+", and size "+size);
-
-    if (type.contentEquals("text/plain")) {
-        final ByteArrayOutputStream out = new ByteArrayOutputStream();
-        media.download(out, new StatusListener() {
-            @Override
-            public void onSuccess() {
-                String content = out.toString();
-                Timber.d("Downloaded media "+content);
-            }
-
-            @Override
-            public void onError(ErrorInfo error) {
-                Timber.e("Error downloading media");
-            }
-        }, new ProgressListener() {
-            @Override
-            public void onStarted() {
-                Timber.d("Download started");
-            }
-
-            @Override
-            public void onProgress(long bytes) {
-                Timber.d("Downloaded "+bytes+" bytes");
-            }
-
-            @Override
-            public void onCompleted(String mediaSid) {
-                Timber.d("Download completed");
-            }
-        });
-    }
+    message.getMediaContentTemporaryUrl(new CallbackListener<String>() {
+        @Override
+        public void onSuccess(String mediaContentUrl) {
+            Log.d("TAG", mediaContentUrl);
+        }
+    });
 }
