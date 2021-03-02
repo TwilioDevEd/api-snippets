7f8c7d56b9c1be8fc15e1b932a7ecea2fd3b9162
Craig Dennis <cdennis@twilio.com>
2021-03-01 16:40:42 -0800

    Adds Parameter Client example

---
+++ twiml/voice/parameter/parameter-1/parameter-1.6.x.py
@@ -0,0 +1,12 @@
+from twilio.twiml.voice_response import Client, Dial, Identity, Parameter, VoiceResponse
+
+response = VoiceResponse()
+dial = Dial()
+client = Client()
+client.identity('user-jane')
+client.parameter(name='FirstName', value='Jane')
+client.parameter(name='LastName', value='Doe')
+dial.append(client)
+response.append(dial)
+
+print(response)
