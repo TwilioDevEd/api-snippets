fa371b2bb295c74d32531c48aedf94ef5f96be31
Sarah Stringer <sstringer@twilio.com>
2021-03-18 16:17:40 -0700

    Remove duplicate code samples.

--- twiml/voice/connect/virtualagent-3/virtualagent.5.x.rb
+++
@@ -1,8 +0,0 @@
-require 'twilio-ruby'
-
-response = Twilio::TwiML::VoiceResponse.new
-response.connect(action: 'https://myactionurl.com/twiml') do |connect|
-    connect.virtual_agent(connector_name: 'project', status_callback: 'https://mycallbackurl.com')
-end
-
-puts response
