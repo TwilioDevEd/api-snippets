69b3aaed17dfe3652bb4acffe43ca5af489b8070
Sarah Stringer <sstringer@twilio.com>
2021-04-30 16:07:46 -0700

    Remove incorrect connect examples.

--- twiml/voice/connect/connect-2/connect-2.3.x.js
+++
@@ -1,9 +0,0 @@
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
-
-const response = new VoiceResponse();
-const connect = response.connect();
-connect.room({
-    statusCallback: 'https://myactionurl.com/'
-}, 'meeting');
-
-console.log(response.toString());
--- twiml/voice/connect/connect-3/connect-3.5.x.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'vendor/autoload.php';
-use Twilio\TwiML\VoiceResponse;
-
-$response = new VoiceResponse();
-$connect = $response->connect(['action' => 'https://myactionurl.com/']);
-$connect->room('meeting', ['statusCallback' => 'https://myactionurl.com/']);
-
-echo $response;
