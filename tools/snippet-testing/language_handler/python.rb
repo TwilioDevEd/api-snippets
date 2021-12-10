f36761ef2dad19d0436ce6ea31d2cafe2196a88d
Alexis Crespo <alexis-2cab@hotmail.com>
2021-12-10 08:55:42 -0500

    Fix python tests runner

--- tools/snippet-testing/language_handler/python.rb
+++ tools/snippet-testing/language_handler/python.rb
@@ -16,7 +16,20 @@ module LanguageHandler
     end
 
     private
-
+    def text_with_custom_header(file_content)
+      cert_path = ENV['FAKE_CERT_PATH']
+      file_content.prepend(
+        "import twilio.rest.resources.base\n"\
+        "import sys\n"\
+        "twilio.rest.resources.base.get_cert_file = lambda: '#{cert_path}'\n"\
+        "sys.modules['twilio.rest.base.resources'] = twilio.rest.resources.base\n"
+      )
+    end
+    def replace_twilio_client_initialization(file_content)
+      cert_path = ENV['FAKE_CERT_PATH']
+      file_content.gsub! 'Client(account_sid, auth_token)',
+        'Client(account_sid, auth_token, http_client=FakerHttpClient())' || file_content
+    end
     def execute_command(file)
       command = bash_string_command(
         'source /usr/local/bin/virtualenvwrapper.sh &&'\
