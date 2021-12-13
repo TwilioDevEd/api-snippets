bd80d1d4b7407073fb24c0a5e43da08b661ab72b
Alexis Crespo <alexis-2cab@hotmail.com>
2021-12-13 11:47:50 -0500

    Restore missing python tests runner methods

--- tools/snippet-testing/language_handler/python.rb
+++ tools/snippet-testing/language_handler/python.rb
@@ -26,6 +26,21 @@ module LanguageHandler
       execute_with_suppressed_output(command, file)
     end
 
+    def text_with_custom_header(file_content)
+      cert_path = ENV['FAKE_CERT_PATH']
+      file_content.prepend(
+        "import twilio.rest.resources.base\n"\
+        "import sys\n"\
+        "twilio.rest.resources.base.get_cert_file = lambda: '#{cert_path}'\n"\
+        "sys.modules['twilio.rest.base.resources'] = twilio.rest.resources.base\n"
+      )
+    end
+    def replace_twilio_client_initialization(file_content)
+      cert_path = ENV['FAKE_CERT_PATH']
+      file_content.gsub! 'Client(account_sid, auth_token)',
+        'Client(account_sid, auth_token, http_client=FakerHttpClient())' || file_content
+    end
+
     def text_with_specific_replacements(file_content)
       text_with_custom_header(file_content)
       replace_twilio_client_initialization(file_content)
