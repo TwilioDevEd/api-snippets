ced8fde650d9ad1cb6c6b1804cc620aa3ec992a8
Sarah Stringer <sstringer@twilio.com>
2022-03-09 10:59:28 -0800

    Other fixes from twiml generator for tokenize.

--- twiml/voice/pay/pay-tokenize-connector/pay-tokenize-connector.5.x.cs
+++ twiml/voice/pay/pay-tokenize-connector/pay-tokenize-connector.5.x.cs
@@ -8,11 +8,7 @@ class Example
     static void Main()
     {
         var response = new VoiceResponse();
-        response.Pay(
-            chargeAmount: "0", 
-            paymentConnector: "My_Pay_Connector", 
-            action: new Uri("https://your-callback-function-url.com/pay")
-        );
+        response.Pay(chargeAmount: "0", paymentConnector: "My_Pay_Connector", action: new Uri("https://your-callback-function-url.com/pay"));
 
         Console.WriteLine(response.ToString());
     }
