e77b7060dea5efdd7ac250792f187703433ccdf5
antonhi <aih180000@utdallas.edu>
2022-07-06 18:18:46 -0500

    Updates to client authentication and Java snippet

--- video/rest/compositions/get-composition-media-file/get-composition-media-file.5.x.cs
+++ video/rest/compositions/get-composition-media-file/get-composition-media-file.5.x.cs
@@ -5,21 +5,25 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
+using Twilio;
 using Newtonsoft.Json;
 
 class Example
 {
     static void Main(string[] args)
     {
-      // Find your API Key SID and Secret at twilio.com/console
-      const string apiKeySid = Environment.GetEnvironmentVariable("TWILIO_API_KEY");
-      const string apiKeySecret = Environment.GetEnvironmentVariable("TWILIO_API_KEY_SECRET");
+      // Find your Account SID and Auth Token at twilio.com/console
+      // and set the environment variables. See http://twil.io/secure
+      string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+      string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+      TwilioClient.Init(accountSid, authToken);
 
       const string compositionSid = "CJXXXX";
       const string uri = $"https://video.twilio.com/v1/Compositions/{compositionSid}/Media?Ttl=3600";
 
       var request = (HttpWebRequest)WebRequest.Create(uri);
-      equest.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(apiKeySid + ":" + apiKeySecret)));
+      request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(Encoding.ASCII.GetBytes(apiKeySid + ":" + apiKeySecret)));
       request.AllowAutoRedirect = false;
       string responseBody = new StreamReader(request.GetResponse().GetResponseStream()).ReadToEnd();
       var mediaLocation = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseBody)["redirect_to"];
--- video/rest/compositions/get-composition-media-file/get-composition-media-file.8.x.java
+++ video/rest/compositions/get-composition-media-file/get-composition-media-file.8.x.java
@@ -3,36 +3,38 @@ import com.twilio.Twilio;
 import com.twilio.rest.video.v1.Composition;
 import com.twilio.rest.video.v1.Composition.Format;
 
-public class GetCompositionMedia{
+public class GetCompositionMedia {
 
-  // Find your credentials at twilio.com/console
-  // To set up environment variables, see http://twil.io/secure
-  public static final String API_KEY_SID = System.getenv("TWILIO_API_KEY_SID");
-  public static final String API_KEY_SECRET = System.getenv("TWILIO_API_KEY_SECRET");
+  // Find your Account SID and Auth Token at twilio.com/console
+  // and set the environment variables. See http://twil.io/secure
+  public static final String ACCOUNT_SID = System.getenv("TWILIO_ACCOUNT_SID");
+  public static final String AUTH_TOKEN = System.getenv("TWILIO_AUTH_TOKEN");
 
   public static void main( String[] args ) throws IOException{
     // Disable HttpClient follow redirect by default
     HttpClientBuilder clientBuilder = HttpClientBuilder.create();
     clientBuilder.disableRedirectHandling();
-
+​
     // Initialize the client
     TwilioRestClient restClient = new TwilioRestClient
-        .Builder(API_KEY_SID, API_KEY_SECRET)
-        .httpClient(new NetworkHttpClient(clientBuilder))
-        .build();
-
+            .Builder(ACCOUNT_SID, AUTH_TOKEN)
+            .httpClient(new NetworkHttpClient(clientBuilder))
+            .build();
+​
     // Retrieve media location
-    String compositionSid = "CJXXXX";
+    String compositionSid = "CJXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
     Request request = new Request(
-        HttpMethod.GET,
-        Domains.VIDEO.toString(),
-        "/v1/Compositions/" + compositionSid + "/Media?Ttl=3600",
-        restClient.getRegion());
-
+            HttpMethod.GET,
+            Domains.VIDEO.toString(),
+            "/v1/Compositions/" + compositionSid + "/Media?Ttl=3600",
+            restClient.getRegion());
+​
     Response response = restClient.request(request);
-    JSONObject json = new JSONObject(response.getContent());
-    String mediaLocation = json.getString("redirect_to");
-
+​
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode actualObj = mapper.readTree(response.getContent());
+    String mediaLocation = actualObj.get("redirect_to").asText();
+​
     // For example, download the media to a local file
     FileUtils.copyURLToFile(new URL(mediaLocation), new File("myFile.mp4"));
   }
