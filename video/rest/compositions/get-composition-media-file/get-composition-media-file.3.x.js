d39d3c79cc1b0f7af93ab4ddf8722e1bb1c751c9
antonhi <aih180000@utdallas.edu>
2022-07-07 16:54:50 -0500

    Header removal for Node

--- video/rest/compositions/get-composition-media-file/get-composition-media-file.3.x.js
+++ video/rest/compositions/get-composition-media-file/get-composition-media-file.3.x.js
@@ -1,7 +1,3 @@
-// NOTE: This example uses the next generation Twilio helper library - for more
-// information on how to download and install this version, visit
-// https://www.twilio.com/docs/libraries/node
-
 const fs = require('fs');
 const request = require('request');
 
