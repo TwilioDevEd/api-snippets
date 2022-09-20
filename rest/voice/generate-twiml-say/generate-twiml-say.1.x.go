93bbfe6ce00e6bad7ea85fb5be9fe0c42511951f
Shawn Stern <stern.shawn@gmail.com>
2022-09-19 19:02:29 -0700

    feat: add initial samples for voice/say in Go

---
+++ rest/voice/generate-twiml-say/generate-twiml-say.1.x.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/twilio/twilio-go/twiml"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK, "Hello from your pals at Twilio!")
+	})
+
+	router.POST("/answer", func(context *gin.Context) {
+		say := &twiml.VoiceSay{
+			Message: "Hello from your pals at Twilio! Have fun.",
+		}
+
+		twimlResult, err := twiml.Voice([]twiml.Element{say})
+		if err != nil {
+			context.String(http.StatusInternalServerError, err.Error())
+		} else {
+			context.Header("Content-Type", "text/xml")
+			context.String(http.StatusOK, twimlResult)
+		}
+	})
+
+	router.Run(":1337")
+}
