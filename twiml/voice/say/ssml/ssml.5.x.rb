ecb7166d93683e24c7d999a612e53700ce8824d6
Shawn Stern <stern.shawn@gmail.com>
2023-03-02 11:31:10 -0800

    fix: ruby prosody argument for words

--- twiml/voice/say/ssml/ssml.5.x.rb
+++ twiml/voice/say/ssml/ssml.5.x.rb
@@ -8,7 +8,7 @@ response.say(voice: 'Polly.Joanna', message: 'Hi') do |say|
   say.add_text('aaaaaa')
   say.phoneme('Words to speak', alphabet: 'x-sampa', ph: 'pɪˈkɑːn')
   say.add_text('bbbbbbb')
-  say.prosody('Words to speak', pitch: '-10%', rate: '85%', volume: '-6dB')
+  say.prosody(words: 'Words to speak', pitch: '-10%', rate: '85%', volume: '-6dB')
   say.s('Words to speak')
   say.say_as('Words to speak', interpretAs: 'spell-out')
   say.sub('Words to be substituted', alias: 'alias')
