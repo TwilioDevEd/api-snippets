23aab7d9fd74af2e16c5fdaa6661b23154651978
Shawn Stern <stern.shawn@gmail.com>
2023-03-31 08:17:45 -0700

    feat: fix all the dead worms

--- security/signature_validation/signature_validation.1.x.go
+++ security/signature_validation/signature_validation.1.x.go
@@ -8,16 +8,16 @@ import (
 )
 
 func main() {
-	// You can find your Auth Token at twilio.com/console
-	// For this example: authToken := "12345"
+	// Your Auth Token from twilio.com/console
 	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
 
+	// Initialize the request validator
 	requestValidator := client.NewRequestValidator(authToken)
 
-	// Twilio's request URL
-	url := "https://mycompany.com/myapp.php?foo=1&bar=2"
+	// Store Twilio's request URL (the url of your webhook) as a variable
+	url := "https://example.com/myapp"
 
-	// Post variables in Twilio's request
+	// Store the application/x-www-form-urlencoded params from Twilio's request as a variable
 	params := map[string]string{
 		"CallSid": "CA1234567890ABCDE",
 		"Caller":  "+12349013030",
@@ -26,8 +26,9 @@ func main() {
 		"To":      "+18005551212",
 	}
 
-	// X-Twilio-Signature header attached to the request
-	signature := "0/KCTR6DLpKmkAf8muzZqo1nDgQ="
+	// Store the X-Twilio-Signature header attached to the request as a variable
+	signature := "Np1nax6uFoY6qpfT5l9jWwJeit0="
 
+	// Check if the incoming signature is valid for your application URL and the incoming parameters
 	fmt.Println(requestValidator.Validate(url, params, signature))
 }
