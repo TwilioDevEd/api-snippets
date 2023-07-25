bb90c074300bd64fa871bc84d6a7be761fcbccfa
Brianna DelValle <bdelvalle@twilio.com>
2023-07-25 14:51:14 -0400

    Deved 8583 (#1036)
    * Add link shortening examples
    
    * Fix github's newline complaints

---
+++ messaging/link-shortening/link-shortening-sms/link-shortening-sms.4.x.js
@@ -0,0 +1,15 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+    .create({
+        shortenUrls: true,
+        messagingServiceSid: 'MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+        body: 'Visit this link to start earning rewards today! https://example.com/N6uAirXeREkpV2MW7kpV2MW7TAvh1zn4gEFMTAvh1zn4gEFMN6uAirXeRE',
+        to: '+15551212121'
+    })
+    .then(message => console.log(message.sid));
---
+++ messaging/link-shortening/link-shortening-sms/link-shortening-sms.6.x.rb
@@ -0,0 +1,19 @@
+# Download the helper library from https://www.twilio.com/docs/ruby/install
+require 'rubygems'
+require 'twilio-ruby'
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+message = @client.messages
+  .create(
+     shorten_urls: true,
+     messaging_service_sid: 'MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+     body: 'Visit this link to start earning rewards today! https://example.com/N6uAirXeREkpV2MW7kpV2MW7TAvh1zn4gEFMTAvh1zn4gEFMN6uAirXeRE',
+     to: '+15551212121'
+   )
+
+puts message.sid
