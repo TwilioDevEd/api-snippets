798fa8c8b709a3f6897a5371cb1bbf23f68ab271
benparj98 <109106005+benparj98@users.noreply.github.com>
2024-06-12 13:22:36 -0700

    Add files via upload

---
+++ messaging/content-templates/create-text-template/TwilioText.java
@@ -0,0 +1,26 @@
+package Examples;
+
+import com.twilio.rest.content.v1.Content;
+
+import java.util.Arrays;
+import java.util.Map;
+
+public class TwilioText {
+    public static String CreateTemplate() {
+        var twilioText = new Content.TwilioText();
+        twilioText.setBody("Hi {{1}}, thanks for contacting Owl Air Support. How can we help?");
+
+        var types = new Content.Types();
+        types.setTwilioText(twilioText);
+
+        var createRequest = new Content.ContentCreateRequest("en", types);
+        createRequest.setFriendlyName("java_sdk_example_twilio_text");
+        createRequest.setVariables(Map.of(
+        "1", "first_name"
+        ));
+
+        var content = Content.creator(createRequest).create();
+
+        return content.getSid();
+    }
+}
